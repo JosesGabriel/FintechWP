51df1f0a7a5490976310a4f29f5ed8ee36503795
Emmanuel Tierra <emman.tierra@gmail.com>
2019-07-16 19:46:22 +0800

    Temporarily removed page-cd.php

--- wp-content/themes/arbitrage-child/page-cd.php
+++ wp-content/themes/arbitrage-child/page-cd.php
@@ -4,17 +4,17 @@
  * Template Name: Gitlab CI/CD
  */
 
-$headers = getallheaders();
+// $headers = getallheaders();
 
-if (!isset($headers['X-Gitlab-Token ']) ||
-    $headers['X-Gitlab-Token '] !== "PD3uh@V51OBd7|b'8Y#$9WZkOo4BYi") {
-    global $wp_query;
-    $wp_query->set_404();
-    status_header( 404 );
-    get_template_part( 404 ); 
-    exit();
-}
+// if (!isset($headers['X-Gitlab-Token ']) ||
+//     $headers['X-Gitlab-Token '] !== "PD3uh@V51OBd7|b'8Y#$9WZkOo4BYi") {
+//     global $wp_query;
+//     $wp_query->set_404();
+//     status_header( 404 );
+//     get_template_part( 404 ); 
+//     exit();
+// }
 
-$output = shell_exec("cd /home/vyndue03/arbitrage.ph && sudo -u vyndue03 -S git pull origin develop");
-var_dump($output);
+// $output = shell_exec("cd /home/vyndue03/arbitrage.ph && sudo -u vyndue03 -S git pull origin develop");
+// var_dump($output);
 exit();

5bca281c4fa4e25ee82992eb2ed411cf22a045e2
Rocky Gonzales <rocky@arbitrage.ph>
2019-07-19 11:20:49 +0800

    Change padding from 15 to 0px in right-dashboard part in global-responsive.php

--- wp-content/themes/arbitrage-child/parts/global-responsive.php
+++ wp-content/themes/arbitrage-child/parts/global-responsive.php
@@ -140,7 +140,7 @@
 		right: -110%;
 		z-index: 9 !important;
 		top: -39px !important;
-		padding: 15px 29px 0 8px !important;
+		padding: 15px 0px 0 8px !important;
 		background-color: rgb(13, 31, 51) !important;
 		width: 40% !important;
 		max-width: 512px !important;

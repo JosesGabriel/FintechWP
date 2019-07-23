b7052db8135103c710d45b4fe6c4d98d1c025e5b
abelarbitrage <abel@arbitrage.ph>
2019-07-23 17:44:05 +0800

    Update template-dashboard.php

--- wp-content/themes/arbitrage-child/template-dashboard.php
+++ wp-content/themes/arbitrage-child/template-dashboard.php
@@ -986,8 +986,6 @@ date_default_timezone_set('Asia/Manila'); ?>
 
                     <?php get_template_part('parts/sidebar', 'alert'); ?>
 					
-					<?php // get_template_part('parts/sidebar', 'ads'); ?>
-					
 					<?php get_template_part('parts/sidebar', 'footer'); ?>
 
 				</div>

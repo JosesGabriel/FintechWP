c7769bf68778b54a88284bf012058c93a1c7448f
Rocky Gonzales <rocky@arbitrage.ph>
2019-07-30 12:45:34 +0800

    Update footer.php

--- wp-content/themes/arbitrage-child/footer.php
+++ wp-content/themes/arbitrage-child/footer.php
@@ -523,16 +523,13 @@ body.admin-bar.et_fixed_nav #main-header, body.admin-bar.et_fixed_nav #top-heade
 
 	jQuery('.right-slider-menu').click(function(){
 
-		var right = jQuery('.inner-placeholder .inner-main-content .right-dashboard-part').css('right');
-
-		console.log('right=> ' + right);
-
-	  if(right == '-110%'){
-			jQuery('.right-dashboard-part').css("right","-7%");
+		
+	  if(jQuery('.right-dashboard-part').css('right') == '-7%' ){
+			jQuery('.right-dashboard-part').css("right","-110%");
 			jQuery('.slidecloseoverlay').css("display","block");
 			jQuery('.right-dashboard-part-overlay').fadeIn("fast");
 		}else{
-			jQuery('.right-dashboard-part').css("right","-110%");
+			jQuery('.right-dashboard-part').css("right","-7%");
 			jQuery('.right-dashboard-part-overlay').fadeIn("fast");
 		}
 	});

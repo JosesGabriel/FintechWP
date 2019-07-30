752051d22f0584fef120ddf82c0862d4fb7346fa
Rocky Gonzales <rocky@arbitrage.ph>
2019-07-30 09:50:35 +0800

    Update global-header.php

--- wp-content/themes/arbitrage-child/parts/global-header.php
+++ wp-content/themes/arbitrage-child/parts/global-header.php
@@ -108,15 +108,17 @@ $cdnorlocal = get_home_url();
                         </ul>
                         
                     </ul>
-      
+        
+                     <div class="right-slider-menu" >
+                        <img src="https://arbitrage.ph/wp-content/themes/arbitrage-child/images/menu.svg" width="22px" style="display: none;">
+                    </div>
+        
                     <div class="opennotification">
                         <div class="notifinnerbase">
                         </div>
                     </div>
 
-                    <div class="right-slider-menu" >
-                        <img src="https://arbitrage.ph/wp-content/themes/arbitrage-child/images/menu.svg" width="22px" style="display: none;">
-                    </div>
+                   
 
                 </div>
             </div>

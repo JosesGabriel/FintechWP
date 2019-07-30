22d4f92520f244596e95d93d0600a91c034b0554
Rocky Gonzales <rocky@arbitrage.ph>
2019-07-30 12:26:57 +0800

    Update footer.php

--- wp-content/themes/arbitrage-child/footer.php
+++ wp-content/themes/arbitrage-child/footer.php
@@ -523,7 +523,7 @@ body.admin-bar.et_fixed_nav #main-header, body.admin-bar.et_fixed_nav #top-heade
 
 	jQuery('.right-slider-menu').click(function(){
 
-		var right = jQuery('.right-dashboard-part').css('right');
+		var right = jQuery('.right-dashboard-part').css('right').val();
 
 		console.log('right=> ' + right);
 

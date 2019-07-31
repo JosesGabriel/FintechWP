8c0faf8e6417e9bf7a31498ffcd3ed69904ae200
Rocky Gonzales <rocky@arbitrage.ph>
2019-07-31 08:34:10 +0800

    Enable right slider menu

--- wp-content/themes/arbitrage-child/footer.php
+++ wp-content/themes/arbitrage-child/footer.php
@@ -521,9 +521,11 @@ body.admin-bar.et_fixed_nav #main-header, body.admin-bar.et_fixed_nav #top-heade
 
 		if($('#right-menu').hasClass('right-slider-menu1')){
 			jQuery('.right-dashboard-part').css("right","-7%");
+			$('#right-slider-icon').attr('src','https://arbitrage.ph/wp-content/themes/arbitrage-child/images/cancel.svg');
 			$('#right-menu').removeClass();
 		}else{
 			jQuery('.right-dashboard-part').css("right","-110%");
+			$('#right-slider-icon').attr('src','https://arbitrage.ph/wp-content/themes/arbitrage-child/images/menu.svg');
 			$('#right-menu').addClass('right-slider-menu1');
 		}
 	});

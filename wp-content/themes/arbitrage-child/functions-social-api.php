87919a441a5030fa9f13f785eed73eb031b53b43
Emmanuel Tierra <emman.tierra@gmail.com>
2019-08-01 23:10:48 +0800

    CHange from custom hook to before_delete_post hook

--- wp-content/themes/arbitrage-child/functions-social-api.php
+++ wp-content/themes/arbitrage-child/functions-social-api.php
@@ -28,20 +28,21 @@ function arbitrage_social_post_api_create($post_id, $user_id, $wall_id) {
     }
 }
 
-add_action('um_activity_before_wall_post_deleted', 'arbitrage_social_post_api_delete');
 /**
  * Deletes a post in social api
  */
-function arbitrage_social_post_api_delete($post_id) {
-    // get this post's user id
-    // not the current id as deletion may have been triggered by an admin
+add_action('before_delete_post', function ($post_id) {
     $user_id = get_post_field('post_author', $post_id);
     $uuid = arbitrage_api_get_user_uuid($user_id);
     $social_post_id = get_post_meta($post_id, 'social_api_post_id', true);
 
-    $data = [
-        'user_id' => $uuid,
-    ];
+    if ($social_post_id) {
 
-    $response = arbitrage_api_curl("api/social/posts/$social_post_id", $data, "DELETE");
-}
+        $data = [
+            'user_id' => $uuid,
+            'pid' => $post_id,
+        ];
+
+        $response = arbitrage_api_curl("api/social/posts/$social_post_id", $data, "DELETE");
+    }
+});

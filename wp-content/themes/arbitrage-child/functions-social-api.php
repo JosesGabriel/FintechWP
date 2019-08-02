a8cafac309d337abc8f547f34172bda11570c18a
Emmanuel Tierra <emman.tierra@gmail.com>
2019-08-02 19:55:06 +0800

    Create WP hook for Social Comment API

--- wp-content/themes/arbitrage-child/functions-social-api.php
+++ wp-content/themes/arbitrage-child/functions-social-api.php
@@ -48,3 +48,29 @@ add_action('before_delete_post', function ($post_id) {
 /**
  * Create a comment in social api
  */
+add_action('wp_insert_comment', function ($comment_id, $comment) {
+    $user_id = get_current_user_id();
+    $user_uuid = arbitrage_api_get_user_uuid($user_id);
+
+    $social_post_id = get_post_meta($comment->comment_post_ID, 'social_api_post_id', true);
+
+    $comment_parent = $comment->comment_parent;
+
+    if ($comment_parent) {
+        $comment_parent = get_comment_meta($comment_parent, 'social_api_comment_id', true);
+    }
+
+    $data = [
+        'parent_id' => $comment_parent,
+        'user_id' => $user_uuid,
+        'content' => $comment->comment_content,
+    ];
+
+    $url = "api/social/posts/$social_post_id/comments";
+
+    $response = arbitrage_api_curl($url, $data);
+
+    if ($response) {
+        add_comment_meta($comment_id, 'social_api_comment_id', $response['comment']['id'], true);
+    }
+});

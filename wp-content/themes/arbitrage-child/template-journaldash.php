54827d065a4f589581c26e9c7e0920a7399f351f
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-08-09 16:30:45 +0800

    journal modal style update'

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -1833,10 +1833,10 @@ if ($getdstocks && $getdstocks != "") {
 																	                                                </div>
 																	                                                <div class="groupinput midd lockedd"><label>Stock</label>
 																	                                                <input type="text" name="inpt_data_stock" id="inpt_data_stock" style="margin-left: -3px;" value="<?php echo $value; ?>" readonly>
-																	                                                <i class="fa fa-lock" aria-hidden="true"></i></div>
+																	                                                <!-- <i class="fa fa-lock" aria-hidden="true"></i></div> -->
 																	                                                <div class="groupinput midd lockedd"><label>Buy Power</label>
 																	                                                <input type="text" name="input_buy_product" id="input_buy_product" style="margin-left: -3px;" value="<?php echo $buypower; ?>" readonly>
-																	                                                <i class="fa fa-lock" aria-hidden="true"></i></div>
+																	                                                <!-- <i class="fa fa-lock" aria-hidden="true"></i></div> -->
 																	                                                <div class="groupinput midd"><label>Buy Price</label><input type="text" name="inpt_data_price"></div>
 																	                                                <div class="groupinput midd"><label>Quantity</label><input type="text" name="inpt_data_qty"></div>
 																	                                            </div>

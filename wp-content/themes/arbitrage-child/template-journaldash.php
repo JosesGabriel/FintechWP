53f1abbf0f2e99355cc5a59ed594964af0fbe318
nick <nickgayo12@gmail.com>
2019-08-10 12:57:49 +0800

    Disable submit button when textfield is empty

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -3687,7 +3687,7 @@ if ($getdstocks && $getdstocks != "") {
 			jQuery('.add-funds-shows').show();
 		});
 		jQuery('#textfield-buyprice').keyup(function(){
-			var inputVal = jQuery('#textfield-buyprice').value;
+			var inputVal = jQuery('#textfield-buyprice').text;
 			// if(inputVal.length != 0){
 			// 	console.log('1');
 			// 	$('#modal-button-confirm').prop('disabled', false);            

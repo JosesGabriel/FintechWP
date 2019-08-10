78479cfb9695b99b726dab82e052cf3f7f8f1d75
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-08-10 21:41:37 +0800

    Chart buy order

--- wp-content/themes/arbitrage-child/template-chart.php
+++ wp-content/themes/arbitrage-child/template-chart.php
@@ -1439,7 +1439,7 @@
                                             		
                                             	?>
                                                 <div class="entr_ttle_bar">
-                                                    <strong>Enter Buy Order</strong> <span class="datestamp_header"><?php date_default_timezone_set('Asia/Manila'); echo date("F j, Y g:i a"); ?></span>
+                                                    <strong>Enter Buy Orderrrrr</strong> <span class="datestamp_header"><?php date_default_timezone_set('Asia/Manila'); echo date("F j, Y g:i a"); ?></span>
                                                 </div>
                                                 <form action="/journal" method="post">
                                                 <div class="entr_wrapper_top">

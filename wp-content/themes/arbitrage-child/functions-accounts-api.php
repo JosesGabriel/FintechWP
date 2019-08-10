de542e3cb76c4184bdf34a29df2c644a00ae5e5c
Emmanuel Tierra <emman.tierra@gmail.com>
2019-08-10 23:53:39 +0800

    Add hook when user removes cover image

--- wp-content/themes/arbitrage-child/functions-accounts-api.php
+++ wp-content/themes/arbitrage-child/functions-accounts-api.php
@@ -11,3 +11,15 @@ add_action('um_after_remove_profile_photo', function ($user_id) {
 
     $response = arbitrage_api_curl('api/users/update', $data);
 });
+
+// delete cover photo
+add_action('um_after_remove_cover_photo', function ($user_id) {
+    $user_uuid = arbitrage_api_get_user_uuid($user_id);
+
+    $data = [
+        'id' => $user_uuid,
+        'cover_image' => null,
+    ];
+
+    $response = arbitrage_api_curl('api/users/update', $data);
+});

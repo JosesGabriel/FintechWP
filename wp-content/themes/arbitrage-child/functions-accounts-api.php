787aff82b50fcd45aecdac6d5daa04a35bc0e1f1
Emmanuel Tierra <emman.tierra@gmail.com>
2019-08-11 14:16:29 +0800

    Add filter when user resizes profile or cover image

--- wp-content/themes/arbitrage-child/functions-accounts-api.php
+++ wp-content/themes/arbitrage-child/functions-accounts-api.php
@@ -1,5 +1,25 @@
 <?php
 
+// update profile or cover after resize
+add_filter('um_ajax_resize_image', function ($output) {
+    extract( $_REQUEST );
+
+    if (in_array($key, ['profile_photo','cover_photo'])) {
+        $user_uuid = arbitrage_api_get_user_uuid( get_current_user_id() );
+
+        $data = [
+            'id' => $user_uuid,
+        ];
+
+        $key = (explode('_', $key))[0] . '_image';
+        $data[$key] = $response['file']['url'];
+
+        arbitrage_api_curl_multipart("api/user/update", $data);
+    }
+
+    return $output;
+});
+
 // delete profile photo
 add_action('um_after_remove_profile_photo', function ($user_id) {
     $user_uuid = arbitrage_api_get_user_uuid($user_id);

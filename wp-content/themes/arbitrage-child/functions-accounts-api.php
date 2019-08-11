cc8d9c374770a5e75f6d26e7cea98d80d6d6f333
Emmanuel Tierra <emman.tierra@gmail.com>
2019-08-11 14:44:56 +0800

    Update profile and cover image delete route

--- wp-content/themes/arbitrage-child/functions-accounts-api.php
+++ wp-content/themes/arbitrage-child/functions-accounts-api.php
@@ -28,11 +28,10 @@ add_action('um_after_remove_profile_photo', function ($user_id) {
     $user_uuid = arbitrage_api_get_user_uuid($user_id);
 
     $data = [
-        'id' => $user_uuid,
         'profile_image' => null,
     ];
 
-    $response = arbitrage_api_curl('api/users/update', $data);
+    $response = arbitrage_api_curl("api/users/$user_uuid/update", $data);
 });
 
 // delete cover photo
@@ -40,9 +39,8 @@ add_action('um_after_remove_cover_photo', function ($user_id) {
     $user_uuid = arbitrage_api_get_user_uuid($user_id);
 
     $data = [
-        'id' => $user_uuid,
         'cover_image' => null,
     ];
 
-    $response = arbitrage_api_curl('api/users/update', $data);
+    $response = arbitrage_api_curl("api/users/$user_uuid/update", $data);
 });

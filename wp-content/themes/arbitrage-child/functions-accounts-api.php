9566f5087a7b35d55262c4f2d21be53a75278e72
Emmanuel Tierra <emman.tierra@gmail.com>
2019-08-11 14:43:55 +0800

    Fix update profile or cover photo on resize

--- wp-content/themes/arbitrage-child/functions-accounts-api.php
+++ wp-content/themes/arbitrage-child/functions-accounts-api.php
@@ -5,16 +5,19 @@ add_filter('um_ajax_resize_image', function ($output) {
     extract( $_REQUEST );
 
     if (in_array($key, ['profile_photo','cover_photo'])) {
-        $user_uuid = arbitrage_api_get_user_uuid( get_current_user_id() );
 
-        $data = [
-            'id' => $user_uuid,
-        ];
+        $response = arbitrage_api_upload_to_gcs($output['source_path']);
 
-        $key = (explode('_', $key))[0] . '_image';
-        $data[$key] = $response['file']['url'];
+        if ($response) {
+            $user_uuid = arbitrage_api_get_user_uuid( get_current_user_id() );
+            $key = (explode('_', $key))[0] . '_image';
 
-        arbitrage_api_curl_multipart("api/user/update", $data);
+            $data = [
+                $key => $response['file']['url'],
+            ];
+    
+            arbitrage_api_curl_multipart("api/users/$user_uuid/update", $data);
+        }
     }
 
     return $output;

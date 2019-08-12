3f4e6f4927b6e097bdc71603a720152218c7a4ff
le <lerrouxlopez@Alexandras-MacBook-Pro.local>
2019-08-12 14:12:08 +0800

    testing stock info

--- wp-content/themes/arbitrage-child/footer-dashboard.php
+++ wp-content/themes/arbitrage-child/footer-dashboard.php
@@ -184,11 +184,12 @@ if ( 'on' === et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
 				curl_close($curl); temp-disabled-end */
 			?>
 			var dstockinfo = '<?php echo $dwatchinfo; ?>';
-			// var dlistfromphp = <?php // print_r(json_encode($dwatchdd['data'])); ?>;
+			console.log(dstockinfo);
+			 var dlistfromphp = <?php // print_r(json_encode($dwatchdd['data'])); ?>;
 
 			// TODO Fix: this is causing front end errors
-			 jQuery.each(dstockinfo.data, function( index, value ) {
-			   jQuery('.listofstocks').append('<a href="#" data-dstock="'+value.symbol+'">'+value.symbol+'</a>');
+			// jQuery.each(dstockinfo.data, function( index, value ) {
+			//   jQuery('.listofstocks').append('<a href="#" data-dstock="'+value.symbol+'">'+value.symbol+'</a>');
 			 });
 
 			var startTime = '9:00 AM';

b0fd942ee140b143df3ff1e9f360aeabb816ac29
Rocky Gonzales <rocky@arbitrage.ph>
2019-08-12 17:52:05 +0800

    Enable sticky sidebar

--- wp-content/themes/arbitrage-child/footer.php
+++ wp-content/themes/arbitrage-child/footer.php
@@ -535,31 +535,31 @@ body.admin-bar.et_fixed_nav #main-header, body.admin-bar.et_fixed_nav #top-heade
 		}
 	});
 
-	// jQuery(window).scroll(function(){
+	 jQuery(window).scroll(function(){
 
 	// 	jQuery('.dashboard-sidebar-left-inner').addClass('fixed-sidebar');
 
-	// 	if($(window).scrollTop() >= 150 ){
+	 	if($(window).scrollTop() >= 150 ){
 
-	// 		if(jQuery('.um-profile').hasClass('topbannerprofile')){
-	// 			return;
-	// 		}else {
-	// 			//jQuery('.dashboard-sidebar-left-inner').addClass('fixed-sidebar');
+	 		if(jQuery('.um-profile').hasClass('topbannerprofile')){
+	 			return;
+	 		}else {
+	 			jQuery('.dashboard-sidebar-left-inner').addClass('fixed-sidebar');
 	// 			jQuery('.right-dashboard-part-inner').addClass('fixed-rightsidebar');
 	// 			jQuery('.top-traiders').addClass('fixed-rightsidebar-traiders');
 	// 			jQuery('.ads1').addClass('fixed-rightsidebar-ads');
 	// 			jQuery('.ontofooter').addClass('fixed-rightsidebar-footer');
-	// 		}
+			}
 
-	// 	}else {
-	// 		//jQuery('.dashboard-sidebar-left-inner').removeClass('fixed-sidebar');
+	 	}else {
+	 		jQuery('.dashboard-sidebar-left-inner').removeClass('fixed-sidebar');
 	// 		jQuery('.right-dashboard-part-inner').removeClass('fixed-rightsidebar');
 	// 		jQuery('.top-traiders').removeClass('fixed-rightsidebar-traiders');
 	// 		jQuery('.ads1').removeClass('fixed-rightsidebar-ads');
 	// 		jQuery('.ontofooter').removeClass('fixed-rightsidebar-footer');
-	// 	}
+	 	}
 
-	// });
+	 });
 
 	</script>
 

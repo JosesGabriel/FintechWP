b1aa443e953d5eb18f8048d0c9b3cb944e62c8e5
le <lerrouxlopez@Alexandras-MacBook-Pro.local>
2019-08-12 15:33:44 +0800

    displaying test stock info

--- wp-content/themes/arbitrage-child/footer-dashboard.php
+++ wp-content/themes/arbitrage-child/footer-dashboard.php
@@ -189,8 +189,8 @@ if ( 'on' === et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
 			//console.log(JSON.parse(dstockinfo));
 			// TODO Fix: this is causing front end errors
 			 jQuery.each(dstockinfo.data, function( index, value ) {
-				console.log(value.symbol);
-			 //  jQuery('.listofstocks').append('<a href="#" data-dstock="'+value.symbol+'">'+value.symbol+'</a>');
+				//console.log(value.symbol);
+			 	jQuery('.listofstocks').append('<a href="#" data-dstock="'+value.symbol+'">'+value.symbol+'</a>');
 			 });
 
 			var startTime = '9:00 AM';

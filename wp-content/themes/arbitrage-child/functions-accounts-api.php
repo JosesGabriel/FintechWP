c1de5b84a02378d37ec3c751a6c1d1b33dae60f2
Emmanuel Tierra <emman.tierra@gmail.com>
2019-08-12 10:30:16 +0800

    Update route to unset profile and cover images

--- wp-content/themes/arbitrage-child/functions-accounts-api.php
+++ wp-content/themes/arbitrage-child/functions-accounts-api.php
@@ -26,21 +26,11 @@ add_filter('um_ajax_resize_image', function ($output) {
 // delete profile photo
 add_action('um_after_remove_profile_photo', function ($user_id) {
     $user_uuid = arbitrage_api_get_user_uuid($user_id);
-
-    $data = [
-        'profile_image' => null,
-    ];
-
-    $response = arbitrage_api_curl("api/users/$user_uuid/update", $data);
+    $response = arbitrage_api_curl("api/users/$user_uuid/profile_image/delete", $data);
 }, 10, 1);
 
 // delete cover photo
 add_action('um_after_remove_cover_photo', function ($user_id) {
     $user_uuid = arbitrage_api_get_user_uuid($user_id);
-
-    $data = [
-        'cover_image' => null,
-    ];
-
-    $response = arbitrage_api_curl("api/users/$user_uuid/update", $data);
+    $response = arbitrage_api_curl("api/users/$user_uuid/cover_image/delete", $data);
 }, 10, 1);

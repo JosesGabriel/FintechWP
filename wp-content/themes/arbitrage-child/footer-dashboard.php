d289d6e836f77dc06d5a58fdb561071af5362de7
le <lerrouxlopez@Alexandras-MacBook-Pro.local>
2019-08-12 15:23:31 +0800

    displaying test stock info

--- wp-content/themes/arbitrage-child/footer-dashboard.php
+++ wp-content/themes/arbitrage-child/footer-dashboard.php
@@ -183,7 +183,7 @@ if ( 'on' === et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
 				$dwatchinfo = curl_exec($curl);
 				curl_close($curl); /*temp-disabled-end */
 			?>
-			var dstockinfo = '<?php echo json_decode($dwatchinfo); ?>';
+			var dstockinfo = '<?php echo $dwatchinfo; ?>';
 			// var dlistfromphp = <?php // print_r(json_encode($dwatchdd['data'])); ?>;
 			console.log(dstockinfo);
 			// TODO Fix: this is causing front end errors

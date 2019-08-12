de7a7909bd65676860c519b72f7c467fc4af2ff7
nick <nickgayo12@gmail.com>
2019-08-12 15:23:54 +0800

    Change vyndue button with the new icon header

--- wp-content/themes/arbitrage-child/parts/global-header.php
+++ wp-content/themes/arbitrage-child/parts/global-header.php
@@ -87,7 +87,7 @@ $cdnorlocal = get_home_url();
                     <div class="dmessagepart" <?php if(is_page(2457)){ ?>style="padding-right:0;"<?php } ?>>
                     	<?php if(!is_page(2457)){ ?>
                             <div class="dmessagepart-wrap" data-show-always="1">
-                                <a href="https://arbitrage.ph/vyndue/"><img src="<?php echo $cdnorlocal; ?>/svg/vyndue-newlogo-white.svg" style="width: 18px;display: inline-block;vertical-align: top;margin-top:3px"></a>
+                                <a href="https://arbitrage.ph/vyndue/"><img src="<?php echo $cdnorlocal; ?>/svg/vyndue-newlogo-white.svg" style="width: 19px;display: inline-block;vertical-align: top;margin-top:3px"></a>
                                 <span class="vyndue-notification" style="display: none;">0</span>
                             </div>
                         <?php } ?>

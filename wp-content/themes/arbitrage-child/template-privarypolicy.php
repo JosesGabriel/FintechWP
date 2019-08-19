da2005f5889d780eeac0915515edeec8dab5ed1a
nick <nickgayo12@gmail.com>
2019-08-19 17:12:19 +0800

    add new page for contact and relink footer pages

--- wp-content/themes/arbitrage-child/template-privarypolicy.php
+++ wp-content/themes/arbitrage-child/template-privarypolicy.php
@@ -29,7 +29,6 @@ get_header('dashboard');
 		<p class="p-padding">Arbitrage PH [https://arbitrage.ph/] (the "Site"). This page informs you of our policies regarding the collection, use and disclosure of Personal Information we receive from users of the Site.</p>
 		<p class="p-padding">We use your Personal Information only for providing and improving the Site. By using the Site, you agree to the collection and use of information in accordance with this policy.</p>
 
-		<br>
 		<h4>Information Collection And Use</h4>
 
 		<p>While using our Site, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to your name ("Personal Information").</p>

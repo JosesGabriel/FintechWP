588577f634dcdb8d0ce938e4a0e11b69d9ddfc50
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-08-20 13:24:41 +0800

    Contact us

--- wp-content/themes/arbitrage-child/template-contacts.php
+++ wp-content/themes/arbitrage-child/template-contacts.php
@@ -41,102 +41,26 @@ get_header('dashboard');
 		</svg>
 	</div>
 <div class="container-fluid">
-	<!--Section: Contact v.2-->
-<section class="mb-4">
-
-<!--Section heading-->
-<h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-<!--Section description-->
-<p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
-	a matter of hours to help you.</p>
-
-<div class="row">
-
-	<!--Grid column-->
-	<div class="col-md-9 mb-md-0 mb-5">
-		<form id="contact-form" name="contact-form" action="mail.php" method="POST">
-
-			<!--Grid row-->
-			<div class="row">
-
-				<!--Grid column-->
-				<div class="col-md-6">
-					<div class="md-form mb-0">
-						<input type="text" id="name" name="name" class="form-control">
-						<label for="name" class="">Your name</label>
-					</div>
-				</div>
-				<!--Grid column-->
-
-				<!--Grid column-->
-				<div class="col-md-6">
-					<div class="md-form mb-0">
-						<input type="text" id="email" name="email" class="form-control">
-						<label for="email" class="">Your email</label>
-					</div>
-				</div>
-				<!--Grid column-->
-
+	<form action="">
+		<div class="row">
+			<div class="col-12">
+				<label for="contact__input--name">Name</label>
+				<input type="text" id="contact__input--name">
 			</div>
-			<!--Grid row-->
-
-			<!--Grid row-->
-			<div class="row">
-				<div class="col-md-12">
-					<div class="md-form mb-0">
-						<input type="text" id="subject" name="subject" class="form-control">
-						<label for="subject" class="">Subject</label>
-					</div>
-				</div>
-			</div>
-			<!--Grid row-->
-
-			<!--Grid row-->
-			<div class="row">
-
-				<!--Grid column-->
-				<div class="col-md-12">
-
-					<div class="md-form">
-						<textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-						<label for="message">Your message</label>
-					</div>
-
-				</div>
-			</div>
-			<!--Grid row-->
-
-		</form>
-
-		<div class="text-center text-md-left">
-			<a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
 		</div>
-		<div class="status"></div>
-	</div>
-	<!--Grid column-->
-
-	<!--Grid column-->
-	<div class="col-md-3 text-center">
-		<ul class="list-unstyled mb-0">
-			<li><i class="fas fa-map-marker-alt fa-2x"></i>
-				<p>San Francisco, CA 94126, USA</p>
-			</li>
-
-			<li><i class="fas fa-phone mt-4 fa-2x"></i>
-				<p>+ 01 234 567 89</p>
-			</li>
-
-			<li><i class="fas fa-envelope mt-4 fa-2x"></i>
-				<p>contact@mdbootstrap.com</p>
-			</li>
-		</ul>
-	</div>
-	<!--Grid column-->
-
-</div>
-
-</section>
-<!--Section: Contact v.2-->
+		<div class="row">
+			<div class="col-12">
+				<label for="contact__input--email">Email</label>
+				<input type="email" id="contact__input--email">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<label for="contact__input--message">Message</label>
+				<textarea name="" id="" cols="30" rows="4" id="contact__input--message"></textarea>
+			</div>
+		</div>
+	</form>
 </div>
 <div class="footer-container">
 	<div class="container a-link-text-align a-link-text-margin a-text-font">

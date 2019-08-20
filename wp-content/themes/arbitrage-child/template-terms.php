de69b2d8b24af2c9dc92ac4c6c7464bfb598e0cd
nick <nickgayo12@gmail.com>
2019-08-20 16:47:48 +0800

    add arbitrage c 2019

--- wp-content/themes/arbitrage-child/template-terms.php
+++ wp-content/themes/arbitrage-child/template-terms.php
@@ -132,7 +132,7 @@ get_header('dashboard');
 		<a href="https://arbitrage.ph/contact/">Contact Us</a> |
 		<a href="https://arbitrage.ph/about/">About Us</a> |
 		<a href="https://arbitrage.ph/FAQ/">FAQ</a> |
-		<a>Arbitrage © 2019</a>
+		<span> Arbitrage © 2019</span>
 	</div>
 </div>
 

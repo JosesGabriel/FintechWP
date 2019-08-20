50d797d1e996d13a8af8e1085968b10e8c46833e
nick <nickgayo12@gmail.com>
2019-08-20 14:48:54 +0800

    Ledger logs and style

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -3533,7 +3533,7 @@ if ($getdstocks && $getdstocks != "") {
                                                                                     <div style="width:95px" class="table-title-live">Buy Value</div>
                                                                                     <div style="width:65px" class="table-title-live">Sell Price</div>
                                                                                     <div style="width:95px" class="table-title-live">Sell Value</div>
-                                                                                    <div style="width:90px" class="table-title-live">Profit/Loss</div>
+                                                                                    <div style="width:80px" class="table-title-live">Profit/Loss</div>
                                                                                     <div style="width:60px" class="table-title-live">%</div>
                                                                                     <div style="width:60px; text-align:center">Action</div>
                                                                                 </div>

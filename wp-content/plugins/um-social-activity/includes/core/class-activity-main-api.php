310d207f615415ca49b658de3ae92b28eef1e1ac
le <lerrouxlopez@Alexandras-MacBook-Pro.local>
2019-08-21 17:04:36 +0800

    no image available for no OG tags

--- wp-content/plugins/um-social-activity/includes/core/class-activity-main-api.php
+++ wp-content/plugins/um-social-activity/includes/core/class-activity-main-api.php
@@ -700,7 +700,7 @@ class Activity_Main_API
             // if images array is empty, do not display
 			if(empty($images)){
                 //$content = str_replace('{post_image}', '', $content);
-                $content = str_replace('{post_image}', '<span class="post-image"><span class="postImageBlur" style="background: url(https://storage.arbitrage.ph/dev/2019/08/db5dee26-pse-flatform-01.jpg);background-size: cover;background-repeat: no-repeat;"><img src="https://storage.arbitrage.ph/dev/2019/08/db5dee26-pse-flatform-01.jpg" class="inpostimage" /></span></span>', $content);
+                $content = str_replace('{post_image}', '<span class="post-image"><span class="postImageBlur" style="background: url(https://storage.arbitrage.ph/dev/2019/08/56fcf011-no_image_available-1.jpg);background-size: cover;background-repeat: no-repeat;"><img src="https://storage.arbitrage.ph/dev/2019/08/56fcf011-no_image_available-1.jpg" class="inpostimage" /></span></span>', $content);
             }else{
                 $content = str_replace('{post_image}', '<span class="post-image"><span class="postImageBlur" style="background: url(' .$images[0]. ');background-size: cover;background-repeat: no-repeat;"><img src="' . $images[0] . '" class="inpostimage" /></span></span>', $content);
             }

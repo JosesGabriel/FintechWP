b23a39cdc1cae224df77f2bd0bdf4cea4ca5060c
nick <nickgayo12@gmail.com>
2019-08-22 09:54:32 +0800

    Display name who reacted post

--- wp-content/themes/arbitrage-child/ultimate-member/templates/activity/user-wall.php
+++ wp-content/themes/arbitrage-child/ultimate-member/templates/activity/user-wall.php
@@ -741,7 +741,7 @@ foreach ( $wallposts->posts as $post ) {
 
 						        		<li class="totabs" data-btname="bearish">Bearish</li>
 
-						        		<li class="totabs" data-btname="liked">Liked</li>
+						        		<!-- <li class="totabs" data-btname="liked">Liked</li> -->
 
 						        	</ul>
 

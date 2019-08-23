a7e50b91dfa1de44f78c508a25675e75d318c0f0
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-08-23 19:08:09 +0800

    Vyndue Safari Support

--- wp-content/themes/arbitrage-child/header-dashboard.php
+++ wp-content/themes/arbitrage-child/header-dashboard.php
@@ -25,6 +25,9 @@ $ismyprofile = ($user->id == $profile_id ? true : false);
 	  src="https://code.jquery.com/jquery-3.4.1.min.js"
 	  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
 	  crossorigin="anonymous"></script>
+	<script>
+		if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {alert('Its Safari');}
+	</script>
 <?php
 global $current_user;
 $user = wp_get_current_user();
@@ -107,9 +110,6 @@ $user = wp_get_current_user();
 		jQuery(document).ready(function () {
 			jQuery('.um-profile-photo .um-btn-auto-width').trigger('click');
 			UM_hide_menus();
-
-			if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {alert('Its Safari');}
-
 		})
 	</script>
 	<?php endif ?>

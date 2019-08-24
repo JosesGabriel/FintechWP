6ecbe7049d7266e152acabf3f323345cbe52cbe0
Rocky Gonzales <rocky@arbitrage.ph>
2019-08-24 09:02:20 +0800

    Search tradelogs update

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -3624,6 +3624,35 @@ if ($getdstocks && $getdstocks != '') {
                                                                                     $dtlprofperc = (abs($dprofit) / ($data_quantity * $data_avr_price)) * 100;
                                                                                     $totalprofit += $dprofit;
                                                                             ?>
+
+                                                                            <li class="s-logs" style="display: none;">
+
+                                                                            	<input type="hidden" value="" name="hsearchlogs">
+                                                                            	<div style="width:99%;">
+																					<div style="width:65px"><?php echo date('m', strtotime($data_sellmonth)); ?>/<?php echo $data_sellday; ?>/<?php echo $data_sellyear; ?></div>
+																					<div style="width:45px"><a href="https://arbitrage.ph/chart/<?php echo $data_stock; ?>" class="stock-label"><?php echo $data_stock; ?></a></div>
+																					<div style="width:55px" class="table-cell-live"><?php echo $data_quantity; ?></div>
+																					<div style="width:65px" class="table-cell-live">₱<?php echo number_format($data_avr_price, 2, '.', ','); ?></div>
+																					<div style="width:95px" class="table-cell-live">₱<?php echo number_format(($data_quantity * $data_avr_price), 2, '.', ','); ?></div>
+																					<div style="width:65px" class="table-cell-live">₱<?php echo number_format($data_sell_price, 2, '.', ','); ?></div>
+																					<div style="width:95px" class="table-cell-live">₱<?php echo number_format($soldplace, 2, '.', ','); ?></div>
+																					<div style="width:80px" class="<?php echo $dprofit > 0 ? 'txtgreen' : 'txtred'; ?> table-cell-live">₱<?php echo number_format($dprofit, 2, '.', ','); ?></div>
+																					<div style="width:65px" class="<?php echo $dprofit > 0 ? 'txtgreen' : 'txtred'; ?> table-cell-live"><?php echo $dprofit > 0 ? '+' : '-'; ?><?php echo number_format($dtlprofperc, 2, '.', ','); ?>%</div>
+																					<div style="width:35px; text-align:center">
+																						<a href="#tradelognotes_<?php echo $data_stock; ?>" class="smlbtn blue fancybox-inline">
+																							<i class="fas fa-clipboard"></i>
+																						</a>
+																					</div>
+																					<div style="width:25px">
+																						<a class="deletelog smlbtn-delete" data-istl="<?php echo $tlvalue['id']; ?>" style="cursor:pointer;text-align:center">
+																							<i class="fas fa-eraser"></i>
+																						</a>
+																					</div>
+																				</div>
+
+
+                                                                            </li>
+
 																			<li class="<?php echo $tlvalue['id']; ?> dloglist">
 
 																				<div style="width:99%;">
@@ -4433,7 +4462,9 @@ if ($getdstocks && $getdstocks != '') {
         		jQuery('.dloglist').css("display","block");
     		}else {
     			jQuery('.dloglist').css("display","none");
-    			console.log($(this).val());
+    			var text = $(this).val();
+    			$('input[name="testing"]').val(text);
+
     		}
 			
 		});

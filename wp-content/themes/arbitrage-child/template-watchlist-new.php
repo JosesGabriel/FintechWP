ce7dc210cd47cd241a008321486f718ba2d8e3b1
nick <nickgayo12@gmail.com>
2019-08-24 17:22:20 +0800

    redesign watchlist page

--- wp-content/themes/arbitrage-child/template-watchlist-new.php
+++ wp-content/themes/arbitrage-child/template-watchlist-new.php
@@ -101,10 +101,7 @@ $watchinfo = get_user_meta('7', '_scrp_stocks_chart', true);
 
 
 	<?php 
-	// get_template_part('parts/global', 'css'); 
-	?>
-	<?php
-	get_template_part('parts/css', 'journal'); 
+	get_template_part('parts/global', 'css'); 
 	?>
     <?php get_template_part('parts/sidebar', 'calc'); ?>
     <?php get_template_part('parts/sidebar', 'varcalc'); ?>

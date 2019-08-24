c14a6e8ebb6398758c030598ad92ff0a47f1f143
Emmanuel Tierra <emman.tierra@gmail.com>
2019-08-24 22:48:04 +0800

    Add endpoint to query stocks

--- wp-content/themes/arbitrage-child/template-charthisto.php
+++ wp-content/themes/arbitrage-child/template-charthisto.php
@@ -19,7 +19,35 @@
     }
 
     if(isset($_GET['query'])){
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, "https://data-api.arbitrage.ph/api/v1/stocks/history/latest?stock=PSE");
+        curl_setopt($curl, CURLOPT_RESOLVE, ['data-api.arbitrage.ph:443:104.25.248.104']);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($curl);
+        curl_close($curl);
 
+        if ($response !== false) {
+            $response = json_decode($response, true);
+
+            $arraymode = $response['data'];
+            $dstock = strtolower($_GET['query']);
+
+            // add params
+            $newinfo = [];
+            foreach ($arraymode as $addvalskey => $addvalsvalue) {
+                $addvalsvalue->full_name = $addvalsvalue->symbol;
+                array_push($newinfo,$addvalsvalue);
+            }
+
+            $listofitems = [];
+            foreach ($newinfo as $key => $value) {
+                if (strpos(strtolower($value->symbol), $dstock) !== false) {
+                    array_push($listofitems, $value);
+                }
+            }
+
+            echo json_encode($listofitems);
+        }
     }
 
     if(isset($_GET['g']) && $_GET['g'] == "fullstack" ){

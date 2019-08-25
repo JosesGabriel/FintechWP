b520e548a0059d01e5863c966d696b418a75293f
Rocky Gonzales <rocky@arbitrage.ph>
2019-08-25 10:18:45 +0800

    Update template-journaldash.php

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -4497,7 +4497,7 @@ if ($getdstocks && $getdstocks != '') {
 			            data: {keyword: keyword},
 			            success: function( data){
 			            	 jQuery(".s-logs").html(''); 
-			            	 jQuery(".s-logs").html(keyword); 
+			            	 jQuery(".s-logs").html(data); 
 			                  console.log("Successful! My post data is: "+ keyword);
 			                },
 			                error: function(error){

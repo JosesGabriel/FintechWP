d90fc0d71dc238b133d3ff3ea00fb13a5ed5a5e1
Rocky Gonzales <rocky@arbitrage.ph>
2019-08-25 14:45:21 +0800

    Update template-journaldash.php

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -4480,13 +4480,14 @@ if ($getdstocks && $getdstocks != '') {
     				var tquantity = $('#tquantity' + i).text();
     				var tavprice = $('#tavprice' + i).text();
     				var tbvalue = $('#tbvalue' + i).text();
+    				var tsellprice = $('#tsellprice' + i).text();
     				console.log(tdata + ' - ' + '-total - row' + totalrow);
 
     				if(keyword == tdata){
     					console.log("success");
     					
     					//$('.tdatalogs').text(tdata);
-    				$("<div style='width:99%;' class='tdatalogs"+ i +"'><div style='width:65px'>" + tdate + "</div><div style='width:45px'>"+ tdata +"</div><div style='width:55px' class='table-cell-live'>" + tquantity + "</div><div style='width:65px' class='table-cell-live'>" + tavprice + "</div><div style='width:95px' class='table-cell-live'> "+ tbvalue +"</div></div>").appendTo('.s-logs');
+    				$("<div style='width:99%;' class='tdatalogs"+ i +"'><div style='width:65px'>" + tdate + "</div><div style='width:45px'>"+ tdata +"</div><div style='width:55px' class='table-cell-live'>" + tquantity + "</div><div style='width:65px' class='table-cell-live'>" + tavprice + "</div><div style='width:95px' class='table-cell-live'> "+ tbvalue +"</div><div style='width:65px' class='table-cell-live'>"+ tsellprice +"</div></div>").appendTo('.s-logs');
     					return;
     				}else{
     					$('.tdatalogs'+ i).remove();

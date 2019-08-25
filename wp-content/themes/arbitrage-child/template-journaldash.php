6a5744586ddf0ffd0272755c940d7e6b9e23d790
Rocky Gonzales <rocky@arbitrage.ph>
2019-08-25 12:38:02 +0800

    Update template-journaldash.php

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -3661,7 +3661,7 @@ if ($getdstocks && $getdstocks != '') {
 
 																				<div style="width:99%;">
 																					<div style="width:65px" class="tdate"><?php echo date('m', strtotime($data_sellmonth)); ?>/<?php echo $data_sellday; ?>/<?php echo $data_sellyear; ?></div>
-																					<div style="width:45px" class="tdata" id="<?php echo 'tdata-' . $tnum; ?>"><a href="https://arbitrage.ph/chart/<?php echo $data_stock; ?>" class="stock-label"><?php echo $data_stock; ?></a></div>
+																					<div style="width:45px" class="<?php echo 'tdata-' . $tnum; ?>" id="<?php echo 'tdata' . $tnum; ?>"><a href="https://arbitrage.ph/chart/<?php echo $data_stock; ?>" class="stock-label"><?php echo $data_stock; ?></a></div>
 																					<div style="width:55px" class="table-cell-live"><?php echo $data_quantity; ?></div>
 																					<div style="width:65px" class="table-cell-live">₱<?php echo number_format($data_avr_price, 2, '.', ','); ?></div>
 																					<div style="width:95px" class="table-cell-live">₱<?php echo number_format(($data_quantity * $data_avr_price), 2, '.', ','); ?></div>
@@ -4474,7 +4474,7 @@ if ($getdstocks && $getdstocks != '') {
     			var tdate = $('.tdate').text();
     			//var tdata = new Array($('.tdata').text());
     			//var tdata = [];
-    			var tdata = $('.tdata').attr('id','tdata-1').text();
+    			var tdata = $('.tdata-' + 1).attr('id','tdata-1').text();
 
     			console.log(tdata);
     			//var td =  $(".tdata").text().length / 2;

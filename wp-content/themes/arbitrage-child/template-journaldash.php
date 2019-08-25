fe4b7ddd270b64c546505501329e14aeaab1a69f
Rocky Gonzales <rocky@arbitrage.ph>
2019-08-25 12:31:06 +0800

    Update template-journaldash.php

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -3661,7 +3661,7 @@ if ($getdstocks && $getdstocks != '') {
 
 																				<div style="width:99%;">
 																					<div style="width:65px" class="tdate"><?php echo date('m', strtotime($data_sellmonth)); ?>/<?php echo $data_sellday; ?>/<?php echo $data_sellyear; ?></div>
-																					<div style="width:45px" class="tdata" id="tdata-<?php echo $tnum; ?>"><a href="https://arbitrage.ph/chart/<?php echo $data_stock; ?>" class="stock-label"><?php echo $data_stock; ?></a></div>
+																					<div style="width:45px" class="tdata" id="<?php echo 'tdata-' . $tnum; ?>"><a href="https://arbitrage.ph/chart/<?php echo $data_stock; ?>" class="stock-label"><?php echo $data_stock; ?></a></div>
 																					<div style="width:55px" class="table-cell-live"><?php echo $data_quantity; ?></div>
 																					<div style="width:65px" class="table-cell-live">₱<?php echo number_format($data_avr_price, 2, '.', ','); ?></div>
 																					<div style="width:95px" class="table-cell-live">₱<?php echo number_format(($data_quantity * $data_avr_price), 2, '.', ','); ?></div>

ab372afdc5581afa3f5df27dfa8616fedad92d1a
Emmanuel Tierra <emman.tierra@gmail.com>
2019-08-26 15:28:55 +0800

    Shorten ticker items list

--- assets/js/angular/controllers.js
+++ assets/js/angular/controllers.js
@@ -81,7 +81,7 @@ app.controller('ticker', ['$scope','$filter', '$http', function($scope, $filter,
             shares: abbr_format(data.volume),
         };
         $scope.ticker.push(transaction);
-        if ($scope.ticker.length > 150) {
+        if ($scope.ticker.length > 50) {
             $scope.ticker.pop();
         }
         $scope.$digest();
@@ -102,7 +102,7 @@ app.controller('ticker', ['$scope','$filter', '$http', function($scope, $filter,
             seller: data.seller.substr(0, 5).trim(),
         };
         $scope.ticker.push(transaction);
-        if ($scope.ticker.length > 50) {
+        if ($scope.ticker.length > 150) {
             $scope.ticker.pop();
         }
         $scope.$digest();

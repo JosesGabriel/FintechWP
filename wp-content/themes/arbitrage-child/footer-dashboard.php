0e8e37e9b8b48ae31f9ac43336ed39a8e9ea11a1
le <lerrouxlopez@Alexandras-MacBook-Pro.local>
2019-08-27 13:31:41 +0800

    Add actual API for stocks list

--- wp-content/themes/arbitrage-child/footer-dashboard.php
+++ wp-content/themes/arbitrage-child/footer-dashboard.php
@@ -190,7 +190,7 @@ if ( 'on' === et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
 			//console.log(JSON.parse(dstockinfo));
 			// TODO Fix: this is causing front end errors
 			 jQuery.each(dstockinfo.data, function( index, value ) {
-				console.log(value.symbol);
+				//console.log(value.symbol);
 			 	jQuery('.listofstocks').append('<a href="#" data-dstock="'+value.symbol+'">'+value.symbol+'</a>');
 			 });
 

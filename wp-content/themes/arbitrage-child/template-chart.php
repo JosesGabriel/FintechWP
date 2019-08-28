6ae64be1940249efe06c5e3bcdeddcbe19b5d061
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-08-28 16:01:47 +0800

    btn outline

--- wp-content/themes/arbitrage-child/template-chart.php
+++ wp-content/themes/arbitrage-child/template-chart.php
@@ -1582,7 +1582,7 @@
                                                     <div class="groupinput">
                                                     	<img class="chart-loader" src="https://arbitrage.ph/wp-content/plugins/um-social-activity/assets/img/loader.svg" style="width: 25px; height: 25px; display: none; float: right; margin-right: 20px;">
                                                         <input type="hidden" value="Live" name="inpt_data_status">
-                                                        <input type="submit" class="confirmtrd green noSelect" value="Confirm Trade">
+                                                        <input type="submit" class="confirmtrd green" style="outline: none;" value="Confirm Trade">
                                                     </div>
                                                  </div>
                                                 </form>

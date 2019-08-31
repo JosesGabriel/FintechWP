3a72392dbd7fb789e1fcf7c3ffee4add1352396c
nick <nickgayo12@gmail.com>
2019-08-31 10:19:24 +0800

    Add input email on countdown

--- wp-content/themes/arbitrage-child/template-countdown.php
+++ wp-content/themes/arbitrage-child/template-countdown.php
@@ -76,6 +76,12 @@
             </div><!-- /.wrap -->
         </div><!-- /.clock-item -->
     </div><!-- /.clock -->
+    <div class="notif-container row">
+        <div class="notif--subb">
+            <input type="email" placeholder="Place your email here to be notified when we launch" class="email--field">
+            <input type="button" value="Sign up" class="email--btn arbitrage-button arbitrage-button--primary">
+        </div>
+    </div>
 </div><!-- /.countdown-wrapper -->
 
 

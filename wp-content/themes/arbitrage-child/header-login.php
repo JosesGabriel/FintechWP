c5b0738605508ef7e7ebd7e0513796eefcff6a1f
Rocky Gonzales <rocky@arbitrage.ph>
2019-08-31 13:50:07 +0800

    Update header-login.php

--- wp-content/themes/arbitrage-child/header-login.php
+++ wp-content/themes/arbitrage-child/header-login.php
@@ -246,12 +246,12 @@ $user = wp_get_current_user();
         </div><!-- /.clock-item -->
 	</div><!-- /.clock -->
 	<div class="notif-container row">
-
-        <div class="notif--subb">
-            <input type="email" placeholder="Place your email here to be notified when we launch" class="email--field" id="email--input">
-            <input type="button" value="Sign up" class="email--btn arbitrage-button arbitrage-button--primary" id="email--button">
-        </div>
-
+		 <form method="post">
+	        <div class="notif--subb">
+	            <input type="email" placeholder="Place your email here to be notified when we launch" class="email--field" id="email--input">
+	            <input type="button" value="Sign up" class="email--btn arbitrage-button arbitrage-button--primary" id="email--button">
+	        </div>
+    	</form>
     </div>
 </div><!-- /.countdown-wrapper -->
 

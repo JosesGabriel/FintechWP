44334fe162e60fbaced416f83d003622e56c81af
Rocky Gonzales <rocky@arbitrage.ph>
2019-08-31 14:30:31 +0800

    Update header-login.php

--- wp-content/themes/arbitrage-child/header-login.php
+++ wp-content/themes/arbitrage-child/header-login.php
@@ -262,12 +262,7 @@ $user = wp_get_current_user();
 if(isset($_POST['send'])){
         $email = $_POST['email'];
        
-        global $wpdb;
-		$table = $wpdb->prefix.'arby_launch_emailnotication';
-		$data = array('user_email' => $email);
-		$format = array('%s');
-		$wpdb->insert($table,$data,$format);
-		
+       //_insert_replace_helper( $table, $data, $format, 'INSERT' );		
     }
 ?>
 

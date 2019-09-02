3cbc701071f285043c2d749e4371b7fa62db8e15
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-02 13:24:59 +0800

    Update sidebar-viewedstocks.php

--- wp-content/themes/arbitrage-child/parts/sidebar-viewedstocks.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-viewedstocks.php
@@ -22,6 +22,10 @@ jQuery(function(){
 
 
         <?php 
+        global $current_user;
+        $user = wp_get_current_user();
+        $userID = $current_user->ID;
+        
             $ismetadis = get_user_meta($userID, '_watchlist_instrumental', true);
             echo $ismetadis;
         ?>

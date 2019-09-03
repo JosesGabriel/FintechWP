ed1c62305dd08c4ad60a3412910d960808a3c36e
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-03 14:41:09 +0800

    Update template-watchlist-new.php

--- wp-content/themes/arbitrage-child/template-watchlist-new.php
+++ wp-content/themes/arbitrage-child/template-watchlist-new.php
@@ -453,7 +453,7 @@ $watchinfo = get_user_meta('7', '_scrp_stocks_chart', true);
 														<div class="">
 
 														<div class="groupinput midd"><label>Stock Code</label>
-															<input type="text" name="" id="" style="margin-left: -3px; text-align: left;" >
+															<input type="text" name="" id="myDropdown" placeholder="Search.." style="margin-left: -3px; text-align: left;" >
 															<div class="dropdown-content ddropbase" style="display: none;">
 																		<input type="hidden" id="dstockname" name="stockname">
 																		<input type="text" placeholder="Search.." id="myInput">

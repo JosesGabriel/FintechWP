af4b5508e191d8b33e4d0142a1bd2b7b4fca27f2
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-04 23:50:08 +0800

    hide ads

--- wp-content/themes/arbitrage-child/template-watchlist-new.php
+++ wp-content/themes/arbitrage-child/template-watchlist-new.php
@@ -581,7 +581,7 @@ $watchinfo = get_user_meta('7', '_scrp_stocks_chart', true);
                     
                     <?php //get_template_part('parts/sidebar', 'watchlist'); ?>
 
-                    <?php get_template_part('parts/sidebar', 'alert'); ?>
+                    <?php //get_template_part('parts/sidebar', 'alert'); ?>
 					
 					<?php get_template_part('parts/sidebar', 'footer'); ?>
 

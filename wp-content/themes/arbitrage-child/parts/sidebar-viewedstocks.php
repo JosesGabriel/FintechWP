82338e8a1f001aade2f2259abab680864b182677
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-04 16:22:02 +0800

    Update sidebar-viewedstocks.php

--- wp-content/themes/arbitrage-child/parts/sidebar-viewedstocks.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-viewedstocks.php
@@ -80,6 +80,7 @@ jQuery(function(){
              for($i = 0; $i < $stockcount; $i++){
                 for ($j = $i + 1; $j < $stockcount; $j++) {
   
+                        if($stock_watched[$i][1]){
                              if ($stock_watched[$i][1] < $stock_watched[$j][1]) {
 
                                     $temp = $stock_watched[$i][0];
@@ -93,6 +94,7 @@ jQuery(function(){
                                     $stock_watched[$j][2] = $temp3;
 
                             }
+                        }
 
                 }
 

feafd309eaf24c80930fb47ed6f6f2b7b348b7b3
Emmanuel Tierra <emman.tierra@gmail.com>
2019-09-04 22:46:39 +0800

    Fix wall post hook not having enough arguments

--- wp-content/themes/arbitrage-child/functions-social-api.php
+++ wp-content/themes/arbitrage-child/functions-social-api.php
@@ -1,11 +1,11 @@
 <?php
 
-add_action('um_activity_after_wall_post_published', 'arbitrage_social_post_api_create');
-add_action('um_activity_after_wall_post_updated', 'arbitrage_social_post_api_create');
+add_action('arbitrage_um_activity_after_wall_post_published', 'arbitrage_social_post_api_create');
+add_action('arbitrage_um_activity_after_wall_post_updated', 'arbitrage_social_post_api_create');
 /**
  * Creates or updates a post in social api
  */
-function arbitrage_social_post_api_create($post_id, $user_id, $wall_id) {
+function arbitrage_social_post_api_create($post_id, $wall_id) {
     $user_id = get_current_user_id();
     $account_user_id = arbitrage_api_get_user_uuid($user_id);
     $social_post_id = get_post_meta($post_id, 'social_api_post_id', true);

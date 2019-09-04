1161174b0472f5ea1dbee33035a9a09923fd8bee
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-09-04 23:57:54 +0800

    notify me

--- wp-content/themes/arbitrage-child/api_page.php
+++ wp-content/themes/arbitrage-child/api_page.php
@@ -151,7 +151,7 @@
 		$addQuery = "INSERT INTO `arby_notifyme_emails` (`id`, `email`, `created_at`) VALUES (NULL, '$str', NULL)";
         $exist = $wpdb->query($addQuery);
 		print_r($exist);
-		print_r('hackdog');
+		print_r('return');
 
 	}else { // market sentiment : check sentiment
 		$dlastupdate = get_post_meta( $adminuser, '_sentiment_'.$_GET['stock'].'_lastupdated', true );

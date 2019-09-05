24ad1660630ec28abd34f4fc5ccf515bd792825c
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-09-05 11:57:18 +0800

    email api

--- wp-content/themes/arbitrage-child/api_page.php
+++ wp-content/themes/arbitrage-child/api_page.php
@@ -149,7 +149,9 @@
         // $str = mysql_real_escape_string($str);
 		$checkQuery = "SELECT * FROM arby_notifyme_emails where email like '$str'";
 		$addQuery = "INSERT INTO `arby_notifyme_emails` (`id`, `email`, `created_at`) VALUES (NULL, '$str', NULL)";
-        $exist = $wpdb->query($addQuery);
+		$exist = $wpdb->query($addQuery);
+		print_r($addQuery);
+		print_r($str);
 		print_r($exist + '69');
 
 	}else { // market sentiment : check sentiment

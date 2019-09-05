f02f7c994b61f05161541b05707f628fbb486f02
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-05 15:51:41 +0800

    Update template-latestnews.php

--- wp-content/themes/arbitrage-child/template-latestnews.php
+++ wp-content/themes/arbitrage-child/template-latestnews.php
@@ -948,34 +948,7 @@ date_default_timezone_set('Asia/Manila'); ?>
 						                	
 								</div>
 
-								<div class="nws-part col-md-4" style="margin-top: 0px;">
-									<?php // get_template_part('parts/sidebar', 'ads');?>
-
-						                		<div class="nws-part">
-							                		<div class="img_sep">
-							                			<div class="ccc" style="background: url('<?php echo $images[$topright]; ?>');background-position: center center;background-size: 100% auto;background-repeat: no-repeat;">
-									                		</div>
-							                		</div>
-							                		<div class="nws-seprator">
-							                			<div class="nws-title">
-							                				<p>
-							                					<strong>
-							                						<a href="https://news.google.com/<?php echo $link[$topright]; ?>" target="_blank">
-							                					<?php echo (strlen($titles[$topright]) > 51 ? substr($titles[$topright], 0, 51) . '...' : $titles[$topright])?></a>
-							                					</strong>
-							                				</p>
-							                			</div>
-							                			<div class="nws-description">
-								                			<p><?php echo (strlen($desc[$topright]) > 120 ? substr($desc[$topright], 0, 120) . '...' : $desc[$topright])?></p>
-															<p><?php echo $source[$topright]; ?></p>
-							                			</div>
-							                		</div>
-						                		</div>
-						                	
-								</div>
-								
-
-
+						
 							</div>
 	                	</div>
                 	</div>

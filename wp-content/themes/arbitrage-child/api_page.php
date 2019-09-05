e281ffc2e5251097105b851bad8f3d0b3d239719
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-09-05 12:30:56 +0800

    email api

--- wp-content/themes/arbitrage-child/api_page.php
+++ wp-content/themes/arbitrage-child/api_page.php
@@ -145,6 +145,7 @@
 
 	}elseif(isset($_GET['daction']) && $_GET['daction'] == 'notify_me_email'){
 		global $wpdb;
+		print_r($_GET['email']);
         $str = stripslashes($_GET['email']);
         // $str = mysql_real_escape_string($str);
 		$checkQuery = "SELECT * FROM arby_notifyme_emails where email like '$str'";
@@ -152,7 +153,7 @@
 		$exist = $wpdb->query($addQuery);
 		print_r($addQuery);
 		print_r($str);
-		print_r($exist + '69');
+		// print_r($exist + '69');
 
 	}else { // market sentiment : check sentiment
 		$dlastupdate = get_post_meta( $adminuser, '_sentiment_'.$_GET['stock'].'_lastupdated', true );

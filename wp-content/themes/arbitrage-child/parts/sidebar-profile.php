7d7c000378a5a3b8005537f5667f606860663cee
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-05 22:24:11 +0800

    Update sidebar-profile.php

--- wp-content/themes/arbitrage-child/parts/sidebar-profile.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-profile.php
@@ -17,26 +17,19 @@
 
 $profile_id = um_profile_id();
 // $friendstotalinit = UM()->Friends_API()->api()->count_friends( $profile_id );
-$friendstotal =  UM()->Friends_API()->api()->count_friends( $profile_id );
-
+$friendreqs = UM()->Friends_API()->api()->friend_reqs_sent( $profile_id );
+$friendstotal = count($friendreqs);
 $coverhphotoactive = um_profile( 'cover_photo' );
 $profilepicactive = um_profile( 'profile_photo' );
-
-
-    //echo "firends->total = " . $friendstotal;
- 
-if ($coverhphotoactive && $profilepicactive && $friendstotal >= 2){
+if ($coverhphotoactive && $profilepicactive && $friendstotal > 3){
   $num = 100;
-  
-}else if((!$coverhphotoactive && $profilepicactive && $friendstotal >= 2) || ($coverhphotoactive && !$profilepicactive && $friendstotal >= 2) || ($coverhphotoactive && $profilepicactive && $friendstotal < 2)){
+}else if((!$coverhphotoactive && $profilepicactive && $friendstotal > 3) || ($coverhphotoactive && !$profilepicactive && $friendstotal > 3) || ($coverhphotoactive && $profilepicactive && $friendstotal < 3)){
   $num = 66;
-}else if((!$coverhphotoactive && !$profilepicactive && $friendstotal >= 2) || ($coverhphotoactive && !$profilepicactive && $friendstotal < 2)|| (!$coverhphotoactive && $profilepicactive && $friendstotal < 2)){
+}else if((!$coverhphotoactive && !$profilepicactive && $friendstotal > 3) || ($coverhphotoactive && !$profilepicactive && $friendstotal < 3)|| (!$coverhphotoactive && $profilepicactive && $friendstotal < 3)){
   $num = 33;
 }else{
   $num = 0;
 }
-
-
 
 ?>
 <div class="left-user-details">

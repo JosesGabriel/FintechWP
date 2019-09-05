d72f500aae298ed8938c76332e0ae1b5c7710469
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-05 22:20:34 +0800

    Update sidebar-profile.php

--- wp-content/themes/arbitrage-child/parts/sidebar-profile.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-profile.php
@@ -17,14 +17,15 @@
 
 $profile_id = um_profile_id();
 // $friendstotalinit = UM()->Friends_API()->api()->count_friends( $profile_id );
-$friendstotal =  UM()->Friends_API()->api()->count_friends($profile_id);
+$friendstotal =  UM()->Friends_API()->api()->count_friends( $profile_id );
 
 $coverhphotoactive = um_profile( 'cover_photo' );
 $profilepicactive = um_profile( 'profile_photo' );
 
 
+    //echo "firends->total = " . $friendstotal;
  
-if($coverhphotoactive && $profilepicactive && $friendstotal >= 2){
+if ($coverhphotoactive && $profilepicactive && $friendstotal >= 2){
   $num = 100;
   
 }else if((!$coverhphotoactive && $profilepicactive && $friendstotal >= 2) || ($coverhphotoactive && !$profilepicactive && $friendstotal >= 2) || ($coverhphotoactive && $profilepicactive && $friendstotal < 2)){

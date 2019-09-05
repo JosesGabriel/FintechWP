904553fddcb31a786b42d9de4c2cdb2d687e92a7
nick <nickgayo12@gmail.com>
2019-09-05 16:33:22 +0800

    Fix charts color journal

--- wp-content/themes/arbitrage-child/template-privarypolicy.php
+++ wp-content/themes/arbitrage-child/template-privarypolicy.php
@@ -11,6 +11,7 @@ get_header();
 
 ?>
 <?php get_template_part('parts/global', 'cssfooter'); ?>
+<?php get_template_part('parts/global', 'css'); ?>
 
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>

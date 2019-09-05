fbcd518c7b48ab006aa7c5bc906dece25d5ef5f1
nick <nickgayo12@gmail.com>
2019-09-05 16:32:08 +0800

    Fix charts color journal

--- wp-content/themes/arbitrage-child/template-terms.php
+++ wp-content/themes/arbitrage-child/template-terms.php
@@ -23,6 +23,7 @@ get_header();
 ?>
 
 <?php get_template_part('parts/global', 'cssfooter'); ?>
+<?php get_template_part('parts/global', 'css'); ?>
 
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 

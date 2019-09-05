9206ff5e308680b341b34c199e0ee858958294e4
nick <nickgayo12@gmail.com>
2019-09-05 17:52:22 +0800

    Fix charts color journal

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -5366,7 +5366,7 @@ if ($getdstocks && $getdstocks != '') {
 	"type": "serial",
 	"categoryField": "category",
 	"rotate": true,
-	"autoMarginOffset": 5,
+	"autoMarginOffset": 80,
 	"marginLeft": 10,
 	"marginRight": 10,
 	"marginTop": 0,

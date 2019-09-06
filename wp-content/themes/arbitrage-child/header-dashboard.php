5a244b22d42f90e60f27fdd81bb9176112d9248d
nick <nickgayo12@gmail.com>
2019-09-06 11:46:52 +0800

    add OG:image

--- wp-content/themes/arbitrage-child/header-dashboard.php
+++ wp-content/themes/arbitrage-child/header-dashboard.php
@@ -16,7 +16,7 @@ $ismyprofile = ($user->ID == $profile_id ? true : false);
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta property="og:title" content="Arbitrage | Philippine Stock Exchange Platform" />
-	<meta property="og:image" content="https://storage.arbitrage.ph/dev/2019/08/3a4bc05c-arbitrage-og-01.jpg" />
+	<meta property="og:image" content="https://storage.arbitrage.ph/dev/2019/09/2f2a3a12-3a4bc05c-arbitrage-og-02.jpg" />
 	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700" rel="stylesheet">
 	<link rel="manifest" href="/manifest.json">     
 	<meta name="msapplication-TileColor" content="#142c46">

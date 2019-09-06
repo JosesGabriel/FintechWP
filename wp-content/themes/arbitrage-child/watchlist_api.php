b99d415eb9b10a0d1a697edab1426992ea312ef6
le <lerrouxlopez@Alexandras-MacBook-Pro.local>
2019-09-06 10:52:09 +0800

    New API Page for watchlist

--- wp-content/themes/arbitrage-child/watchlist_api.php
+++ wp-content/themes/arbitrage-child/watchlist_api.php
@@ -4,5 +4,26 @@
 	* Template page for Watchlist Page Platform
     */
     
-echo 'Hello World';
+	header('Content-Type: application/json');
+	global $wp, $wp_query, $wp_the_query, $wp_rewrite, $wp_did_header;
+	require(getcwd().'/wp-load.php');
+
+    date_default_timezone_set("Asia/Manila");
+    
+
+$action = $_GET['action'];
+
+switch($action){
+    case 'getSMS':
+    break;
+    default:
+    echo 'no action';
+}
+
+function getSMS(){
+echo 'get SMS';
+}
+
+
+
 ?>    

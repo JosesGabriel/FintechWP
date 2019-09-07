1ccc14f06308043aad3ae8c0edfbb0a70000be30
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-07 11:44:42 +0800

    Update footer-dashboard.php

--- wp-content/themes/arbitrage-child/footer-dashboard.php
+++ wp-content/themes/arbitrage-child/footer-dashboard.php
@@ -105,6 +105,8 @@ if ( 'on' === et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
 				e.preventDefault();
 
 				var isstock = jQuery(this).parents('#add-watchlist-param').find("#dstockname").val();
+
+				console.log(isstock);
 				
 				//var countli = jQuery(".listofinfo li").length;
 				console.log(isstock);

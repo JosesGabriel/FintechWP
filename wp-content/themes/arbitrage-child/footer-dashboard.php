e22aef27e1bbd8c2ae59eacfb8166740e6694061
le <lerrouxlopez@Alexandras-MacBook-Pro.local>
2019-09-08 23:03:51 +0800

    Adding Email Notif API

--- wp-content/themes/arbitrage-child/footer-dashboard.php
+++ wp-content/themes/arbitrage-child/footer-dashboard.php
@@ -360,8 +360,7 @@ if ( 'on' === et_get_option( 'divi_back_to_top', 'false' ) ) : ?>
 			});
 
 			jQuery('input[type="checkbox"]').click(function(){
-				console.log(this.id);
-
+				console.log(jQuery(this));
 			});
 			//jQuery('.ddropbase a').click(function(e){
 			jQuery(document).on('click','.ddropbase a',function(e){

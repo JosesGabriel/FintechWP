8b81f8dc832ad864d4359f8938b1df9c8c558a94
le <lerrouxlopez@Alexandras-MacBook-Pro.local>
2019-09-09 00:22:46 +0800

    Adding SMS Modal

--- wp-content/themes/arbitrage-child/template-watchlist-new.php
+++ wp-content/themes/arbitrage-child/template-watchlist-new.php
@@ -473,37 +473,73 @@ $watchinfo = get_user_meta('7', '_scrp_stocks_chart', true);
 											<div class="dclosetab addwatchtab " style="width: 271px;">
 
 
-                                                                <!-- wathlist phone number modal -->    
-                                                                  <!--<button class="btn btn-primary btn-lg" id="btnphonetrigger" data-toggle="modal" data-target="#modal-phonenum" style="display:none;" > -->
-                                                                  <div class="modal" id="modal-phonenum" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <!-- wathlist phone number modal -->    
+                                                <div class="modal" id="depositmods" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 																		<div class="modal-dialog modal-modelbox-margin" role="document" style="left: 0; width: 300px">
 																			<div class="modal-content">
 																				<div class="modal-header header-depo">
-																					<h5 class="modal-title title-depo" id="exampleModalLabel">Add a Cellphone Number</h5>
+																					<h5 class="modal-title title-depo" id="exampleModalLabel">Add Funds</h5>
 																					<button type="button" class="close close-depo" data-dismiss="modal" aria-label="Close">
 																						<i class="fas fa-times modal-btn-close-deposit"></i>
 																					</button>
 																				</div>
 																				<hr class="style14 style15">
-																				<form action="/watchlist" method="post" class="add-funds-show depotincome">
+																				<div class="button-funds groupinput select" style="z-index: 25; margin-bottom: 0; margin-left: 4px;">
+																					<select class="rnd" name="" id="" style="z-index: 20;">
+																						<option class="deposit-modal-btn show-button1" value="">Deposit Funds</option>
+																						<option class="deposit-modal-btn show-button2" value="">Dividend Income</option>
+																					</select>
+																					<!-- <a class="deposit-modal-btn show-button1 arbitrage-button arbitrage-button--primary" style="float: right; font-size: 15px;">Dividend Income</a>
+																					<a class="deposit-modal-btn show-button2 arbitrage-button arbitrage-button--info" style="float: left; font-size: 15px;">Deposit Funds</a> -->
+																				</div>
+																				<form action="/journal" method="post" class="add-funds-show depotincome">
 																				<div class="modal-body depo-body">
 																					<div class="dmainform">
 																						<div class="dinnerform">
 																							<div class="dinitem">
-																									<h5 class="modal-title title-depo-in" id="exampleModalLabel" style="font-weight: 300;font-size: 13px;">Cellphone Number</h5>
-																									<div class="dninput"><input type="text" name="dcellnum" class="depo-input-field number" style="background: #4e6a85; text-align: right;"></div>
+																									<h5 class="modal-title title-depo-in" id="exampleModalLabel" style="font-weight: 300;font-size: 13px;">Enter Amount</h5>
+																									<!-- <div class="dnlabel">Amount</div> -->
+																									<div class="dninput"><input type="text" name="damount" class="depo-input-field number" style="background: #4e6a85; text-align: right;"></div>
 																								</div>
 																							</div>
 																						</div>
 																					</div>
 
 																					<div class="modal-footer footer-depo">
+																						<!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
+																						<input type="hidden" name="ddate" value="<?php echo date('Y-m-d'); ?>">
+																						<input type="hidden" name="istype" value="deposit">
+																						<!-- <input type="submit" name="subs" value="Deposit" class="depotbutton arbitrage-button arbitrage-button--primary"> -->
+																						<a href="#" class="depotbutton arbitrage-button arbitrage-button--primary" style="font-size: 11px;">Deposit</a>
+																						<!-- <button type="button" class="btn btn-primary">Deposit Now!</button> -->
 																					</div>
+																				</form>
+																				<form action="/journal" method="post" class="add-funds-shows dividincome" style="display: none;">
+																						<div class="modal-body depo-body">
+																							<div class="dmainform">
+																								<div class="dinnerform">
+																									<div class="dinitem">
+																										<h5 class="modal-title title-depo-in" id="exampleModalLabel">Dividend Income</h5>
+																										<!-- <div class="dnlabel">Amount</div> -->
+																										<div class="dninput modal-title-content-dev"><input type="text" name="damount" class="depo-input-field" style="text-align: right;"></div>
+																									</div>
+																								</div>
+																							</div>
+																						</div>
+																						<div class="modal-footer footer-depo">
+																							<!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
+																							<input type="hidden" name="ddate" value="<?php echo date('Y-m-d'); ?>">
+																							<input type="hidden" name="istype" value="dividend">
+																							<!-- <input type="submit" name="subs" value="Deposit" class="divibutton arbitrage-button arbitrage-button--primary"> -->
+																							<a href="#" class="divibutton arbitrage-button arbitrage-button--primary">Deposit</a>
+																							<!-- <input type="submit" name="subs" value="Deposit Now!" class="depo-mon-btn"> -->
+																							<!-- <button type="button" class="btn btn-primary">Deposit Now!</button> -->
+																						</div>
 																				</form>
 																			</div>
 																		</div>
-																	</div> 
-                                                                    <!-- wathlist phone number modal -->    
+																	</div>
+                                                <!-- wathlist phone number modal -->    
 
 
 

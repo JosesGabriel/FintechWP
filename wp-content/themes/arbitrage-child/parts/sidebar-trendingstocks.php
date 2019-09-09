0afe04eb6f461df0cafea0f1b85331ae8878e4c9
nick <nickgayo12@gmail.com>
2019-09-09 11:13:19 +0800

    Change hits to hit if hit number is one

--- wp-content/themes/arbitrage-child/parts/sidebar-trendingstocks.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-trendingstocks.php
@@ -159,7 +159,14 @@ jQuery(".stocks-hidden-content").click(function () {
                 <li class="even <?php echo $countss; ?>">
                     <span><?php echo $lfstvalue['stock']; ?></span>
                     <a href="#"><?php echo ucwords($lfstvalue['stnamename']); ?> <br>
-                    <p><?php echo $lfstvalue['following']; print_r($lfstvalue['following']);?> Hits</p></a>
+                    <p><?php
+                         echo $lfstvalue['following'];
+                         
+                         if(strlen($lfstvalue['following']) == 1){
+                            print_r($lfstvalue['following']);
+
+                         }
+                        ?> Hits</p></a>
                 </li>
                 <?php echo ($countss == $numinarrat ? '</div>' : ''); ?>
                  <?php $countss++; ?>

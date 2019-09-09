48784d0744aa94e7d2653dcfad808c34c14c6e1d
Ralph Tolipas <ralph_tolipas@Ralph-Mac.local>
2019-09-09 13:41:18 +0800

    update ticker

--- wp-content/themes/arbitrage-child/template-ticker.php
+++ wp-content/themes/arbitrage-child/template-ticker.php
@@ -559,7 +559,7 @@
 		background-color:#2c3e50; 
 		text-align:left;
 	}
-
+    /*
 	@keyframes marquee {
 		0% {
 			transform: translate(0, 0);
@@ -568,6 +568,7 @@
 			transform: translate(-100%, 0);
 		}
 	}
+    */
     </style>
     
 <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
@@ -577,12 +578,12 @@
 	jQuery(document).ready(function() {
 			forevertickerinit();
 			function forevertickerinit() {
-				jQuery('.arb_custom_ticker').animate({'width': '+=100px'}, 600, "linear", function() {
+				jQuery('.arb_custom_ticker').animate({'width': '+=100px'}, 500, "linear", function() {
 					foreverticker();
 				});
 			}
 			function foreverticker() {
-				jQuery('.arb_custom_ticker').animate({'width': '+=100px'}, 600, "linear", function() {
+				jQuery('.arb_custom_ticker').animate({'width': '+=100px'}, 500, "linear", function() {
 					forevertickerinit();
 				});
 			}

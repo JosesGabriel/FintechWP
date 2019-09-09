a2e6c0ab1c1cdca9331c78b4a1a4f910a43a1cfb
Emmanuel Tierra <emman.tierra@gmail.com>
2019-09-09 11:24:26 +0800

    Add new endpoints for bid-ask

--- assets/js/angular/controllers.js
+++ assets/js/angular/controllers.js
@@ -651,7 +651,31 @@ app.controller('chart', ['$scope','$filter', '$http', '$rootScope', function($sc
     // TODO: ANGULARJS NATIVE TIMEOUT
     function updateMarketDepth(force) {
         if ($scope.stock) {
+            $http.get('https://data-api.arbitrage.ph/api/v1/stocks/market-depth/latest/full-depth?exchange=PSE&symbol=' + $scope.stock.symbol)
+                .then(function (response) {
+                    if (response.data.success) {
+                        let data = response.data.data;
 
+                        $scope.fullaskperc = data.ask_total_percent;
+                        $scope.fullasktotal = data.ask_total;
+                        $scope.fullbidperc = data.bid_total_percent;
+                        $scope.fullbidtotal = data.bid_total;
+
+                    }
+                });
+
+            $http.get('https://data-api.arbitrage.ph/api/v1/stocks/market-depth/latest/top-five-depth?exchange=PSE&symbol=' + $scope.stock.symbol)
+                .then(function (response) {
+                    if (response.data.success) {
+                        let data = response.data.data;
+
+                        $scope.askperc = data.ask_total_percent;
+                        $scope.asktotal = data.ask_total;
+                        $scope.bidperc = data.bid_total_percent;
+                        $scope.bidtotal = data.bid_total;
+
+                    }
+                });
         }
     }
 	setInterval(updateMarketDepth, 5000);

9208ba7bacbf2e52323f0c3161d9c5a37102ba8d
Emmanuel Tierra <emman.tierra@gmail.com>
2019-09-09 11:25:44 +0800

    Remove old bid-ask endpoint on symbol change

--- assets/js/angular/controllers.js
+++ assets/js/angular/controllers.js
@@ -961,45 +961,6 @@ app.controller('tradingview', ['$scope','$filter', '$http', '$rootScope', functi
                                 $scope.$parent.$digest();
                             });
 
-                        // $http.get("//marketdepth.pse.tools/api/market-depth?symbol=" + symbol).then( function (response) {
-                        //     if (response.data.success) {
-                        //         $scope.$parent.marketdepth = response.data.data;
-                        //     }
-                        //     $scope.$parent.bidtotal = 0;
-                        //     $scope.$parent.asktotal = 0;
-                        //     $scope.$parent.bidperc = 0;
-                        //     $scope.$parent.askperc = 0;
-                        //     var keepGoing = true;
-                        //     angular.forEach($scope.$parent.marketdepth, function(value, key) {
-                        //       if(keepGoing) {
-                        //         $scope.$parent.bidtotal += (!value.bid_volume ? 0 : parseInt(value.bid_volume));
-                        //         $scope.$parent.asktotal += (!value.ask_volume ? 0 : parseInt(value.ask_volume));
-                        //         if(key == 4){
-                        //           keepGoing = false;
-                        //         }
-                        //       }
-                        //     });
-                        //     $scope.$parent.bidperc = ($scope.$parent.bidtotal / ($scope.$parent.bidtotal + $scope.$parent.asktotal)) * 100;
-                        //     $scope.$parent.askperc = ($scope.$parent.asktotal / ($scope.$parent.bidtotal + $scope.$parent.asktotal)) * 100;
-                        //     console.log("asking: " +$scope.$parent.asktotal);
-                        //     console.log("bidding: "+$scope.$parent.bidtotal);
-                        //     console.log("ask persentage: "+$scope.$parent.askperc);
-                        //     console.log("bid percentage: "+$scope.$parent.bidperc);
-                        //     $scope.$parent.fullbidtotal = 0;
-                        //     $scope.$parent.fullasktotal = 0;
-                        //     $scope.$parent.fullbidperc = 0;
-                        //     $scope.$parent.fullaskperc = 0;
-
-                        //     var keepGoing = true;
-                        //     angular.forEach($scope.$parent.marketdepth, function(value, key) {
-                        //         $scope.$parent.fullbidtotal += (!value.bid_volume ? 0 : parseInt(value.bid_volume));
-                        //         $scope.$parent.fullasktotal += (!value.ask_volume ? 0 : parseInt(value.ask_volume));
-                        //     });
-                        //     $scope.$parent.fullbidperc = ($scope.$parent.fullbidtotal / ($scope.$parent.fullbidtotal + $scope.$parent.fullasktotal)) * 100;
-                        //     $scope.$parent.fullaskperc = ($scope.$parent.fullasktotal / ($scope.$parent.fullbidtotal + $scope.$parent.fullasktotal)) * 100;
-
-                            
-
                             
                         // });
                         // socket.emit('stock', symbol, function(data) {

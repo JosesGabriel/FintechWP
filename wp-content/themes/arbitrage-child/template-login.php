7a126eba030f89c1a1c58479209fcb9ba8009ff3
nick <nickgayo12@gmail.com>
2019-09-10 11:13:43 +0800

    show password on login

--- wp-content/themes/arbitrage-child/template-login.php
+++ wp-content/themes/arbitrage-child/template-login.php
@@ -770,7 +770,7 @@ div#um-shortcode-social-133 a.um-button.um-button-social i {
     font-size: 22px;
 }
 .um-col-alt-b {
-    display: none;
+    display: none !important;
 }
 .contercontrol {display:none !important;}
 

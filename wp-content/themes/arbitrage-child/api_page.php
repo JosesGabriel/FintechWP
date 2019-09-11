1cbdc8bcfaa19ddc135bcf70dc03c792ffda1ca7
nick <nickgayo12@gmail.com>
2019-09-11 18:24:40 +0800

    Password confirmation

--- wp-content/themes/arbitrage-child/api_page.php
+++ wp-content/themes/arbitrage-child/api_page.php
@@ -419,17 +419,17 @@
 		$subject = 'Password Reset Confirmation';
 		$message = '
 		<div class="container" style="width: 100%; font-family: "Roboto", sans-serif; color: #142c46;">
-		<div class="em-head" style="padding: 23px 0px 23px 31px; background-color: #142c46; background-image: url("https://arbitrage.ph/email-templates/email-template/lines.png"); background-position: 5vh 34%; background-size: 103%;"><img class="arbi-logo" style="width: 24%;" src="https://arbitrage.ph/email-templates/email-template/logo.png" /></div>
+		<div class="em-head" style="padding: 23px 0px 23px 31px; background-color: #142c46; background-image: url("'.$homeurlgen.'/email-templates/email-template/lines.png"); background-position: 5vh 34%; background-size: 103%;"><img class="arbi-logo" style="width: 24%;" src="'.$homeurlgen.'/email-templates/email-template/logo.png" /></div>
 		<div class="site-name" style="text-align: right; font-size: 15px; float: right; margin: 20px 30px 0px 0px;color: #142c46;">Arbitrage <a class="trade-btn" style="color: #142c46; cursor: pointer; padding: 3px 9px; border-radius: 20px; border: 3px solid #142c46;">Trade now </a></div>
 		<div class="em-container" style="-webkit-box-shadow: 0px 2px 8px -2px rgba(0,0,0,0.53); -moz-box-shadow: 0px 2px 8px -2px rgba(0,0,0,0.53); box-shadow: 0px 2px 8px -2px rgba(0,0,0,0.53);">
 			<div class="em-content-handler">
 				<div class="em-content-center" style="margin: 0 39px; padding-bottom: 45px; padding-top: 67px;">
 					<div class="em-content-topic" style="text-align: left; font-size: 25px; font-weight: 500; margin-top: 0px; margin-bottom: 53px; line-height: 33px;">We received a request to reset the password for your account. Your new password is indicated below.</div>
 					<a class="login-prime-btn" style="border-bottom: 3px solid #142c46; padding: 10px 0; color: #142c46; font-size: 18px; border-radius: 0; text-decoration: none !important; font-weight: 500;" href="'.$homeurlgen.'">'. $passgen .'</a>
-					<div class="em-content-body" style="text-align: left; margin-top: 47px; font-size: 15px; line-height: 20px; font-weight: 500;">If you didnt make this request, ignore this email or <a>report it to us.</a></div>
-					<div class="greet-container" style="font-weight: 500;">
-						<div class="em-greet sss" style="margin-top: 52px; font-size: 15px;">Thank you!</div>
-						<div class="em-greet-name" style="font-size: 15px;">The <a href="'.$homeurlgen.'">Arbitrage</a> Team</div>
+					<div class="em-content-body" style="text-align: left; margin-top: 47px; font-size: 15px; line-height: 20px; font-weight: 500;color: #142c46;">If you didnt make this request, ignore this email or <a>report it to us.</a></div>
+					<div class="greet-container" style="font-weight: 500;color: #142c46;">
+						<div class="em-greet sss" style="margin-top: 52px; font-size: 15px;color: #142c46;">Thank you!</div>
+						<div class="em-greet-name" style="font-size: 15px;color: #142c46;">The <a href="'.$homeurlgen.'">Arbitrage</a> Team</div>
 					</div>
 				</div>
 			</div>

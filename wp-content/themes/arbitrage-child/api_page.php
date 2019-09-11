53b1d627371f928eddcd1f6ff72524920e93b802
nick <nickgayo12@gmail.com>
2019-09-11 12:06:00 +0800

    Password confirmation

--- wp-content/themes/arbitrage-child/api_page.php
+++ wp-content/themes/arbitrage-child/api_page.php
@@ -393,22 +393,22 @@
     }elseif(isset($_GET['daction']) && $_GET['daction'] == 'email_pass_reset'){
 		global $wpdb;
 		$emailstr = stripslashes($_GET['email']);
-		// echo $emailstr;
-
-		return json_encode($emailstr);
+		echo $emailstr;
+		// return json_encode($emailstr);
 
 		// Search if email is existing
 		$checkQuery = "SELECT * FROM arby_users WHERE user_email like '$emailstr'";
 		$exist = $wpdb->query($addQuery);
 
 		// create random temp password
-		// $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-		// $pass = array(); //remember to declare $pass as an array
-		// $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-		// for ($i = 0; $i < 8; $i++) {
-		// 	$n = rand(0, $alphaLength);
-		// 	$pass[] = $alphabet[$n];
-		// }
+		$alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+		$pass = array(); //remember to declare $pass as an array
+		$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+		for ($i = 0; $i < 8; $i++) {
+			$n = rand(0, $alphaLength);
+			$pass[] = $alphabet[$n];
+		}
+		echo $pass;
 		// return json_encode($pass);
 
 		// update users password to new temp password

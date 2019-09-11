6b6649bdc18c4ed3cc5275969c3bf3f70c4b2ae1
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-11 19:24:01 +0800

    Update sidebar-topgainerslosers.php

--- wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
@@ -17,7 +17,7 @@
 
         foreach($stockinfo as $stkey => $stvals){
 
-            //$time[$i] = $stvals->lastupdatetime;
+            $time = $stvals->lastupdatetime;
 
             $stock[$i][0] = $stvals->symbol;
             $stock[$i][1] = $stvals->changepercentage;
@@ -40,7 +40,11 @@
                        <?php for($j=0; $j < 5; $j++) {?> 
                             <li class="odd">
                                 <span><?php echo $stock[$j][0]; ?></span>
+
                                 <a href="#"><?php echo $stock[$j][2]; ?><br><p style="color: #53b987 !important;"><?php echo number_format($stock[$j][1], 2, '.', ','); ?>%</p></a>
+
+                                <a href="#"><?php echo $stock[$j][2]; ?><br><p style="color: #53b987 !important;"><?php echo number_format($stock[$j][1], 2, '.', ','); ?>%</p></a>
+
                             </li>
                         <?php } ?>
             </ul>

86407fad7c82027e8e989ded5a4ed80c27a72aa9
nick <nickgayo12@gmail.com>
2019-09-11 11:55:16 +0800

    Password confirmation

--- wp-content/themes/arbitrage-child/api_page.php
+++ wp-content/themes/arbitrage-child/api_page.php
@@ -402,17 +402,17 @@
 		$exist = $wpdb->query($addQuery);
 
 		// create random temp password
-		$alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-		$pass = array(); //remember to declare $pass as an array
-		$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-		for ($i = 0; $i < 8; $i++) {
-			$n = rand(0, $alphaLength);
-			$pass[] = $alphabet[$n];
-		}
-		return json_encode($pass);
+		// $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+		// $pass = array(); //remember to declare $pass as an array
+		// $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+		// for ($i = 0; $i < 8; $i++) {
+		// 	$n = rand(0, $alphaLength);
+		// 	$pass[] = $alphabet[$n];
+		// }
+		// return json_encode($pass);
 
 		// update users password to new temp password
-		$updatepass = "UPDATE arby_users SET user_pass = '$pass' WHERE user_email = '$emailstr'";
+		// $updatepass = "UPDATE arby_users SET user_pass = '$pass' WHERE user_email = '$emailstr'";
 
 		// send email include all created credentials
 

0850f700a6c8845b230e21a0a9a59a501693e149
nick <nickgayo12@gmail.com>
2019-09-11 12:02:46 +0800

    Password confirmation

--- wp-content/themes/arbitrage-child/api_page.php
+++ wp-content/themes/arbitrage-child/api_page.php
@@ -393,7 +393,7 @@
     }elseif(isset($_GET['daction']) && $_GET['daction'] == 'email_pass_reset'){
 		global $wpdb;
 		$emailstr = stripslashes($_GET['email']);
-		echo $emailstr;
+		// echo $emailstr;
 
 		return json_encode($emailstr);
 

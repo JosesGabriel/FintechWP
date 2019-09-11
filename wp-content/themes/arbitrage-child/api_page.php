a87b7b1a8cfa822ff264e4ebb2073a2bb2464c1b
arphie <e3arphie@gmail.com>
2019-09-11 16:00:41 +0800

    R&R 23

--- wp-content/themes/arbitrage-child/api_page.php
+++ wp-content/themes/arbitrage-child/api_page.php
@@ -460,7 +460,8 @@
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_URL, 'https://data-api.arbitrage.ph/api/v1/stocks/list');
 		// curl_setopt($curl, CURLOPT_RESOLVE, ['data-api.arbitrage.ph:443:104.25.248.104']);
-		curl_setopt($curl, CURLOPT_RESOLVE, ['data-api.arbitrage.ph:443:104.199.140.243']);
+		// curl_setopt($curl, CURLOPT_RESOLVE, ['data-api.arbitrage.ph:443:104.199.140.243']);
+
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		$gerdqoute = curl_exec($curl);
 		curl_close($curl);

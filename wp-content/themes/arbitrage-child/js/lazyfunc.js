024b079e4449d43ab15b87325988e5c2c970143e
arphie <e3arphie@gmail.com>
2019-09-11 17:00:56 +0800

    R&R 30

--- wp-content/themes/arbitrage-child/js/lazyfunc.js
+++ wp-content/themes/arbitrage-child/js/lazyfunc.js
@@ -7,6 +7,19 @@ $( document ).ready(function() {
             dataType: 'json', // added data type
             success: function(res) {
                 console.log(res);
+                $.each(res, function( index, value ) {
+                    $toappend = '<li class="even '+index+'">';
+                    $toappend += '<span>'+value.stock+'</span>';
+                    $toappend += '<a href="#">'+value.stnamename+'<br>';
+                    $toappend += '<p>'+value.following+' Hits</p></a>';
+                    $toappend += '</li>';
+
+                    if(index < 6){
+                        $("ul.trendingme").append($toappend);
+                    } else {
+                        $("ul.trendingme > .trend-content-hidden").append($toappend);
+                    }
+                });
             },
             error: function (xhr, ajaxOptions, thrownError) {
                 console.log(xhr.status);

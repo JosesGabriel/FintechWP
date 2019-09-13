a5a793634b0989c0767cf9681fedefdbaf09c86c
Ralph Tolipas <ralph_tolipas@Ralph-Mac.local>
2019-09-13 13:15:12 +0800

    update

--- wp-content/themes/arbitrage-child/template-ticker.php
+++ wp-content/themes/arbitrage-child/template-ticker.php
@@ -583,7 +583,7 @@
 				});
 			}
 			function foreverticker() {
-                console.log('working on it..');
+                console.log('working..');
 				jQuery('.arb_custom_ticker').animate({'width': '+=100px'}, 500, "linear", function() {
 					forevertickerinit();
 				});

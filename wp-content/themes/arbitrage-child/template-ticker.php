0218224bbdbe58eb229d57795af705303870a476
Ralph Tolipas <ralph_tolipas@Ralph-Mac.local>
2019-09-14 00:58:36 +0800

    update

--- wp-content/themes/arbitrage-child/template-ticker.php
+++ wp-content/themes/arbitrage-child/template-ticker.php
@@ -631,9 +631,9 @@
 <body>
 <div class="arb_top_ticker">
     <div ng-controller="ticker" class="sd_border_btm arb_custom_ticker_wrapper">
-    <ul class="list-inline">
+    <ul class="list-inline marqueethis">
             <li ng-repeat="transaction in ticker">
-                {{transaction.price}}
+                 {{::transaction.price}}
             </li>
         </ul>
     </div>

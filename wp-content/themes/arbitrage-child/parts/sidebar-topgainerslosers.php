767a88ce0610b3e9574a8929c9487234f437f736
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-14 10:33:49 +0800

    Update sidebar-topgainerslosers.php

--- wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
@@ -75,7 +75,7 @@
                                         <li class="odd">
                                             <span><?php echo $stock[$j][0]; ?></span>
 
-                                            <a href="#"><?php echo $stock[$j][2]; ?><br><p style="color: #53b987 !important;"><?php echo number_format($stock[$j][1], 2, '.', ','); ?>%</p></a>
+                                            <a href="#"><?php echo $stock[$j][2]; ?><br><p style="color: #53b987 !important;"><?php echo number_format($stock[$j][1], 2, '.', ','); ?>%</p><p>test</p></a>
 
                                         </li>
 

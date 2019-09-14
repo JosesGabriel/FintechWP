a1cbffdb3c1b76ee277ef5fdc0d0d4a864415c3a
nick <nickgayo12@gmail.com>
2019-09-15 01:58:28 +0800

    desc sorting top stocks chart

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -2122,7 +2122,7 @@ if($issampledata){
 																									],
 																								];
 																							}
-																							krsort($finalwinning);
+																							arsort($finalwinning);
                                                                                             $dwinning = '';
                                                                                             $intowinchartbands = '';
                                                                                             $intowinchartlabels = '';

852499b2e07f1899787443219286a56a38dc9a7e
nick <nickgayo12@gmail.com>
2019-09-14 14:39:31 +0800

    no message/message

--- wp-content/themes/arbitrage-child/template-notification.php
+++ wp-content/themes/arbitrage-child/template-notification.php
@@ -42,7 +42,7 @@ date_default_timezone_set('Asia/Manila');
 <script>
     $(document).ready(function(){
         $('.um-notifications-none').show();
-        $('.um-notifications-none').appendTo('.um-notification-ajax');
+        $('.um-notifications-none').appendTo('.um-notification-ajax').remove();
         var hassclass = $('.um-notification-ajax div').hasClass('read');
         if( hassclass ) {
             $('.um-notifications-none').hide();

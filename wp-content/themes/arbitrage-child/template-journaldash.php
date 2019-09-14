e0a28a19d1eb0d28fe1b67cb4fe77615bf4bdf93
arphie <e3arphie@gmail.com>
2019-09-14 13:49:23 +0800

    R&R 3-14

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -546,16 +546,16 @@ if($issampledata){
 		$currentaloccolor = '"#f44235",';
 		if ($dtradeingfo) {
 			foreach ($dtradeingfo as $trinfokey => $trinfovalue) {
-				print_r($trinfovalue);
+				// print_r($trinfovalue);
 				$key = array_search(strtoupper($trinfovalue['stockname']), array_column($gerdqoute->data, 'symbol'));
 				$stockdetails = $gerdqoute->data[$key];
 				$dstockinfo = $stockdetails;
-				$marketval = $dstockinfo->last * $dstocktraded['totalstock'];
+				$marketval = $dstockinfo->last * $trinfovalue['totalstock'];
 				$dsellfees = getjurfees($marketval, 'sell');
 				$dtotal = $marketval - $dsellfees;
 	
 				$dequityp += $dtotal;
-				$currentalocinfo .= '{"category" : "'.$trinfovalue['stockname'].'", "column-1" : "'.number_format($dstocktraded['totalstock'], 2, '.', '').'"},';
+				$currentalocinfo .= '{"category" : "'.$trinfovalue['stockname'].'", "column-1" : "'.number_format($dtotal, 2, '.', '').'"},';
 				$currentaloccolor .= '"'.$aloccolors[$trinfokey + 1].'",';
 			}
 		}

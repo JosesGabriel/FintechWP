de18738dc35402c815147c0fb70dd5e52c961fb3
nick <nickgayo12@gmail.com>
2019-09-15 02:43:09 +0800

    desc sorting top stocks chart

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -2059,7 +2059,7 @@ if($issampledata){
                                                         // usort($iswinstocks, 'sortprofits');
                                                         $totalwin = 0;
 														$finalwinning = [];
-														krsort($finalwinning);
+														ksort($finalwinning);
                                                         foreach ($iswinstocks as $pxwinkey => $pxwinvalue) {
                                                             array_push($finalwinning, $pxwinvalue);
                                                             $totalwin += $pxwinvalue['dprofit'];
@@ -2070,7 +2070,7 @@ if($issampledata){
                                                         // usort($finalwinning, 'winningsort');
                                                         $finalloss = [];
 														$totalloss = 0;
-														arsort($finalwinning);
+														asort($finalwinning);
                                                         foreach ($islossstocks as $pxlosskey => $pxlossvalue) {
                                                             array_push($finalloss, $pxlossvalue);
                                                             $totalloss += $pxlossvalue['dprofit'];

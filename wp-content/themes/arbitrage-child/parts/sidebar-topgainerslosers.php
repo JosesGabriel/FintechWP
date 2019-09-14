beccd33ca80bdd59d3050d11d4bcfd5533676108
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-14 10:35:59 +0800

    Update sidebar-topgainerslosers.php

--- wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
@@ -75,7 +75,7 @@
                                         <li class="odd">
                                             <span><?php echo $stock[$j][0]; ?></span>
 
-                                            <a href="#"><?php echo $stock[$j][2]; ?><br><p style="color: #53b987 !important;"><?php echo number_format($stock[$j][1], 2, '.', ','); ?>%</p><p>test</p></a>
+                                            <a href="#"><?php echo $stock[$j][2]; ?><br><p style="color: #53b987 !important;"><?php echo number_format($stock[$j][1], 2, '.', ','); ?>%</p></a>
 
                                         </li>
 
@@ -84,7 +84,7 @@
                                         <li class="odd">
                                             <span><?php echo $stocky[$j][0]; ?></span>
 
-                                            <a href="#"><?php echo $stocky[$j][2]; ?><br><p style="color: #53b987 !important;"><?php echo number_format($stocky[$j][1], 2, '.', ','); ?>%</p></a>
+                                            <a href="#"><?php echo $stocky[$j][2]; ?><br><p style="color: #53b987 !important;"><?php echo number_format($stocky[$j][1], 2, '.', ','); ?>%</p><p>test</p></a>
 
                                         </li>
 

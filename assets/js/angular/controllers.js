9f266c0d163bfbc7492bc8ac20e3aa3963626327
Ralph Tolipas <ralph_tolipas@Ralph-Mac.local>
2019-09-14 18:14:52 +0800

    update

--- assets/js/angular/controllers.js
+++ assets/js/angular/controllers.js
@@ -65,26 +65,28 @@ app.controller('ticker', ['$scope','$filter', '$http', function($scope, $filter,
     $scope.ticker = [];
     
     var transaction = [
-             {symbol:"AC", price:"32.2", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
-             {symbol:"AC", price:"25.70", change:3, shares:"12.3"},
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
+             {"sym":"AC","ex":"PSE","prv":price_format(909.5),"o":913,"c":0,"chg":909.5,"chgpc":90950,"l":906.5,"h":913,"avg":909.6758,"vol":abbr_format(87080),"val":79214570,"tr":827,"t":1568433166}
         ]
 
         for (i in transaction){

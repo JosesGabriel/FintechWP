6e2733c69729452966abc9d74dd0714cbfffc294
nick <nickgayo12@gmail.com>
2019-09-15 16:45:44 +0800

    fix other charts in top stocks

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -2166,7 +2166,7 @@ if($issampledata){
                                                                                                 $intowinchartbands .= '},';
 
                                                                                                 $intowinchartlabels .= '{';
-                                                                                                $intowinchartlabels .= '"text": "'. ($fwinvalue['dstock'] >= 3 ? '' : $fwinvalue['dstock']).'",';
+                                                                                                $intowinchartlabels .= '"text": "'. ($fwinvalue['dstock'] >= 2 ? ' ' : $fwinvalue['dstock']).'",';
                                                                                                 $intowinchartlabels .= '"x": "49%",';
                                                                                                 $intowinchartlabels .= '"y": "'.($fwinkey == 2 ? '6.5' : ($fwinkey == 1 ? '13.4' : ($flosskey == 0 ? '20' : '33'))).'%",';
                                                                                                 $intowinchartlabels .= '"size": 11,';
@@ -2226,7 +2226,7 @@ if($issampledata){
 																								$intolosschartbands .= '},';
 																								
 																								$intolosschartlabels .= '{';
-																								$intolosschartlabels .= '"text": "'. ($flossvalue['dstock'] >= 3 ? '' : $flossvalue['dstock']).'",';
+																								$intolosschartlabels .= '"text": "'. ($flossvalue['dstock'] >= 3 ? '' : $flossvalue['dstock']) .'",';
 																								$intolosschartlabels .= '"x": "49%",';
 																								$intolosschartlabels .= '"y": "'.($flosskey == 0 ? '6.5' : ($flosskey == 1 ? '13.4' : ($flosskey == 2 ? '20' : '33'))).'%",';
 																								$intolosschartlabels .= '"size": 11,';

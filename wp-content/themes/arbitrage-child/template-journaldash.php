0bb428e1c5425c5b0f87f19012f564c9e3961e66
nick <nickgayo12@gmail.com>
2019-09-15 16:15:44 +0800

    fix other charts in top stocks

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -2158,9 +2158,9 @@ if($issampledata){
                                                                                                 $intowinchartbands .= '},';
 
                                                                                                 $intowinchartlabels .= '{';
-                                                                                                $intowinchartlabels .= '"text": "'.$fwinvalue['dstock'].'",';
+                                                                                                $intowinchartlabels .= '"text": "'. ($fwinvalue['dstock'] >= 3 ? '' : $fwinvalue['dstock']).'",';
                                                                                                 $intowinchartlabels .= '"x": "49%",';
-                                                                                                $intowinchartlabels .= '"y": "'.($fwinkey == 2 ? '6.5' : ($fwinkey == 1 ? '13.4' : ($fwinkey == 0 ? '20' : '33'))).'%",';
+                                                                                                $intowinchartlabels .= '"y": "'.($fwinkey == 2 ? '6.5' : ($fwinkey == 1 ? '13.4' : ($flosskey == 0 ? '20' : '33'))).'%",';
                                                                                                 $intowinchartlabels .= '"size": 11,';
                                                                                                 $intowinchartlabels .= '"bold": false,';
                                                                                                 $intowinchartlabels .= '"color": "#d8d8d8",';
@@ -2218,7 +2218,7 @@ if($issampledata){
 																								$intolosschartbands .= '},';
 																								
 																								$intolosschartlabels .= '{';
-																								$intolosschartlabels .= '"text": "'.$flossvalue['dstock'].'",';
+																								$intolosschartlabels .= '"text": "'. ($fwinvalue['dstock'] >= 3 ? '' : $fwinvalue['dstock']).'",';
 																								$intolosschartlabels .= '"x": "49%",';
 																								$intolosschartlabels .= '"y": "'.($flosskey == 0 ? '6.5' : ($flosskey == 1 ? '13.4' : ($flosskey == 2 ? '20' : '33'))).'%",';
 																								$intolosschartlabels .= '"size": 11,';

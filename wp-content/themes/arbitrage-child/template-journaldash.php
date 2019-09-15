64853807c875681570d582397a86c5791e2e9ca1
arphie <e3arphie@gmail.com>
2019-09-16 00:15:59 +0800

    R&R 3-59

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -749,15 +749,15 @@ if($issampledata){
 																							<div class="groupinput midd lockedd"><label>Buy Power</label>
 																							<input type="text" name="input_buy_product" id="input_buy_product" class="number" step="0.01" style="margin-left: -4px;" value="<?php echo number_format($buypower, 2, '.', ','); ?>" readonly>
 																							<i class="fa fa-lock" aria-hidden="true"></i></div>
-																							<div class="groupinput midd"><label>Buy Price</label><input type="text" id="enterdataprice" name="inpt_data_price" class="textfield-buyprice number" required></div>
-																							<div class="groupinput midd"><label>Quantity</label><input type="text" id="enterdataquantity" name="inpt_data_qty" class="textfield-quantity number" required></div>
+																							<div class="groupinput midd"><label>Buy Price</label><input type="text" id="entertopdataprice" name="inpt_data_price" class="textfield-buyprice number" required></div>
+																							<div class="groupinput midd"><label>Quantity</label><input type="text" id="entertopdataquantity" name="inpt_data_qty" class="textfield-quantity number" required></div>
 																							<div class="groupinput midd lockedd"><label>Total Price</label><input readonly="" type="text" class="number" name="inpt_data_total_price" value=""><i class="fa fa-lock" aria-hidden="true"></i></div>
 																						</div>
 																						<div class="entr_col">
 																							<div class="groupinput midd lockedd"><label>Curr. Price</label><input readonly type="text" name="inpt_data_currprice" value=""><i class="fa fa-lock" aria-hidden="true"></i></div>
 																							<div class="groupinput midd lockedd"><label>Change</label><input readonly type="text" name="inpt_data_change" value="%"><i class="fa fa-lock" aria-hidden="true"></i></div>
 																							<div class="groupinput midd lockedd"><label>Open</label><input readonly type="text" name="inpt_data_open" value=""><i class="fa fa-lock" aria-hidden="true"></i></div>
-																							<div class="groupinput midd lockedd"><label>Low</label><input readonly type="text" name="inpt_data_low" value=""><i class="fa fa-lock" aria-hidden="true"></i></div>
+																							<div class="groupinput midd lockedd"><label>Low</label><input readonly type="text" name="inpt_data_low	" value=""><i class="fa fa-lock" aria-hidden="true"></i></div>
 																							<div class="groupinput midd lockedd"><label>High</label><input readonly type="text" name="inpt_data_high" value=""><i class="fa fa-lock" aria-hidden="true"></i></div>
 																						</div>
 																						<div class="entr_col">
@@ -3707,10 +3707,10 @@ if($issampledata){
 		});
 
 		// calculate total price
-		jQuery(document).on('keyup', 'input[name="inpt_data_price"], input[name="inpt_data_qty"]', function (e) {
-			let price = jQuery('input[name="inpt_data_price"]').val().replace(/,/g, '');
+		jQuery(document).on('keyup', '#entertopdataprice, #entertopdataquantity', function (e) {
+			let price = jQuery('#entertopdataprice').val().replace(/,/g, '');
 			// let quantity = jQuery('input[name="inpt_data_qty"]').val().replace(/,/g, '');
-			let quantity = jQuery('input[name="inpt_data_qty"]').val();
+			let quantity = jQuery('#entertopdataquantity').val();
 			console.log(price + " ~ " + quantity);
 			let total_price = parseFloat(price) * Math.trunc(quantity);
 			

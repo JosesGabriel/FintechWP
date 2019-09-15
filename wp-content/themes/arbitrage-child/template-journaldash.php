3ab15d877eab026da214a949f75c3ba0275ef14c
nick <nickgayo12@gmail.com>
2019-09-15 12:22:22 +0800

    fix other charts in top stocks

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -2144,7 +2144,7 @@ if($issampledata){
 																								// { "color": "#00e676", "startValue": 0, "endValue": 90, "radius": "100%", "innerRadius": "85%", "balloonText": "90%"},';
 
                                                                                                 $intowinchartbands .= '{';
-                                                                                                $intowinchartbands .= '"color": "#ffffff",';
+                                                                                                $intowinchartbands .= ' "color": "'.($fwinkey == 0 ? '#2c3e51' : ($fwinkey == 1 ? '#0c1e34' : ($fwinkey == 2 ? '#182a3f' : ''))).'",';
                                                                                                 $intowinchartbands .= '"startValue": 0,';
                                                                                                 $intowinchartbands .= '"endValue": "100%",';
                                                                                                 $intowinchartbands .= '"radius": "'.($fwinkey == 0 ? '100' : ($fwinkey == 1 ? '85' : ($fwinkey == 2 ? '65' : ''))).'%",';

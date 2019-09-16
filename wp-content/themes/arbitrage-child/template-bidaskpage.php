e94476c7c582cd04ec30b1ee6e841a95de03bbec
Ralph Tolipas <ralph_tolipas@Ralph-Mac.local>
2019-09-16 16:10:36 +0800

    update

--- wp-content/themes/arbitrage-child/template-bidaskpage.php
+++ wp-content/themes/arbitrage-child/template-bidaskpage.php
@@ -65,36 +65,50 @@
 
 </head>
 <body>
-    <table class="table table-bordered table-sm">
-    <thead>
-        <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        </tr>
-        <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        </tr>
-        <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
-        </tr>
-    </tbody>
-    </table>
+
+<div class="container">
+  <div class="row">
+    <!-- Bid -->
+    <div class="col-sm">
+        <table class="table table-sm table-bordered">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Volume</th>
+            <th scope="col">Bid</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <!-- Ask -->
+    <div class="col-sm">
+        <table class="table table-sm table-bordered">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Volume</th>
+            <th scope="col">Bid</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+  </div>
+</div>
+    
 
 	<!-- end page container -->
 	<!-- ================== BEGIN BASE JS ================== -->

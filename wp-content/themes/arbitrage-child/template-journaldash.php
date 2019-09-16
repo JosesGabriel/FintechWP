faaa7b3fe79ca78f6815a9d28c5362f1b836c128
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-09-16 15:56:48 +0800

    required date on journal

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -3811,7 +3811,7 @@ if($issampledata){
 			var dbuypower = parseFloat($(".dentertrade #input_buy_product").val().replace(/,/g, ''));
 			var total_price = parseFloat(jQuery('input[name="inpt_data_total_price"]').val().replace(/,/g, ''));
 			var buySell__date = jQuery('#journal__trade-btn--date-picker').val();
-			if(dstock != "" && dbuypower > 0 && total_price < dbuypower){
+			if(dstock != "" && dbuypower > 0 && total_price < dbuypower && buySell__date != ""){
 				jQuery(".dentertrade").submit();
 			} else if (buySell__date == "") {
 				swal('Date is required.');

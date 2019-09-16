72463c18b5bd0f2f2d32ba4d2d26ff6ef21a8b78
nick <nickgayo12@gmail.com>
2019-09-16 13:22:46 +0800

    add separator numbers

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -3811,17 +3811,27 @@ if($issampledata){
 			console.log(total_price + " ~ " + thetradefees(total_price, 'buy'));
 
 			let finaltotal = parseFloat(total_price) + parseFloat(thetradefees(total_price, 'buy'));
-			jQuery('input[name="inpt_data_total_price"]').val(replaceCommas(finaltotal));
+			var currentVal = finaltotal;
+            var testDecimal = testDecimals(currentVal);
+            if (testDecimal.length > 1) {
+                currentVal = currentVal.slice(0, -1);
+            }
+			jQuery('input[name="inpt_data_total_price"]').val(replaceCommas(currentVal));
 
+			function testDecimals(currentVal) {
+				var count;
+				currentVal.match(/\./g) === null ? count = 0 : count = currentVal.match(/\./g);
+				return count;
+			}
 			function replaceCommas(yourNumber) {
-            var components = yourNumber.toString().split(".");
-            if (components.length === 1) 
-                components[0] = yourNumber;
-            components[0] = components[0].replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-            if (components.length === 2)
-                components[1] = components[1].replace(/\D/g, "");
-            return components.join(".");
-        }
+				var components = yourNumber.toString().split(".");
+				if (components.length === 1) 
+					components[0] = yourNumber;
+				components[0] = components[0].replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+				if (components.length === 2)
+					components[1] = components[1].replace(/\D/g, "");
+				return components.join(".");
+			}
 		});
 
 		// jQuery("")

d41d081495816ba6730cc0b2d577e55286dce13f
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-16 10:35:37 +0800

    Update template-journaldash.php

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -3099,7 +3099,7 @@ if($issampledata){
                                                                                             <div class="trdleft">
                                                                                                 <div class="onelnetrd"><span class="modal-notes-ftitle"><strong>Strategy:</strong></span> 
                                                                                                         
-                                                                                                        <select class="rnd" name="" id="" class="selecteditlog">
+                                                                                                        <select class="rnd selecteditlog" name="" id="">
                                                                                                             <option  value="Bottom Picking">Bottom Picking</option>
                                                                                                             <option  value="Breakout Play">Breakout Play</option>
                                                                                                             <option  value="Trend Following">Trend Following</option>

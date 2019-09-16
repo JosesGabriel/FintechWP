a0cf7fee51c5cca5fd7c120e5166d5ebded6a94d
Ralph Tolipas <ralph_tolipas@Ralph-Mac.local>
2019-09-16 11:44:58 +0800

    update

--- wp-content/themes/arbitrage-child/template-bidaskpage.php
+++ wp-content/themes/arbitrage-child/template-bidaskpage.php
@@ -33,6 +33,7 @@
     <style type="text/css">
 
     </style>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
     <script>
         jQuery(document).ready(function() {
             console.log('load all');

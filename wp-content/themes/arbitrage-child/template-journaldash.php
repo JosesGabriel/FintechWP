58721049a6725cac2a22a55b32cced66c49efbf1
arphie <e3arphie@gmail.com>
2019-09-17 20:09:58 +0800

    R&R 5-105

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -3345,7 +3345,7 @@ if($issampledata){
 			let quantity = jQuery('input[name="inpt_data_qty_sold"]').val().replace(/,/g, '');
 
 			let totalmarket = parseFloat(price) * parseFloat(quantity);
-			let finalcost = totalmarket + parseFloat(thetradefees(totalmarket, 'sell'));
+			let finalcost = totalmarket - parseFloat(thetradefees(totalmarket, 'sell'));
 			// console.log(finalcost.toFixed(2));
 			if(!isNaN(finalcost)){
 				jQuery('input[name="inpt_data_total_sold_price"]').val(finalcost.toFixed(2));

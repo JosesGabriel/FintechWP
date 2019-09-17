9eae6564f3955a53894ee17f0d7ad271776b504d
Emmanuel Tierra <emman.tierra@gmail.com>
2019-09-17 06:56:59 +0000

    Hotfix/google ads

--- wp-content/themes/arbitrage-child/header.php
+++ wp-content/themes/arbitrage-child/header.php
@@ -10,6 +10,15 @@
 
 	gtag('config', 'UA-147416476-1');
 	</script>
+
+	<!-- Google Ads -->
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script>
+		(adsbygoogle = window.adsbygoogle || []).push({
+		google_ad_client: "ca-pub-4838120237791146",
+		enable_page_level_ads: true
+		});
+	</script>
 	
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta property="og:title" content="Arbitrage | Free Stock Trading Platform" />

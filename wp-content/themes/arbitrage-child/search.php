5186bc84bc73f34e722b7b503ff251cc995cc1ad
nick <nickgayo12@gmail.com>
2019-09-17 20:00:23 +0800

    Search page refactor

--- wp-content/themes/arbitrage-child/search.php
+++ wp-content/themes/arbitrage-child/search.php
@@ -832,7 +832,6 @@ $dstockinfo = $gerdqoute->data;
                                                                                             <div class="onbfdata"> <?php echo $value['followers']; ?> Connections </div>
                                                                                             <div class="onbfollow">
                                                                                                 <?php echo UM()->Friends_API()->api()->friend_button( $value['id'], $user->ID ); ?>
-                                                                                                <a href="#" id="mingle-btn" class="mingle-btn um-button um-alt" data-user_id1="<?php echo $value['id']; ?>" data-user_id2="<?php echo $user->ID; ?>">Mingle</a>
                                                                                             </div>
                                                                                         </div>
                                                                                     </div>

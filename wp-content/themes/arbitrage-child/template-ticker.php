907582d2af073414834024b51ca47a9f2642255b
Ralph Tolipas <ralph_tolipas@Ralph-Mac.local>
2019-09-18 08:05:59 +0800

    update

--- wp-content/themes/arbitrage-child/template-ticker.php
+++ wp-content/themes/arbitrage-child/template-ticker.php
@@ -580,12 +580,12 @@
 			
             forevertickerinit();
 			function forevertickerinit() {
-				jQuery('.arb_custom_ticker').animate({'width': '+=100px'}, 1500, "linear", function() {
+				jQuery('.arb_custom_ticker').animate({'width': '+=100px'}, 10000, "linear", function() {
 					foreverticker();
 				});
 			}
 			function foreverticker() {
-				jQuery('.arb_custom_ticker').animate({'width': '+=100px'}, 1500, "linear", function() {
+				jQuery('.arb_custom_ticker').animate({'width': '+=100px'}, 10000, "linear", function() {
 					forevertickerinit();
 				});
 			}

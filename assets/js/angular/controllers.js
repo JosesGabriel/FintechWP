cf91c6af34ffe676c7b841b1ec9c1bc1a108435c
Emmanuel Tierra <emman.tierra@gmail.com>
2019-09-18 10:56:10 +0800

    Enable socket for ticker

--- assets/js/angular/controllers.js
+++ assets/js/angular/controllers.js
@@ -107,27 +107,27 @@ app.controller('ticker', ['$scope','$filter', '$http', function($scope, $filter,
     $scope.ticker = [];
     var counter = 1;
 
-    // socket.on('psec', function (data) {  
-    //     var ctr = counter += 1;
-    //     var transaction = {
-    //         counter: ctr,
-    //         symbol: data.sym,
-    //         price:  price_format(data.prv),
-    //         change: data.chg,
-    //         shares: abbr_format(data.vol)
-    //     };
-    //     $scope.ticker.push(transaction);
-    //     ticker_data_ralph.push(transaction);
-    //     if ($scope.ticker.length > 150) {
-    //         $scope.ticker.pop();
-    //     }
+    socket.on('psec', function (data) {  
+        var ctr = counter += 1;
+        var transaction = {
+            counter: ctr,
+            symbol: data.sym,
+            price:  price_format(data.prv),
+            change: data.chg,
+            shares: abbr_format(data.vol)
+        };
+        $scope.ticker.push(transaction);
+        ticker_data_ralph.push(transaction);
+        if ($scope.ticker.length > 150) {
+            $scope.ticker.pop();
+        }
 
-    //     if (ticker_data_ralph.length > 150) {
-    //         ticker_data_ralph.pop();
-    //     }
-    //     $scope.$digest();
-    // });
-    // $scope.select = goToChart;
+        if (ticker_data_ralph.length > 150) {
+            ticker_data_ralph.pop();
+        }
+        $scope.$digest();
+    });
+    $scope.select = goToChart;
 }]);
 
 // app.controller('psei', function($scope, $http) {  

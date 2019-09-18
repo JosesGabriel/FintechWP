59f0dce44c808bf70cf6803bb0aaeb605e233688
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-18 13:03:24 +0800

    Update sidebar-topgainerslosers.php

--- wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
@@ -3,7 +3,7 @@
 <script>
     jQuery(document).ready( function() {
 
-        $.ajax({
+       /* $.ajax({
               type:'GET',
               url:'https://data-api.arbitrage.ph/api/v1/stocks/history/latest?exchange=PSE',
               dataType: 'json',
@@ -28,7 +28,7 @@
                 error: function(response) {
                     
                 }
-            });
+            });*/
      });
 
 </script>
@@ -43,17 +43,17 @@
         $response = curl_exec($curl);
         curl_close($curl);
 
-        $myArray = $_COOKIE['myArray'];
-        $myArray1 = json_decode($myArray);
-        $stockss = $myArray1->data;
-        print_r($stockss);
+       // $myArray = $_COOKIE['myArray'];
+        //$myArray1 = json_decode($myArray);
+        //$stockss = $myArray1->data;
+        //print_r($stockss);
         //$myArray1 = json_decode($myArray);
 
-         foreach($stockss as $stkey => $stockval){
+        // foreach($stockss as $stkey => $stockval){
 
-                echo $stockval->symbol;
+              //  echo $stockval->symbol;
 
-            }
+         //   }
 
 
         if ($response !== false) {
@@ -98,7 +98,7 @@
     <div class="top-stocks">
             <div class="to-top-title gainers-title"><strong>Top Gainers </strong></div>
 
-            <div><?php print_r($myArray->symbol); ?></div>
+           <!-- <div><?php //print_r($myArray->symbol); ?></div>-->
 
             <hr class="style14 style15" style="width: 90% !important;margin-bottom: 2px !important;margin-top: 6px !important;/* margin: 5px 0px !important; */">
             <div class="to-content-part gainers">

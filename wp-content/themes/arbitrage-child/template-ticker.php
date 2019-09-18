977e5ada368a09993779afce2d02afd71c4f48d4
Ralph Tolipas <ralph_tolipas@Ralph-Mac.local>
2019-09-18 09:50:11 +0800

    update

--- wp-content/themes/arbitrage-child/template-ticker.php
+++ wp-content/themes/arbitrage-child/template-ticker.php
@@ -597,7 +597,7 @@
                 for(i in ticker_data){
                     let ids = `li#${ticker_data[i].counter}`;
                     let element = jQuery(ids);
-                    element.remove();
+                    //element.remove();
                 }     
                 if(jQuery(".arb_custom_ticker").width()>25000){
                     location.reload(true);

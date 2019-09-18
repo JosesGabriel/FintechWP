b85b7b66ad6f049ccf66447451f7c044c6138974
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-18 12:36:46 +0800

    Update sidebar-topgainerslosers.php

--- wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
@@ -99,7 +99,7 @@
     <div class="top-stocks">
             <div class="to-top-title gainers-title"><strong>Top Gainers </strong></div>
 
-            <div><?php echo $myArray->data; ?></div>
+            <div><?php echo $myArray1; ?></div>
 
             <hr class="style14 style15" style="width: 90% !important;margin-bottom: 2px !important;margin-top: 6px !important;/* margin: 5px 0px !important; */">
             <div class="to-content-part gainers">

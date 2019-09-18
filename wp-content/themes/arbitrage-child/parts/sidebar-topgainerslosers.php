ccb569c458e198fcb3f1775154dabe841e3b2513
Rocky Gonzales <rocky@arbitrage.ph>
2019-09-18 15:45:35 +0800

    Update sidebar-topgainerslosers.php

--- wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-topgainerslosers.php
@@ -64,7 +64,6 @@
                     });
 
                     for (var i = 0; i < 5; i++) {
-                        console.log(stocks2[i][0] + ' - ' + stocks2[i][1].toFixed(2) + ' - ' + stocks2[i][3]);
 
                         var list = '<li class="odd"> <span>' + stocks2[i][0] + '</span>';
                             list += '<a href="#">' + stocks2[i][2] + '<br><p style="color: #53b987 !important;">' + stocks2[i][1].toFixed(2) + '%</p></a>';
@@ -72,11 +71,6 @@
 
                         jQuery(".gainers ul").append(list);
                     }
-
-
-                    stocks2.sort(function(a, b){
-                        return a[1] - b[1];
-                    });
 
                     
 

e00d3a2f8a975ccc51b2d9ea8871b03e8215f2af
Emmanuel Tierra <emman.tierra@gmail.com>
2019-09-19 11:58:17 +0800

    Remove status

--- wp-content/themes/arbitrage-child/template-charting.php
+++ wp-content/themes/arbitrage-child/template-charting.php
@@ -20,7 +20,7 @@ function respond($success = false, $data = [], $status = 500)
     $data['status'] = $success ? 'ok' : 'error';
     $status = $success ? 200 : $status;
     http_response_code($status);
-    echo json_encode(array_push($data, $status));
+    echo json_encode($data);
     die();
 }
 

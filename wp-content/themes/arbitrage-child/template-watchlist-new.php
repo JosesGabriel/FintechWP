c74450acb4d10af88f88f83db4278f4a1cbd3f85
arphie <e3arphie@gmail.com>
2019-09-24 12:20:15 +0800

    watchlist add error messages 1

--- wp-content/themes/arbitrage-child/template-watchlist-new.php
+++ wp-content/themes/arbitrage-child/template-watchlist-new.php
@@ -819,7 +819,7 @@ $watchinfo = get_user_meta('7', '_scrp_stocks_chart', true);
 					$("#add-watchlist-param").submit();
 					$(this).hide();
 				} else {
-					$('.addwatchtab .selectnotifitems').append('<div class="indicatorerror errorpop">You have add a value to atleast one indicator</div>');
+					$('.addwatchtab .selectnotifitems').prepend('<div class="indicatorerror errorpop">You have add a value to atleast one indicator</div>');
 					console.log("stop the submit");
 				}
 			} else {

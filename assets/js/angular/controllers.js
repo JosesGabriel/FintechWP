85332a817f6bca181ab688153e1a72871f059b4a
arphie <e3arphie@gmail.com>
2019-09-26 13:17:27 +0800

    sell button chart fix 35

--- assets/js/angular/controllers.js
+++ assets/js/angular/controllers.js
@@ -838,6 +838,7 @@ app.controller('tradingview', ['$scope','$filter', '$http', '$rootScope', functi
 
                     angular.element(".arb_sell").attr("data-stocksel",_symbol); //setter
                     console.log("outhere");
+                    angular.element("#confirmsellparts").hide();
 
                     $http({
                         method : "GET",
@@ -852,11 +853,13 @@ app.controller('tradingview', ['$scope','$filter', '$http', '$rootScope', functi
                         console.log(response.data);
                         if(response.data.status == "yes_stock"){
                             angular.element(".arb_sell").attr("data-hasstock","has_stock"); //setter
-
+                            
                             // add detail from api
                             angular.element("#tradelogs").val(response.data.data.tradelog);
                             angular.element("#sellvolume").val(response.data.data.volume);
                             angular.element("#sellavrprice").val(response.data.data.averageprice);
+
+                            angular.element("#confirmsellparts").show();
                         } else {
                             angular.element(".arb_sell").attr("data-hasstock","no_stock"); //setter
                         }

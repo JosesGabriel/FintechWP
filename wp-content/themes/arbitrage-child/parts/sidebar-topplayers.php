f7dfb6db9642c4aa005a52d5abc1a2dc7f19164b
Emmanuel Tierra <emman.tierra@gmail.com>
2019-09-30 13:17:56 +0800

    Remove unused code

--- wp-content/themes/arbitrage-child/parts/sidebar-topplayers.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-topplayers.php
@@ -1,25 +1,3 @@
-<?php
-
-    function nice_number($n) {
-        // first strip any formatting;
-        $n = (0+str_replace(",", "", $n));
-
-        // is this a number?
-        if (!is_numeric($n)) return false;
-
-        // now filter it;
-        if ($n > 1000000000000000) return round(($n/1000000000000000), 2).'Quad';
-        elseif ($n > 1000000000000) return round(($n/1000000000000), 2).'T';
-        elseif ($n > 1000000000) return round(($n/1000000000), 2).'B';
-        elseif ($n > 1000000) return round(($n/1000000), 2).'M';
-        elseif ($n > 1000) return round(($n/1000), 2).'K';
-
-        return number_format($n);
-    }
-
-
-?>
-
 <div class="dtanks">
 
 	<h5>Top Players</h5>

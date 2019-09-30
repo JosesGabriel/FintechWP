6f3251a61eec03b3cc540eb46f94832d0539dec9
Emmanuel Tierra <emman.tierra@gmail.com>
2019-09-30 15:28:44 +0800

    FIx Google Ads script

--- wp-content/themes/arbitrage-child/header-message.php
+++ wp-content/themes/arbitrage-child/header-message.php
@@ -28,7 +28,7 @@ $ismyprofile = ($user->ID == $profile_id ? true : false);
 	</script>
 
 	<!-- Google Ads -->
-    <script async src=“https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js“></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({
             google_ad_client: "ca-pub-7982031973729040",
--- wp-content/themes/arbitrage-child/template-emailpassreset.php
+++ wp-content/themes/arbitrage-child/template-emailpassreset.php
@@ -23,7 +23,7 @@ $homeurlgen = get_home_url();
     </script>
 
     <!-- Google Ads -->
-    <script async src=“https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js“></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({
             google_ad_client: "ca-pub-7982031973729040",

a49d6b2f1d89f32af3dd11d8d33e8f8237bc8649
arphie <e3arphie@gmail.com>
2019-09-30 10:27:19 +0800

    delete live porfolio 7

--- wp-content/themes/arbitrage-child/template-journaldash.php
+++ wp-content/themes/arbitrage-child/template-journaldash.php
@@ -1010,7 +1010,7 @@ if($issampledata){
 	$buypower = 0;
 	$initcapital = $dledger[0]->tranamount;
     foreach ($dledger as $getbuykey => $getbuyvalue) {
-        if ($getbuyvalue->trantype == 'deposit' || $getbuyvalue->trantype == 'selling' || $getbuyvalue->trantype == 'dividend' ) {
+        if ($getbuyvalue->trantype == 'deposit' || $getbuyvalue->trantype == 'selling' || $getbuyvalue->trantype == 'dividend' || $getbuyvalue->trantype == 'deleted_live') {
             $buypower = $buypower + $getbuyvalue->tranamount;
         } else {
             $buypower = $buypower - $getbuyvalue->tranamount;

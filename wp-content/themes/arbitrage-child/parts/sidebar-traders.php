fd1d12a352a7b9a3d45b27caabaf6d63244a1a08
Emmanuel Tierra <emman.tierra@gmail.com>
2019-09-30 13:08:34 +0800

    Remove commented code

--- wp-content/themes/arbitrage-child/parts/sidebar-traders.php
+++ wp-content/themes/arbitrage-child/parts/sidebar-traders.php
@@ -1,38 +1,3 @@
-<?php
-	// global $current_user;
-	// $user = wp_get_current_user();
-	// $userID = $current_user->ID;
-	// $topargs = array(
-	// 	'role'          =>  '',
-	// );
-	// $users = get_users($topargs);
-	// $newuserlist = array();
-	// foreach ($users as $key => $value) {
-
-	// 	if (!UM()->Friends_API()->api()->is_friend($value->ID, $userID) && $value->ID != $userID) {
-			
-	// 		if ( $pending = UM()->Friends_API()->api()->is_friend_pending( $value->ID, $userID) ) {
-	// 			// if ($pending == $userID) {
-	// 			// 	echo $value->data->user_login." respond to request -<br />";
-	// 			// } else {
-	// 			// 	echo $value->data->user_login." request sent -<br />";
-	// 			// }
-	// 		} else {
-	// 			$userdetails['id'] = $value->ID;
-	// 			$userdetails['displayname'] = (!empty($value->data->display_name) ? $value->data->display_name : $value->data->user_login);
-	// 			$userdetails['followers'] = UM()->Followers_API()->api()->count_followers( $value->ID );
-	// 			$userdetails['user_nicename'] = $value->data->user_nicename;
-	// 			array_push($newuserlist, $userdetails);
-	// 		}
-	// 	}
-	// }
-
-	// usort($newuserlist, function($a, $b) {
-	// 	return $a['followers'] <=> $b['followers'];
-	// });
-	// $toptraiders = array_reverse($newuserlist);
-
-?>
 <div class="top-traiders" style="margin-top: 10px;">
   <div class="top-traiders-inner">
 	  <div class="to-top-title">Who to Mingle</div>

e05894da4db25bd9653bde79fbf19008cfac4c43
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-02 14:15:05 +0800

    edit watchlist page

---
+++ wp-content/themes/arbitnew/watchlist/header-files.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/wp-content/plugins/um-friends/assets/css/um-friends.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/parts_style.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/page_style.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/responsive.css?<?php echo time(); ?>">
+    <?php wp_head(); ?>
+    
+</head>
+
+<header id="main-header" class="dashboard-header">
+    <div class="header-dashboard">
+        <div class="header-dashboard-inner">
+            <div class="left-header-part">
+                <div class="left-header-inner">
+                    <div class="logo-image">
+                        <a href="/"><img src="/wp-content/themes/arbitrage-child/images/arblogo_svg1.svg" style="width: 33px;"></a>
+                    </div>
+                    <div class="searchbar">
+                        <div class="inner-seatch">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                            <?php
+                                $search_term = get_search_query();
+                                //echo do_shortcode("[et_pb_search admin_label='Search Arbitrage...' background_layout='light' text_orientation='left' exclude_pages='off' exclude_posts='off' hide_button='off' /]");
+                             ?>
+
+                            <script>
+                                jQuery(document).ready(function(e) {
+                                    jQuery(".inner-seatch input[type='text']").val("<?php if (!$search_term){ echo "Search"; } else { echo $search_term; } ?>");
+                                });
+                            </script>
+							
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="right-header-part">
+                <div class="right-header-inner">
+                    <!-- <div class="optsdropdown">
+
+                    </div> -->
+
+                    <ul class="main-drops">
+                        <div class="main-user-name" id="main-user-name">
+                             <div class="ontotools">
+                                <a><img src="<?php $cdnorlocal ?>/svg/menu.svg" style="width: 17px;display: inline-block;vertical-align: top;margin-top: 4px;"></a>
+                            </div>
+                        </div>
+                        <ul id="droppouts" style="box-shadow: 0px 2px 4px 1px rgba(7, 13, 19, 0.52);">
+                            <li><a href="#">Buy/Sell Calculator</a></li>
+                            <li><a href="#">VAR Calculator</a></li>
+                            <li><a href="#">Average Price Calculator</a></li>
+                            <li><a href="<?php echo get_home_url(); ?>/multicharts/">Multichart</a></li>
+                        </ul>
+                    </ul>
+
+                    <div class="ontomitif">
+                        <div class="um-notification-b right" data-show-always="1">
+                            <img src="<?php $cdnorlocal ?>/svg/bell.svg" style="width: 19px;display: inline-block;vertical-align: top;margin-top: 3px;">
+                            <span class="um-notification-live-count count-0 counter" style="display: none !important;">0</span>
+                            <span class="um-notification-live-count count-0 countx_max" style="display: none;"></span>
+                        </div>
+                    </div>
+					
+                    <div class="dmessagepart" <?php if(is_page(2457)){ ?>style="padding-right:0;"<?php } ?>>
+                    	<?php if(!is_page(2457)){ ?>
+                            <div class="dmessagepart-wrap" data-show-always="1">
+                                <a href="/vyndue/"><img src="<?php echo $cdnorlocal; ?>/svg/vyndue-newlogo-white.svg" style="width: 19px;display: inline-block;vertical-align: top;margin-top:3px"></a>
+                                <span class="vyndue-notification" style="display: none;">0</span>
+                            </div>
+                        <?php } ?>
+                    </div>
+                    
+                    <ul class="main-drop">
+                        <div class="main-user-name" id="main-user-name">
+                            <div class="header-image">
+                                <div class="user-image" style="background: url('<?php echo esc_url( get_avatar_url( $user->ID ) ); ?>') no-repeat center center;">&nbsp;</div>
+                            </div>
+                            
+                        </div>
+                        <ul id="droppouts" style="">
+                            <li><a href="/user/<?php echo um_user('user_login') ?>/?profiletab=main&amp;um_action=edit" class="real_url">Edit Profile</a></li>
+                            <li><a href="<?php echo get_home_url(); ?>/user/">My Account</a></li>
+                            <hr class="style14 style15">
+                            <li class="onto-last-element"><a href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a></li>
+                        </ul>
+                        
+                    </ul>
+        
+                     <div class="right-slider-menu" >
+                        <img id="right-slider-icon" src="<?php echo get_home_url(); ?>/wp-content/themes/arbitrage-child/images/menu.svg" width="22px">
+                    </div>
+                    <div id="right-menu" class="right-slider-menu1"></div>
+
+                    <div class="opennotification">
+                        <div class="notifinnerbase">
+                        </div>
+                    </div>
+
+                   
+
+                </div>
+            </div>
+            <br class="clear">
+        </div>
+        <div class="border-colored"></div>
+    </div>
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+</header>
---
+++ wp-content/themes/arbitnew/watchlist/sidebar-viewedstocks.php
@@ -0,0 +1,161 @@
+<script type="text/javascript">
+
+jQuery(function(){
+  
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  var colors = ['#f44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50'];
+  var dcount = 0;
+  jQuery('.top-stocks .to-content-part ul .even span, .top-stocks .to-content-part ul .odd span').each(function(index,el){
+    if (dcount == '10') {dcount = 0; }
+    jQuery(el).css('border-color',colors[dcount]);
+    dcount++;
+  });
+});
+</script>
+
+<div class="top-stocks">
+    <div class="to-top-title"><strong>Most Watched Stocks</strong></div>
+    <hr class="style14 style15" style="width: 90% !important;margin-bottom: 2px !important;margin-top: 6px !important;/* margin: 5px 0px !important; */">
+    <div class="to-content-part">
+
+
+        <?php 
+
+       
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, "https://data-api.arbitrage.ph/api/v1/stocks/list");
+        curl_setopt($curl, CURLOPT_RESOLVE, ['data-api.arbitrage.ph:443:34.92.99.210']);
+        curl_setopt($curl, CURLOPT_DNS_USE_GLOBAL_CACHE, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        if ($response !== false) {
+            $response = json_decode($response);
+            $stockinfo = $response->data;
+        }
+
+        $num = 0;
+        $counter = 1;
+        $stockcount = 0;
+        //$stock_watched = array();      
+        $users = get_users( array( 'fields' => array( 'ID' ) ) );
+
+        foreach($stockinfo as $stkey => $stvals){
+        
+            foreach($users as $user_id){
+           
+                $havemeta = get_user_meta($user_id->ID, '_watchlist_instrumental', true);
+
+                if($havemeta){
+                    
+                            foreach ($havemeta as $key => $value) {        
+                                
+                                        if ($stvals->symbol == $value['stockname']) {
+                                            $stock_watched[$stockcount][0] = $stvals->symbol;
+                                            $stock_watched[$stockcount][1] = $counter;
+                                            $stock_watched[$stockcount][2] = $stvals->description;
+                                            $counter++;   
+                                            //$stockcount++;  
+                                        }
+
+                                 }
+
+                         }       
+
+                     }
+
+                $stockcount++;
+                $counter = 1;
+             }
+    
+             usort($stock_watched, function($a, $b) {
+                return $b[1] <=> $a[1];
+            });
+
+             
+
+             ?>
+             <ul>
+             <?php
+
+             for($i = 0; $i < 10; $i++){
+
+                 if($stock_watched[$i][0] != null && $stock_watched[$i][0] != ""){
+
+                       
+                                    ?>
+                                            <li class="odd">
+                                                <span><?php echo $stock_watched[$i][0]; ?></span>
+                                                <a href="#"><?php echo $stock_watched[$i][2]; ?><br><p><?php echo $stock_watched[$i][1]; ?> Following</p></a>
+                                            </li>
+
+                                    <?php
+
+                           
+                        }
+
+                     if($i == 4){
+                        echo "<div class='hide-show watched-hidden-content'>";
+                    }
+                   
+                }
+
+           
+
+                    echo "</div>";
+            
+            ?>  
+
+            </ul>
+
+                       
+               
+    </div>
+    <!-- <div class="to-bottom-seemore" style="display: inline-flex;">
+        <i class="fas fa-sort-down" style="
+        font-size: 16px;
+        margin-right: 3px;
+        vertical-align: initial;
+    "></i>
+        <div class="see-more-btn button-toggle-content">
+            <strong>See more</strong>
+        </div>
+    </div> -->
+    <div class="to-bottom-seemore" style="display: inline-flex;color: #cecece;font-size: 13px;">
+        <div class="see-more-btn button-toggle-content">
+            <i class="fas fa-sort-down" id="fa-up" style="
+                font-size: 13px;
+                margin-right: 3px;
+                vertical-align: initial;
+                bottom: 0px;
+                top: -2px;
+                position: relative;
+            "></i>
+            <strong>See more</strong>
+        </div>
+    </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script>
+// $(document).ready(function(){
+// 	$('.button-toggle-content').on('click', function(){
+// 		$('.watched-hidden-content').toggle();
+// 	});
+// });
+$(document).ready(function(){
+    $(".button-toggle-content").click(function () {
+        $(".watched-hidden-content").toggle('slow');
+        if($(".button-toggle-content").hasClass('isopen')){
+            $(".button-toggle-content").html('<i class="fas fa-sort-down" id="fa-up" style="bottom: 0px;top: -2px;position: relative;font-size: 16px;margin-right: 4px;vertical-align: initial;"></i><strong>Show more</strong>').removeClass('isopen').slideDown( "slow" );
+            $(".watched-hidden-content").slideUp( "slow" );
+        }else {
+            $(".button-toggle-content").html('<i class="fas fa-sort-up" id="fa-up" style="bottom: 0;top: 4px;position: relative;font-size: 16px;margin-right: 4px;vertical-align: initial;"></i><strong>Hide</strong>').addClass('isopen');
+            $(".watched-hidden-content").slideDown( "slow" );
+        }
+    });
+});
+</script>
+

c6624eab9100f68a8bd59bcc7914e070d20b4257
Emmanuel Tierra <emman.tierra@gmail.com>
2019-10-02 14:50:35 +0800

    Remove unnecessary scripts

--- wp-content/themes/arbitnew/template-ticker.php
+++ wp-content/themes/arbitnew/template-ticker.php
@@ -29,10 +29,7 @@
 	<link rel="stylesheet" href="/assets/css/animate.min.css" />
 	<link rel="stylesheet" href="/assets/css/style.min.css" />
 	<link rel="stylesheet" href="/assets/css/theme/default.css" id="theme" />
-    <link href="/assets/plugins/select2/dist/css/select2.min.css" rel="stylesheet" />
     <link href="/assets/css/style-chart.css" rel="stylesheet" />
-
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
 	<style>
 		.arb_top_ticker {display:block;}
 		.list-inline>li,
@@ -100,10 +97,7 @@
 		<script src="/assets/crossbrowserjs/respond.min.js"></script>
 		<script src="/assets/crossbrowserjs/excanvas.min.js"></script>
 	<![endif]-->
-	<script src="/assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-	<script src="/assets/plugins/jquery-cookie/jquery.cookie.js"></script>
 	<script src="/assets/js/apps.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.3/socket.io.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.1/moment.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></script>
@@ -114,45 +108,14 @@
 	<script src="//cdnjs.cloudflare.com/ajax/libs/angular-sanitize/1.6.6/angular-sanitize.min.js"></script>
 	<script src="//cdn.rawgit.com/Luegg/angularjs-scroll-glue/master/src/scrollglue.js"></script>
 	<script src="/assets/plugins/ng-embed/dist/ng-embed.min.js"></script>
-	<script src="/assets/js/jquery.fullscreen-min.js"></script>
-	<script src="/assets/plugins/gritter/js/jquery.gritter.js"></script>
-    <script src="/assets/plugins/select2/dist/js/select2.min.js"></script>
-    <script src="/assets/js/aes.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js"></script>
-	<script src="https://platform.twitter.com/widgets.js"></script>
 	<script>
-		var modalzindex = 10000;
-    	var nightmode = localStorage.getItem('theme') == 'dark';
         var socket = io.connect('https://dev-socket-api.arbitrage.ph');
 
 		jQuery(document).ready(function() {
 			App.init();
-		    jQuery( function () {
-		        jQuery(".stocks-select2").select2({placeholder:"Stock", width: '100%'})
-		    });
 		});
-		var _stocks     = {};
-		var _admin 		= false;
-		var _moderator 	= false;
-		var _client_id 	= 'arbitrage.ph';
-		var _user_id 	= '<?php echo $user->ID; ?>'
-		var _username 	= '<?php echo $user->user_email; ?>';
-		var _symbol 	= '<?php 
-		$getcururl = $_SERVER['REQUEST_URI'];
-		if ($getcururl == "/chart/"){
-			echo "PSEI";
-		}else{
-			$remchrt = str_replace("/chart/", "", $getcururl);
-			$getfsymb = str_replace("/", "", $remchrt);
-			echo strtoupper($getfsymb);
-		}
-		?>';
 	</script>
 <script src="/assets/js/angular/functions.js?v=1.219"></script>
 <script src="/assets/js/angular/ticker-controller.js?v=<?php echo time() ?>"></script>
-<script src="/assets/js/angular/directives.js?v=1.218"></script>
-<script src="/assets/js/angular/filters.js?v=1.218"></script>
-<script src="/assets/tradingview/charting_library/charting_library.min.js?v=1.218"></script>
-
 </body>
 </html> 

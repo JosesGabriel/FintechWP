ad89f7dae314f9bf6d77d154d0207802a74c1044
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-10-02 18:46:46 +0800

    for test: change to public curl

--- wp-content/themes/arbitnew/functions-data-api.php
+++ wp-content/themes/arbitnew/functions-data-api.php
@@ -48,7 +48,7 @@ class DataAPI extends WP_REST_Controller
 
     public function sendViaCurl($url){
         curl_setopt($curl, CURLOPT_URL, $url);
-        $result = curl_exec($curl);
+        $result = curl_exec($this->$curl);
 
         return json_decode($result);
     }

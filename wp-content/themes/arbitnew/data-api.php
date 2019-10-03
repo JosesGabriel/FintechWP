6864ca4ad5715cda6b7b7756049ad5033dc17a85
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-10-03 13:18:06 +0800

    wp rest response set

--- wp-content/themes/arbitnew/data-api.php
+++ wp-content/themes/arbitnew/data-api.php
@@ -3,11 +3,11 @@
 	* Template Name: Data API Page
 */
 
-$data = wp_get_current_user();
-
-echo json_encode([
+$currentUser = wp_get_current_user();
+$data = json_encode([
 	"is_user_login" => is_user_logged_in(),
-	"user_id" => $data->ID,
+	"user_id" => $currentUser->ID,
 ]);
 
+return new WP_REST_Response($data, 200);
 ?>

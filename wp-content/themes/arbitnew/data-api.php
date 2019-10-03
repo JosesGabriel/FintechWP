345f24c9c3ceeff49bf75cb193c8f6228a3800a3
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-10-03 13:20:26 +0800

    replace return to echo

--- wp-content/themes/arbitnew/data-api.php
+++ wp-content/themes/arbitnew/data-api.php
@@ -4,10 +4,9 @@
 */
 
 $currentUser = wp_get_current_user();
-$data = json_encode([
+return json_encode([
 	"is_user_login" => is_user_logged_in(),
 	"user_id" => $currentUser->ID,
 ]);
 
-echo new WP_REST_Response($data, 200);
 ?>

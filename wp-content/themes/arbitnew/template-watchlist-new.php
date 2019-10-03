0bb843e963131b97be8624cba4947b9b5441eb88
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-03 10:49:53 +0800

    Update template-watchlist-new.php

--- wp-content/themes/arbitnew/template-watchlist-new.php
+++ wp-content/themes/arbitnew/template-watchlist-new.php
@@ -57,7 +57,7 @@ if (isset($_POST) && !empty($_POST)) {
      $wpdb->insert('arby_usermeta', array(
                 'user_id' => $userID,
                 'metakey' => '_watchlist_instrumental',
-                'meta_value' => 'selling',
+                'meta_value' => 'selling'
                 // ... and so on
             ));
 

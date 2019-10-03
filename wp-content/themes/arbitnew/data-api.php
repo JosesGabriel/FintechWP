2f7c4847ebcb026e9f0f14db6c74e21005d89408
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-10-03 13:23:31 +0800

    add content type header

--- wp-content/themes/arbitnew/data-api.php
+++ wp-content/themes/arbitnew/data-api.php
@@ -3,6 +3,8 @@
 	* Template Name: Data API Page
 */
 
+header('Content-Type: application/json');
+
 $currentUser = wp_get_current_user();
 echo json_encode([
 	"is_user_login" => is_user_logged_in(),

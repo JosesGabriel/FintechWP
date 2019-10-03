68d26b2ff8ba85190cd4b1df412d8ad4bbddafc2
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-03 18:04:59 +0800

    Update template-watchlist-new.php

--- wp-content/themes/arbitnew/template-watchlist-new.php
+++ wp-content/themes/arbitnew/template-watchlist-new.php
@@ -120,13 +120,24 @@ $watchinfo = get_user_meta('7', '_scrp_stocks_chart', true);
 
                                                                 //echo $value['stockname'];
                                                                 ?>
+                                                                <script type="text/javascript">
+                                                                    
+                                                                    $.ajax({
+                                                                                url: "/wp-json/data-api/v1/stocks/history/latest?exchange=PSE&symbol='<?php echo $value['stockname']; ?>'",
+                                                                                type: 'GET',
+                                                                                dataType: 'json', // added data type
+                                                                                success: function(res) {
+                                                                                        
+                                                                                    //jQuery.each(res.data, function(index, value) {      
+                                                                                            console.log(res.data);    
+                                                                                    //});  
 
-                                                                <?php
-                                                                    // get current price and increase/decrease percentage
-                                                     
-                                                                       
-                                                                    //if (is_object($dinstall['data'])):
-                                                                ?>
+                                                                                },
+                                                                                error: function (xhr, ajaxOptions, thrownError) {
+                                                                                    
+                                                                                }
+                                                                            })
+                                                                    </script>
 
                                                                 <li class="watchonlist" class="to-watch-data" data-dstock="<?php echo $value['stockname']; ?>" data-dhisto='<?php echo json_encode($dstockinfo); ?>'>
                                                                     <!--<div class="watchlist--buttons">

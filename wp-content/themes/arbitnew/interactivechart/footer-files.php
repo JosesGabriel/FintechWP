e511bfa7c4936c1671b7721f1f2436904bf07f4a
Emmanuel Tierra <emman.tierra@gmail.com>
2019-10-04 12:56:10 +0800

    Change functions scope

--- wp-content/themes/arbitnew/interactivechart/footer-files.php
+++ wp-content/themes/arbitnew/interactivechart/footer-files.php
@@ -391,22 +391,21 @@
 				jQuery(this).val(replaceCommas(currentVal));
 
 			});
-
-			function testDecimals(currentVal) {
-				var count;
-				currentVal.match(/\./g) === null ? count = 0 : count = currentVal.match(/\./g);
-				return count;
-			}
-
-			function replaceCommas(yourNumber) {
-				var components = yourNumber.toString().split(".");
-				if (components.length === 1) 
-					components[0] = yourNumber;
-				components[0] = components[0].replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-				if (components.length === 2)
-					components[1] = components[1].replace(/\D/g, "");
-				return components.join(".");
-			}
-
 		});
+		
+		function testDecimals(currentVal) {
+			var count;
+			currentVal.match(/\./g) === null ? count = 0 : count = currentVal.match(/\./g);
+			return count;
+		}
+
+		function replaceCommas(yourNumber) {
+			var components = yourNumber.toString().split(".");
+			if (components.length === 1) 
+				components[0] = yourNumber;
+			components[0] = components[0].replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+			if (components.length === 2)
+				components[1] = components[1].replace(/\D/g, "");
+			return components.join(".");
+		}
 	</script>

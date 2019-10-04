397f4e86a3377d114f541eae9e4681a744dc0326
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-04 10:18:30 +0800

    Update template-watchlist-new.php

--- wp-content/themes/arbitnew/template-watchlist-new.php
+++ wp-content/themes/arbitnew/template-watchlist-new.php
@@ -167,7 +167,7 @@ $watchinfo = get_user_meta('7', '_scrp_stocks_chart', true);
                                                                             </div>
 
                                                                             <div class="dpricechange">
-                                                                                <div class="curprice">&#8369;<?php echo $dinstall['data']->last; ?></div>
+                                                                                <div class="curprice">&#8369;</div>
                                                                                 <?php if (strpos($dinstall['data']->changepercentage, '-') !== false): ?>
                                                                                     <div class="curchange onred"><?php echo round($dinstall['data']->changepercentage, 2); ?>%</div>
 

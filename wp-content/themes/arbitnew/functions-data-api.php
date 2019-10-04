9a712ec47ac2c4b212707ab9fb53f52105f23463
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-10-04 21:49:35 +0800

    test slash

--- wp-content/themes/arbitnew/functions-data-api.php
+++ wp-content/themes/arbitnew/functions-data-api.php
@@ -21,7 +21,6 @@ class DataAPI extends WP_REST_Controller
     {
         $this->guzzleClient = new GuzzleHttp\Client([
             'http_errors' => false,
-            'handler' => new GuzzleHttp\Handler\CurlHandler()
             ]);
         $this->dataBaseUrl = 'data-api.arbitrage.ph';
         $this->client_secret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfbmFtZSI6IjRSQjErUjQ5MyJ9.SZzdF4-L3TwqaGxfb8sR-xeBWWHmGyM4SCuBc1ffWUs';
@@ -160,7 +159,7 @@ class DataAPI extends WP_REST_Controller
         // //endregion forward request
 
         // return $result;
-
+        return "https://{$_SERVER['HTTP_HOST']}";
         $promise = $this->guzzleClient->requestAsync("GET", "/wp-json/data-api/v1/charts/history?symbol=TEL&exchange=PSE&resolution=1D&from=2019-09-14&to=2019-10-04", [
             "headers" => [
                 "Content-type" => "application/json",

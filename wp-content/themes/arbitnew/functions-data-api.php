3d5101682c9d3836df56425ccd4189b49d1187d5
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-10-04 20:46:51 +0800

    for test: try async

--- wp-content/themes/arbitnew/functions-data-api.php
+++ wp-content/themes/arbitnew/functions-data-api.php
@@ -125,7 +125,7 @@ class DataAPI extends WP_REST_Controller
             ]);
 
         $promise = $this->guzzleClient->sendAsync($request)->then(function ($response) {
-            return json_decode($response->getBody());
+            return $response->getStatusCode();
         });
         
         $promise->wait();

a98557a7cca20ffba55a60aa8f689317d1641f3e
nick <nickgayo12@gmail.com>
2019-10-05 12:33:47 +0800

    migrating css to dedicated page style 21

--- wp-content/themes/arbitnew/parts/sidebar-latestnews.php
+++ wp-content/themes/arbitnew/parts/sidebar-latestnews.php
@@ -1,11 +1,34 @@
 <div class="latest-news" style="margin-top:10px;">
     <div class="to-top-title">Bulletins
     </div>
-    <hr class="style14 style15"">
+    <hr class="style14 style15">
     <div class="to-content-part">
-        <div class="to-rss-inner">
+        <div class="to-rss-inner"><div id="super_rss_reader-5" class="et_pb_widget widget_super_rss_reader"><h4 class="widgettitle">Bulletin</h4>
+
+        <!-- Start - Super RSS Reader v2.7-->
+        <div class="super-rss-reader-widget">
+<div class="srr-wrap srr-vticker srr-style-none" data-visible="5" data-speed="4000"><div>
+
+	<div class="srr-item odd" style="display: inline-flex;"><div class="srr-item-cont" style=""><time class="srr-date">02<div>Oct</div></time></div><div class="srr-content-cont" style="display: block;"><div class="srr-title 2"><a href="https://businessmirror.com.ph/2019/10/02/why-japans-isetan-mitsukoshi-is-entering-the-philippine-market/" target="_blank" rel="nofollow" title="Why Japan's Isetan Mitsukoshi is entering the Philippine market  "><div class="dnewstitle">Why Japan's Isetan Mitsukoshi is enterin...</div><div class="dauthor"></div></a></div><div class="srr-meta"></div></div></div>
+
+	<div class="srr-item even" style="display: inline-flex;"><div class="srr-item-cont" style=""><time class="srr-date">04<div>Oct</div></time></div><div class="srr-content-cont" style="display: block;"><div class="srr-title 2"><a href="https://business.inquirer.net/280384/d1-milano-supports-philippine-moto-heritage-weekend" target="_blank" rel="nofollow" title="D1 Milano supports Philippine Moto Heritage Weekend  "><div class="dnewstitle">D1 Milano supports Philippine Moto Heritage W...</div><div class="dauthor"></div></a></div><div class="srr-meta"></div></div></div>
+
+	<div class="srr-item odd" style="display: inline-flex;"><div class="srr-item-cont" style=""><time class="srr-date">03<div>Oct</div></time></div><div class="srr-content-cont" style="display: block;"><div class="srr-title 2"><a href="https://business.inquirer.net/280236/ph-shares-sink-to-7600-on-disappointing-us-factory-numbers" target="_blank" rel="nofollow" title="PH shares sink to 7600 on disappointing US factory numbers  "><div class="dnewstitle">PH shares sink to 7600 on disappointing US fa...</div><div class="dauthor"></div></a></div><div class="srr-meta"></div></div></div>
+
+	<div class="srr-item even" style="display: inline-flex;"><div class="srr-item-cont" style=""><time class="srr-date">04<div>Oct</div></time></div><div class="srr-content-cont" style="display: block;"><div class="srr-title 2"><a href="https://businessmirror.com.ph/2019/10/04/trendy-global-online-marketplace-to-help-philippine-business-owners-grow-their-business/" target="_blank" rel="nofollow" title="Trendy global online marketplace to help Philippine business owners grow their business  "><div class="dnewstitle">Trendy global online marketplace to help Phil...</div><div class="dauthor"></div></a></div><div class="srr-meta"></div></div></div>
+
+	<div class="srr-item odd" style="display: inline-flex;"><div class="srr-item-cont" style=""><time class="srr-date">03<div>Oct</div></time></div><div class="srr-content-cont" style="display: block;"><div class="srr-title 2"><a href="https://www.bworldonline.com/sophos-partners-with-wordtext-to-widen-philippine-customer-base/" target="_blank" rel="nofollow" title="Sophos partners with Wordtext to widen Philippine customer base  "><div class="dnewstitle">Sophos partners with Wordtext to widen Philip...</div><div class="dauthor"></div></a></div><div class="srr-meta"></div></div></div>
+
+</div>
+        </div> 
+
+
+</div>
+        <!-- End - Super RSS Reader -->
+        
+</div> <!-- end .et_pb_widget -->
         </div>
-    </div>
+        </div>
     <div class="to-bottom-seemore">
 		<a href="/bulletins/">
 			<strong class="view__all">View all</strong>

aea610008f03826ee623c5d64012cf2c928ddbe8
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-05 12:54:32 +0800

    Update sidebar-viewedstocks.php

--- wp-content/themes/arbitnew/watchlist/sidebar-viewedstocks.php
+++ wp-content/themes/arbitnew/watchlist/sidebar-viewedstocks.php
@@ -50,6 +50,7 @@ $.ajax({
 
 $stocks = $_GET['stocks'];
 
+print_r($stocks);
 ?>
 
 <div class="top-stocks">
@@ -66,10 +67,10 @@ $stocks = $_GET['stocks'];
         ///$response = curl_exec($curl);
         //curl_close($curl);
 
-       // if ($stocks !== false) {
-       //     $response = json_decode($stocks);
-        //    $stockinfo = $response->data;
-      //  }
+        if ($stocks !== false) {
+            $stocks = json_decode($stocks);
+            $stockinfo = $stocks->data;
+        }
 
         $num = 0;
         $counter = 1;

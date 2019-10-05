1f1f3a4915c6d11b4835ef98756a3c063b91cc25
nick <nickgayo12@gmail.com>
2019-10-05 10:00:43 +0800

    migrating css to dedicated page style 18

--- wp-content/themes/arbitnew/parts/sidebar-topplayers.php
+++ wp-content/themes/arbitnew/parts/sidebar-topplayers.php
@@ -1,17 +1,17 @@
-<div class="dtanks">
-
-	<h5>Top Players</h5>
-	<hr class="style14 style15" style="width: 100% !important;margin-bottom: 5px !important;margin-top: 10px !important;">
-	<div class="ranks">
-		<div id="preloader" class="trendingpreloader">
-			<div id="status">&nbsp;</div>
-			<div id="status_txt"></div>
+<div class="dtanks" style="margin-top: 10px;">
+	<div class="to-top-title">Top Players</div>
+	<hr class="style14 style15">
+	<div class="to-content-part">    
+		<div class="ranks">
+			<div id="preloader" class="trendingpreloader">
+				<div id="status">&nbsp;</div>
+				<div id="status_txt"></div>
+			</div>
+			<ul class="topsect"></ul>
+			<ul class="othersect"></ul>
+			<ul class="myrank"></ul>
+			<!-- <h5>Ranks</h5> -->
 		</div>
-		<ul class="topsect"></ul>
-		<ul class="othersect"></ul>
-		<ul class="myrank"></ul>
-		<!-- <h5>Ranks</h5> -->
-		<a class="viewmoreplayers">View more</a>
 	</div>
-
+	<a class="viewmoreplayers to-bottom-seemore">See more</a>
 </div>

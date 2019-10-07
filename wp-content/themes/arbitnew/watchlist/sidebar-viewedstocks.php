a7ccb4c31b04005094566bfdd62431bc477530fb
Emmanuel Tierra <emman.tierra@gmail.com>
2019-10-07 05:43:46 +0000

    Add timeout when  fetching bids and asks
    Error reference: https://code.angularjs.org/1.6.9/docs/error/$rootScope/inprog?p0=$digest

--- wp-content/themes/arbitnew/watchlist/sidebar-viewedstocks.php
+++ wp-content/themes/arbitnew/watchlist/sidebar-viewedstocks.php
@@ -106,12 +106,11 @@ $watchlist = $wpdb->get_results('select meta_value from arby_usermeta where meta
 
          if($stock_watched[$i][0] != null && $stock_watched[$i][0] != ""){
             $stockname = $stock_watched[$i][0];
-            $following = $stock_watched[$i][1];
-            echo "<script> lateststocks('$stockname');</script>";
+           
               ?>
                       <li class="odd">
                           <span><?php echo $stock_watched[$i][0]; ?></span>
-                          <a href="#"><label class="desc_<?php echo $stockname; ?>"></label><br><p><?php echo $following; ?> Following</p></a>
+                          <a href="#"><?php  echo "<script> lateststocks('$stockname');</script>"; ?><br><p><?php echo $stock_watched[$i][1]; ?> Following</p></a>
                       </li>
 
               <?php

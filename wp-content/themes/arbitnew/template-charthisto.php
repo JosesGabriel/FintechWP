2959604a3ae768a3e15b1f1ae9bc2de951a0fabb
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-10-07 11:43:24 +0800

    FOR TEST: query response 5

--- wp-content/themes/arbitnew/template-charthisto.php
+++ wp-content/themes/arbitnew/template-charthisto.php
@@ -19,7 +19,7 @@
     if(isset($_GET['query'])){
         $guzzle = new GuzzleRequest();
 
-        $request = $guzzle->request("GET", get_site_url(null, '', 'https') . "/wp-json/data-api/v1/stocks/list", [
+        $request = $guzzle->request("GET", "https://data-api.arbitrage.ph/api/v1/stocks/list", [
             "headers" => [
                 "Content-type" => "application/json",
                 "Authorization" => "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",

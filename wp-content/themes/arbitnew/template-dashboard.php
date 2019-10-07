877233804c66e27a2ba9aae15caba9da6b6fbd31
lerrouxlopez <disturbancerights@yahoo.com>
2019-10-07 13:35:31 +0800

    hide watchlist sidebar

--- wp-content/themes/arbitnew/template-dashboard.php
+++ wp-content/themes/arbitnew/template-dashboard.php
@@ -29,7 +29,7 @@ require("parts/sidebar-avarageprice.php");
 					 			get_template_part('parts/sidebar', 'trendingstocks');
 					 			get_template_part('parts/sidebar', 'traders');
 								get_template_part('parts/sidebar', 'latestnews');
-								get_template_part('parts/sidebar', 'watchlist');
+								#get_template_part('parts/sidebar', 'watchlist');
 								#get_template_part('parts/sidebar', 'topplayers');
                 get_template_part('parts/sidebar', 'alert');
 					 			get_template_part('parts/sidebar', 'footer');

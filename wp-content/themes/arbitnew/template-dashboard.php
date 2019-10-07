a7519b586e7b28d2828df819d43dcc8347842a55
nick <nickgayo12@gmail.com>
2019-10-07 12:15:41 +0800

    migrating css to dedicated page style 37

--- wp-content/themes/arbitnew/template-dashboard.php
+++ wp-content/themes/arbitnew/template-dashboard.php
@@ -30,10 +30,14 @@ require("parts/sidebar-avarageprice.php");
 					 			get_template_part('parts/sidebar', 'traders');
 								get_template_part('parts/sidebar', 'latestnews');
 								get_template_part('parts/sidebar', 'watchlist');
+					?>
+					<div class="container-sticky">
+					<?php
 								get_template_part('parts/sidebar', 'topplayers');
                 #get_template_part('parts/sidebar', 'alert');
 					 			get_template_part('parts/sidebar', 'footer');
 					 ?>
+					 </div>
 				</div>
 			</div>
 			<br class="clear">

a667186047fa01128df80e17b297231c4d59af27
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-07 11:35:17 +0800

    Update sidebar-viewedstocks.php

--- wp-content/themes/arbitnew/watchlist/sidebar-viewedstocks.php
+++ wp-content/themes/arbitnew/watchlist/sidebar-viewedstocks.php
@@ -11,7 +11,7 @@
 
                   jQuery.each(res.data, function(index, value) {
                       if(symbol == value.symbol){
-                        $('.description a').text(value.description);
+                        $('.desc').text(value.description);
                       }
                   });
             },
@@ -105,10 +105,11 @@ $watchlist = $wpdb->get_results('select meta_value from arby_usermeta where meta
 
          if($stock_watched[$i][0] != null && $stock_watched[$i][0] != ""){
             $stockname = $stock_watched[$i][0];
+            echo "<script> lateststocks('$stockname');</script>";
               ?>
                       <li class="odd">
                           <span><?php echo $stock_watched[$i][0]; ?></span>
-                          <a href="#" class="description"><?php echo "<script> lateststocks('$stockname');</script>"; ?><br><p><?php echo $stock_watched[$i][1]; ?> Following</p></a>
+                          <a href="#"><span class="desc"></span><br><p><?php echo $stock_watched[$i][1]; ?> Following</p></a>
                       </li>
 
               <?php

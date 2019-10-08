e9367171a8c5e0b8e57be662792197189ca3c56b
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-08 11:22:30 +0800

    Update sidebar-watchlist.php

--- wp-content/themes/arbitnew/parts/sidebar-watchlist.php
+++ wp-content/themes/arbitnew/parts/sidebar-watchlist.php
@@ -4,7 +4,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-nvd3/1.0.9/angular-nvd3.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nvd3/1.8.6/nv.d3.css">
 <?php
-include "../watchlist/watchlist-sidebarloader.php";
 global $current_user;
 $user = wp_get_current_user();
 $userID = $current_user->ID;
@@ -239,3 +238,7 @@ if ($dwatchinfo !== null) :
     <?php endif; ?>
     </div>
   </div>
+
+  <?php
+    include "../watchlist/watchlist-sidebarloader.php";
+  ?>

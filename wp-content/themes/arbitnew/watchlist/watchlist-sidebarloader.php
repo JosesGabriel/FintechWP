35ad6be9ded053f21856041624ad333c97affe5a
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-08 11:37:54 +0800

    Update watchlist-sidebarloader.php

--- wp-content/themes/arbitnew/watchlist/watchlist-sidebarloader.php
+++ wp-content/themes/arbitnew/watchlist/watchlist-sidebarloader.php
@@ -110,6 +110,7 @@
   
     
     $( document ).ready(function() {
+        console.log('loader....');
         new loadwatctlistsidebar(<?php echo $user->ID; ?>);
         new loadMiniCharts(<?php echo $user->ID; ?>);
     });

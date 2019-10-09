f819e56a0407170e58b84bd0101ef8a6f1e502b9
nick <nickgayo12@gmail.com>
2019-10-09 17:58:49 +0800

    Fix style on minichart

--- wp-content/themes/arbitnew/dashboard/footer-files.php
+++ wp-content/themes/arbitnew/dashboard/footer-files.php
@@ -73,7 +73,7 @@
                                     },
                                     "trendLines":[],
                                     "graphs":[ {
-                                        "balloonColor": "undefined", "balloonText": "[[value]]", "bullet": "round", "bulletAlpha": 0, "bulletBorderColor": "undefined", "bulletBorderThickness": 6, "bulletColor": "#ff1744", "bulletSize": 0, "columnWidth": 0, "fillAlphas": 0.05, "fillColors": dcolor, "gapPeriod": 3, "id": "AmGraph-1", "legendAlpha": 0, "legendColor": "undefined", "lineColor": dcolor, "lineThickness": 0.8, "minBulletSize": 18, "minDistance": 0, "negativeBase": 2, "negativeFillAlphas": 0, "negativeLineAlpha": 0, "title": "Expense Report", "topRadius": 0, "type": "smoothedLine", "valueField": "column-1", "visibleInLegend": !1
+                                        "balloonColor": "undefined", "balloonText": "[[value]]", "bullet": "round", "bulletAlpha": 0, "bulletBorderColor": "undefined", "bulletBorderThickness": 6, "bulletColor": "#ff1744", "bulletSize": 2, "columnWidth": 0, "fillAlphas": 0.05, "fillColors": dcolor, "gapPeriod": 3, "id": "AmGraph-1", "legendAlpha": 0, "legendColor": "undefined", "lineColor": dcolor, "lineThickness": 0.8, "minBulletSize": 18, "minDistance": 0, "negativeBase": 2, "negativeFillAlphas": 0, "negativeLineAlpha": 0, "title": "Expense Report", "topRadius": 0, "type": "smoothedLine", "valueField": "column-1", "visibleInLegend": !1
                                     }],
                                     "guides":[],
                                     "valueAxes":[ {
@@ -87,8 +87,8 @@
                                         "borderAlpha": 0,
                                         "borderColor": "",
                                         "borderThickness": 0,
-                                        "fillAlpha": 0
-                                        "color": "#FFFFFF",
+                                        "fillAlpha": 0,
+                                        "color": "#FFFFFF"
                                     },
                                     "titles":[],
                                     "dataProvider": candles

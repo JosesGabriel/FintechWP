250dfe3498a2a6910e60653c1e249dd084558c85
nick <nickgayo12@gmail.com>
2019-10-10 11:16:11 +0800

    Fix style on minichart

--- wp-content/themes/arbitnew/dashboard/footer-files.php
+++ wp-content/themes/arbitnew/dashboard/footer-files.php
@@ -50,7 +50,6 @@
                                     candles.push({"category": ckey,"column-1": addslog});
                                     
                                 });
-                                let ballontext = (parseFloat(ischange)).toFixed(2);
                                 let dcolor = (changetext == "up" ? '#53b987' : '#eb4d5c');
                                 AmCharts.makeChart( "chartdiv"+stock, {
                                     "type":"serial",
@@ -69,7 +68,7 @@
                                     "chartCursor": {
                                         "enabled": true,
                                         "cursorColor": dcolor,
-                                        "categoryBalloonText": ballontext
+                                        "graphBulletSize": 2,
                                     },
                                     "trendLines":[],
                                     "graphs":[ {

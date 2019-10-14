ceaf68051cd0fe8f84a7570fad9e77d8a396f268
nick <nickgayo12@gmail.com>
2019-10-14 16:31:29 +0800

    Change hits to price & percentage change

--- wp-content/themes/arbitnew/js/lazyfunc.js
+++ wp-content/themes/arbitnew/js/lazyfunc.js
@@ -16,28 +16,29 @@ $( document ).ready(function() {
     });
 
     $.ajax({
-        url: "/sidebar-api/?daction=trendingstocks",
+        url: "https://dev-v1.arbitrage.ph/wp-json/data-api/v1/stocks/history/latest?exchange=PSE&symbol=PHEN",
         type: 'GET',
         dataType: 'json', // added data type
         success: function(res) {
-            $(".trendingpreloader").hide();
-            $.each(res, function( index, value ) {
-                var colors = ['#f44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50'];
-                var toappend = '<li class="even '+index+'">';
-                toappend += '<span style="border-color: '+colors[index]+';">'+value.stock+'</span>';
-                toappend += '<a href="#">'+value.stnamename+'<br>';
-                toappend += '<p>'+value.following+' Hits</p></a>';
-                toappend += '</li>';
+            console.log(res);
+            // $(".trendingpreloader").hide();
+            // $.each(res, function( index, value ) {
+            //     var colors = ['#f44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50'];
+            //     var toappend = '<li class="even '+index+'">';
+            //     toappend += '<span style="border-color: '+colors[index]+';">'+value.stock+'</span>';
+            //     toappend += '<a href="#">'+value.stnamename+'<br>';
+            //     toappend += '<p>'+value.following+' Hits</p></a>';
+            //     toappend += '</li>';
 
-                if(index < 6){
-                    $("ul.trendingme > .trend-content-hidden").before(toappend);
-                } else {
-                    $("ul.trendingme > .trend-content-hidden").append(toappend);
-                }
-            });
+            //     if(index < 6){
+            //         $("ul.trendingme > .trend-content-hidden").before(toappend);
+            //     } else {
+            //         $("ul.trendingme > .trend-content-hidden").append(toappend);
+            //     }
+            // });
         },
         error: function (xhr, ajaxOptions, thrownError) {
-
+            console.log('error');
         }
     });
 

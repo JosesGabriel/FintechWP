1e5548f849a679dbb001d61f96614b33b1d6ddf0
nick <nickgayo12@gmail.com>
2019-10-14 16:39:23 +0800

    Change hits to price & percentage change 2

--- wp-content/themes/arbitnew/js/lazyfunc.js
+++ wp-content/themes/arbitnew/js/lazyfunc.js
@@ -22,7 +22,7 @@ $( document ).ready(function() {
         success: function(res) {
             $(".trendingpreloader").hide();
             $.each(res, function( index, value ) {
-                console.log(index);
+                // console.log(index);
                 console.log(value);
                 
                 // var colors = ['#f44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50'];

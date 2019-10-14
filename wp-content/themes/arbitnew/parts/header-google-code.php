0d01bd6d6dfcee74e96ab15f81dca77d953e4585
Emmanuel Tierra <emman.tierra@gmail.com>
2019-10-14 02:36:31 +0000

    Revert "Update responsive.css"
    This reverts commit 8dafb8f3183f215935230274e4b9bfeec79c3be2.

---
+++ wp-content/themes/arbitnew/parts/header-google-code.php
@@ -0,0 +1,25 @@
+<?php if (WP_PROD_ENV): ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-147416476-1"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-147416476-1');
+</script>
+
+<!-- Global site tag (gtag.js) - Google Ads: 753053364 -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+ (adsbygoogle = window.adsbygoogle || []).push({
+   google_ad_client: "ca-pub-6045441458046529",
+   enable_page_level_ads: true
+ });
+</script>
+
+<!-- Event snippet for Traffic to Site conversion page -->
+<script>
+gtag('event', 'conversion', {'send_to': 'AW-753053364/BJxpCLndqK4BELTdiucC'});
+</script>
+<?php endif ?>

717f7ed1eca6ab307d7e73d756b479c5d0f0827c
nick <nickgayo12@gmail.com>
2019-10-15 18:54:14 +0800

    trending stocks

--- wp-content/themes/arbitnew/js/lazyfunc.js
+++ wp-content/themes/arbitnew/js/lazyfunc.js
@@ -24,13 +24,13 @@ $( document ).ready(function() {
             $.each(res, function( index, value ) {
                 if (value.change > 0){
                     var statuscolor = "green";
-                    var statuscaret = "<i class='fa fa-caret-down'> </i>";
+                    var statuscaret = "<i class='fa fa-caret-up'> </i>";
                 } else if (value.change == 0){
                     var statuscolor = "yellow";
                     var statuscaret = "";
                 } else {
                     var statuscolor = "red";
-                    var statuscaret = "<i class='fa fa-caret-up'> </i>";
+                    var statuscaret = "<i class='fa fa-caret-down'> </i>";
                 }               
                 var colors = ['#f44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50'];
                 var toappend = '<li class="even '+index+'">';

1464e2947d8723171760de54d0e6d1dac13ea870
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-15 17:22:05 +0800

    Update virtualtrade.js

--- wp-content/themes/arbitnew/virtual/virtualtrade.js
+++ wp-content/themes/arbitnew/virtual/virtualtrade.js
@@ -5,8 +5,11 @@ $(document).ready(function(){
 	    url:'/wp-json/data-api/v1/stocks/history/latest?exchange=PSE',
 	    dataType: 'json',
 	    success: function(response) {
+	    	var opt = '';
 	    	jQuery.each(response.data, function(i, val) {
 	    		console.log(val.symbol);
+	    		opt = "<option value="+ val.symbol +">" + val.symbol + "</opttion>";
+	    		$('#inpt_data_select_stock').append(opt);
 	    	});
 	    },
 	      error: function(response) {                 

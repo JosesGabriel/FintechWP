185d2a01a187e12152b0f1233eb6b2a8004fbea0
arphie <e3arphie@gmail.com>
2019-10-15 11:54:32 +0800

    updated virtual trade api

--- wp-content/themes/arbitnew/functions-virtual-api.php
+++ wp-content/themes/arbitnew/functions-virtual-api.php
@@ -26,6 +26,13 @@ class VirtualAPI extends WP_REST_Controller
                 'callback' => [$this, 'initvirtual'],
             ],
         ]);
+
+        register_rest_route($base_route, 'livetrade', [
+            [
+                'method' => 'POST',
+                'callback' => [$this, 'pushLiveTrade'],
+            ],
+        ]);
     }
 
     public function respond($success = false, $data = [], $status = 500)
@@ -36,6 +43,28 @@ class VirtualAPI extends WP_REST_Controller
         return new WP_REST_Response($data, $status);
     }
 
+    // recursive functions
+    public function getjurfees($funmarketval, $funtype){
+        // Commissions
+        $dpartcommission = $funmarketval * 0.0025;
+        $dcommission = ($dpartcommission > 20 ? $dpartcommission : 20);
+        // TAX
+        $dtax = $dcommission * 0.12;
+        // Transfer Fee
+        $dtransferfee = $funmarketval * 0.00005;
+        // SCCP
+        $dsccp = $funmarketval * 0.0001;
+        $dsell = $funmarketval * 0.006;
+
+        if ($funtype == 'buy') {
+            $dall = $dcommission + $dtax + $dtransferfee + $dsccp;
+        } else {
+            $dall = $dcommission + $dtax + $dtransferfee + $dsccp + $dsell;
+        }
+        return $dall;
+    }
+
+    // initialize
     public function initvirtual()
     {
         global $wpdb;
@@ -51,7 +80,8 @@ class VirtualAPI extends WP_REST_Controller
             tradenotes varchar(250),
             buydate varchar(250),
             vtcategory varchar(250),
-            vttype varchar(250)
+            vttype varchar(250),
+            userid int(10)
         )");
 
         $wpdb->query("create table if not exists arby_vt_tradelog (
@@ -60,10 +90,37 @@ class VirtualAPI extends WP_REST_Controller
             volume varchar(250),
             selldate varchar(250),
             vtcategory varchar(250),
-            vttype varchar(250)
+            vttype varchar(250),
+            userid int(10)
         )");
 
         return $this->respond(true, ['data' => "success?"], 200);
+    }
+
+    // pusher
+    public function pushLiveTrade($details)
+    {
+        global $wpdb;
+        $data = $details->get_params();
+        // save information
+        $sql = "insert into arby_vt_live (stockname, buyprice, volume, emotion, strategy, tradeplan, tradenotes, buydate, vtcategory, vttype, userid) values ('".$data['stockname']."', '".$data['buyprice']."', '".$data['volume']."', '".$data['emotion']."', '".$data['strategy']."', '".$data['tradeplan']."', '".$data['tradenotes']."', '".$data['buydate']."', '".$data['category']."', '".$data['type']."', '".$data['userid']."')";
+        $insertlive = $wpdb->query($sql);
+        if($insertlive){
+            // return data
+            $marketvalue = $data['volume'] * $data['buyprice'];
+            $totalcost = $marketvalue - $this->getjurfees($marketvalue, 'sell');
+            $data['marketvals'] = $marketvalue;
+            $profit = $totalcost - $marketvalue;
+
+            $profitperc = ($profit / $marketvalue) * 100;
+            $data['profit'] = $profit;
+            $data['profitperc'] = $profitperc;
+
+            return $this->respond(true, ['data' => $data], 200);
+        } else {
+            return $this->respond(true, ['error' => 'inserting not successful'], 400);
+        }
+        
     }
 
 }

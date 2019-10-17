8410423fdda4131441d1bcf6cef785d76e352a25
nick <nickgayo12@gmail.com>
2019-10-17 11:00:09 +0800

    Improve style on bullish/bearish tooltip

--- wp-content/themes/arbitnew/dashboard/dashboard-scripts.js
+++ wp-content/themes/arbitnew/dashboard/dashboard-scripts.js
@@ -184,7 +184,7 @@
 					$('.authorSentiment__tooltip').hide();
 					$(this).addClass('authorSentimentBearish--active');
 				});
-				$('.popname ul li').click(function(){
+				$('.popname ul li').on('click', function(){
 					$('.authorSentiment__tooltip').css({'display': 'inline'});
 				});
 				// end joses

7696d6ea7c62627390492b9990ad04dd0bbc1dde
Emmanuel Tierra <emman.tierra@gmail.com>
2019-10-17 14:07:06 +0800

    Separate stock list into its own component

--- wp-content/themes/arbitnew/template-chart.php
+++ wp-content/themes/arbitnew/template-chart.php
@@ -184,60 +184,7 @@
 																				<div class="vertical-box-cell">
 																					<div class="tab-content vertical-box-inner-cell" style="background-color: transparent; border-radius: 0; padding: 0; margin-bottom: 0;">
 																						<div data-scrollbar="true" data-height="100%" style="height: 100%;">
-																							<div class="vertical-box tab-pane fade in active" id="allstock">
-																								<?php if ( ! WP_PROD_ENV): ?>
-																								<table class="table table-condensed m-b-0" style="font-size: 10px; width:90%;">
-																									<thead style="position: fixed; background-color: #2c3e50">
-																										<tr>
-																											<th class="text-default border-default text-left" nowrap="nowrap" ng-click="sortStocks('symbol')" style="padding: 3px 12px 3px 6px !important; cursor: pointer;">
-																												<strong>STOCK</strong>
-																											</th>
-																											<th class="text-default border-default text-left" nowrap="nowrap" ng-click="sortStocks('last')" style="padding: 3px 15px 3px 4px !important; cursor: pointer;">
-																												<strong>LAST</strong>
-																											</th>
-																											<th class="text-default border-default text-left" nowrap="nowrap" ng-click="sortStocks('change_percentage')" style="padding: 3px !important; cursor: pointer;">
-																												<strong>CHANGE</strong>
-																											</th>
-																											<th class="text-default border-default text-left" nowrap="nowrap" ng-click="sortStocks('value')" style="padding: 3px !important; cursor: pointer;">
-																												<strong>VALUE</strong>
-																											</th>
-																											<th class="text-default border-default text-left" nowrap="nowrap" ng-click="sortStocks('trades')" style="padding: 3px 3px 3px 10px !important; cursor: pointer;">
-																												<strong>TRADES</strong>
-																											</th>
-																										</tr>
-																									</thead>
-																								</table>
-
-																								<table class="dstocklistitems table table-condensed m-b-0 text-inverse border-default" style="font-size: 10px; border-bottom: 1px solid; width:97%; margin-top: 19px;">
-																									<tbody>
-																										
-																										<tr 
-																											ng-repeat="stock in stocks | orderBy: sort : reverse track by stock.symbol" 
-																											ng-class="{'text-green': stock.displayChange > 0, 'text-red': stock.displayChange < 0, 'text-yellow': stock.displayChange == 0, 'bg-grey-transparent-5': stock.symbol == $parent.stock.symbol, 'hidden': sort != 'symbol' && !latest_trading_date.isSame(stock.lastupdatetime, 'day')}" 
-																											change-alt="stock"
-																											style="font-weight: bold;"
-																											>
-																											<td class="text-default dspecitem" style="padding: 0px 7px 0 7px !important;" ng-click="select(stock.symbol)" style="cursor: pointer;">
-																												<div style="width: 0; height: 0; overflow: hidden; display: block;">
-																													<input type="radio" name="selected_stock" ng-model="selectedStock" value="{{::stock.symbol}}" id="select-{{::stock.symbol}}"/>
-																												</div>
-																												<div class="ditemone" style="cursor: pointer;">{{::stock.symbol}}</div>
-																											</td>
-																											<td align="left" ng-click="select(stock.symbol)" style="cursor: pointer;">{{stock.displayLast}}</td>
-																											<td align="left" ng-click="select(stock.symbol)" style="cursor: pointer;text-align: center;">{{stock.displayChange}}%</td>
-																											<td align="left" class="text-default" ng-click="select(stock.symbol)" style="cursor: pointer;">{{stock.displayValue}}</td>
-																											<td align="right" class="text-default" ng-click="select(stock.symbol)" style="cursor: pointer;padding-right: 5px !important;">{{stock.trades | numeraljs:'0,0'}}</td>
-																										</tr>
-																										<tr ng-if="stocks.length == 0">
-																											<td colspan="5" align="center">No Data Found</td>
-																										</tr>
-																									</tbody>
-																								</table>
-																								<?php else: ?>
-																								<div
-																									style="padding: 20px; text-align: center; color: #fff;">Temporarily disabled.</div>
-																								<?php endif ?>
-																							</div>
+																							<?php require "interactivechart/components/stocks-list.php" ?>
 
 																							<div class="vertical-box tab-pane fade" id="watchlists">
 																								<div class="arb_watchlst_cont">

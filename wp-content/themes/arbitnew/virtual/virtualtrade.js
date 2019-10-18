4b545e527aa10034eb09cf4ea678ab8ccd1ab671
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-18 15:56:14 +0800

    Update virtualtrade.js

--- wp-content/themes/arbitnew/virtual/virtualtrade.js
+++ wp-content/themes/arbitnew/virtual/virtualtrade.js
@@ -78,7 +78,7 @@ $(document).ready(function(){
 				    success: function(response) {				    	
 				    	$.each(response.data, function(i, val) {
 
-				    		console.log(val);	
+				    		console.log(val.description);	
 
 				    	});	
 				     },

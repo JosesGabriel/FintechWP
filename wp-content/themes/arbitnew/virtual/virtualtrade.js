dd1da165bccf5110e1eb9ed04dba114e8e341553
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-18 11:14:38 +0800

    Update virtualtrade.js

--- wp-content/themes/arbitnew/virtual/virtualtrade.js
+++ wp-content/themes/arbitnew/virtual/virtualtrade.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
 
-	var stockdata;
 	$.ajax({
 	    type:'GET',
 	    url:'/wp-json/virtual-api/v1/buyvalues',
 	    dataType: 'json',
 	    success: function(response) {
 	    	var opt = '';
-	    	stockdata = response;
 	    	$.each(response.data, function(i, val) {
 	    		opt = "<option value="+ val.symbol +">" + val.symbol + "</option>";
 	    		$('#inpt_data_select_stock').append(opt);
@@ -21,41 +19,54 @@ $(document).ready(function(){
 	$('.groupinput').on('change', 'select.data_stocks',function(){
 
 		var sdata = $(this).val();
-		$.each(stockdata.data, function(i, val) {
 
-	    		if(sdata == val.symbol){
-	    			$('.sdesc').text(val.description);
-	    			$('.cprice').text((val.last).toFixed(2));
-	    			$('.pdetails.prev').text((val.close).toFixed(2));
-	    			$('.pdetails.low').text((val.low).toFixed(2));
-	    			$('.pdetails.klow').text(val.weekyearlow);
-	    			$('.pdetails.vol').text(nFormatter(parseFloat(val.volume)));
-	    			$('.pdetails.trade').text((val.trades).toFixed(2));
-	    			$('.pdetails.open').text((val.open).toFixed(2));
-	    			$('.pdetails.high').text((val.high).toFixed(2));
-	    			$('.pdetails.khigh').text((val.weekyearhigh).toFixed(2));
-	    			$('.pdetails.val').text(nFormatter(parseFloat(val.value)));
-	    			$('.pdetails.av').text((val.average).toFixed(2));
+		$.ajax({
+		    type:'GET',
+		    url:'/wp-json/virtual-api/v1/buyvalues',
+		    dataType: 'json',
+		    success: function(response) {
 
-	    			$.ajax({
-					    type:'GET',
-					    url:'/wp-json/virtual-api/v1/marketdepth?stock='+ sdata,
-					    dataType: 'json',
-					    success: function(response) {
+		    	$.each(response.data, function(i, val) {
 
-					    	var bid = parseFloat(response.data.bid_total_percent).toFixed(2);
-					    	var ask = parseFloat(response.data.ask_total_percent).toFixed(2);
-					    	
-					    	$('.arb_bar_green').css('width', bid + '%');
-					    	$('.arb_bar_red').css('width', ask + '%');
-					    },
-					      error: function(response) {                 
-					      }
-					 });
+			    		if(sdata == val.symbol){
+			    			$('.sdesc').text(val.description);
+			    			$('.cprice').text((val.last).toFixed(2));
+			    			$('.pdetails.prev').text((val.close).toFixed(2));
+			    			$('.pdetails.low').text((val.low).toFixed(2));
+			    			$('.pdetails.klow').text(val.weekyearlow);
+			    			$('.pdetails.vol').text(nFormatter(parseFloat(val.volume)));
+			    			$('.pdetails.trade').text((val.trades).toFixed(2));
+			    			$('.pdetails.open').text((val.open).toFixed(2));
+			    			$('.pdetails.high').text((val.high).toFixed(2));
+			    			$('.pdetails.khigh').text((val.weekyearhigh).toFixed(2));
+			    			$('.pdetails.val').text(nFormatter(parseFloat(val.value)));
+			    			$('.pdetails.av').text((val.average).toFixed(2));
+
+			    			$.ajax({
+							    type:'GET',
+							    url:'/wp-json/virtual-api/v1/marketdepth?stock='+ sdata,
+							    dataType: 'json',
+							    success: function(response) {
+
+							    	var bid = parseFloat(response.data.bid_total_percent).toFixed(2);
+							    	var ask = parseFloat(response.data.ask_total_percent).toFixed(2);
+							    	
+							    	$('.arb_bar_green').css('width', bid + '%');
+							    	$('.arb_bar_red').css('width', ask + '%');
+							    },
+							      error: function(response) {                 
+							      }
+							 });
 
 
-	    		} 
-	    });
+			    		} 
+			    });
+		    	
+
+		    },
+		    error: function(response) {                 
+		    }
+	 	});
 
 	});
 
@@ -72,9 +83,6 @@ $(document).ready(function(){
 	     }
 	     return num;
 	}
-
-
-
 
 	$('.btnbuy').on('click', function(){
 		$('.btnbuy').css('background','#25ae5f');

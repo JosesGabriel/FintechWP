7dbc9b3b3a3fece7c5486f1294d8f7fc9a1f58e8
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-10-21 20:21:58 +0800

    draggable hotfix

--- wp-content/themes/arbitnew/interactivechart/interactivecharts-scripts.js
+++ wp-content/themes/arbitnew/interactivechart/interactivecharts-scripts.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
 
     $(window).load(function() {
-        $('#draggable_buysell').draggable();
-        $('#draggable_buysell').addClass('iyot');
+        $('#draggable_buysell').draggable({cancel:false});
 
         $("#status, #status_txt").fadeOut("fast");
         $("#preloader").delay(400).fadeOut("slow");

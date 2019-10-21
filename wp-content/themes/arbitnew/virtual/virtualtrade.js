bf09fe10e7d32a524f00ec682c52f84cfc7a9378
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-21 16:27:52 +0800

    Update virtualtrade.js

--- wp-content/themes/arbitnew/virtual/virtualtrade.js
+++ wp-content/themes/arbitnew/virtual/virtualtrade.js
@@ -287,13 +287,13 @@ $(document).ready(function(){
 	     return num;
 	}
 
-	function addcomma(n, sep) {
+	function addcomma(n, sep, decimals) {
 	    sep = sep || "."; // Default to period as decimal separator
-	    //decimals = decimals || 2; // Default to 2 decimals
+	    decimals = decimals || 2; // Default to 2 decimals
 
 	    return n.toLocaleString().split(sep)[0]
 	        + sep
-	        + n.split(sep)[1];
+	        + n.toFixed(2).split(sep)[1];
 	}
 
 

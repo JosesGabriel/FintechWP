fd36cda906f9b6dbcd4c4f54edfd5ac0c4339567
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-10-21 19:41:18 +0800

    draggable hotfix

--- wp-content/themes/arbitnew/interactivechart/footer-files.php
+++ wp-content/themes/arbitnew/interactivechart/footer-files.php
@@ -1,5 +1,13 @@
 
 	<!-- ================== BEGIN BASE JS ================== -->
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script>
+	$( function() {
+		jQuery( "#draggable_buysell" ).draggable();
+		alert('here');
+	} );
+	</script>
 	<script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
 
 	<!--[if lt IE 9]>

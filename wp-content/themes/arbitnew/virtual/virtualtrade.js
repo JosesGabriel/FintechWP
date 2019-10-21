c51df543ae78ee6a6cfff1b4abb44d72100388a1
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-21 12:19:44 +0800

    Update virtualtrade.js

--- wp-content/themes/arbitnew/virtual/virtualtrade.js
+++ wp-content/themes/arbitnew/virtual/virtualtrade.js
@@ -79,6 +79,8 @@ $(document).ready(function(){
 		    		$(".data_logs").remove();
 		    		jQuery.each(response.data, function(i, val) {
 			    		var data_tradelogs = '';
+			    		var profit = parseFloat(response.data[i].profit).toFixed(2);
+			    		var profperc = parseFloat(response.data[i].profitperc).toFixed(2);
 			    		data_tradelogs += '<li class="data_logs">';
 	                    data_tradelogs += '<div style="width:100%;">';
 	                    data_tradelogs += '<div style="width:45px">'+response.data[i].stockname+'</div>';                                                                                	
@@ -88,8 +90,8 @@ $(document).ready(function(){
 	                    data_tradelogs += '<div style="width:95px" class="table-title-live">₱</div>';
 	                    data_tradelogs += '<div style="width:65px" class="table-title-live">'+response.data[i].sellprice+'</div>';
 	                    data_tradelogs += '<div style="width:88px" class="table-title-live">₱</div>';
-	                    data_tradelogs += '<div style="width:80px" class="'+(profit < 0 ? 'dredpart ' : 'dgreenpart ')+'table-title-live">'+parseFloat(response.data[i].profit).toFixed(2)+'</div>';
-	                    data_tradelogs += '<div style="width:65px" class="'+(profperc < 0 ? 'dredpart ' : 'dgreenpart ')+'table-title-live">'+parseFloat(response.data[i].profitperc).toFixed(2)+'%</div>';
+	                    data_tradelogs += '<div style="width:80px" class="'+(profit < 0 ? 'dredpart ' : 'dgreenpart ')+'table-title-live">'+profit+'</div>';
+	                    data_tradelogs += '<div style="width:65px" class="'+(profperc < 0 ? 'dredpart ' : 'dgreenpart ')+'table-title-live">'+profperc+'%</div>';
 	                    data_tradelogs += '<div style="width:65px; text-align:center">';
 	                    data_tradelogs += '<div style="width:27px; text-align:center"><a class="smlbtn blue tldetails" data-tlstrats="" data-tltradeplans="" data-tlemotions="" data-tlnotes="" data-outcome=""><i class="fas fa-clipboard"></i></a></div>';
 	                    data_tradelogs += '<div style="width:25px"><a class="deletelog smlbtn-delete" data-istl="" style="cursor:pointer;text-align:center"><i class="fas fa-eraser"></i></a></div>';

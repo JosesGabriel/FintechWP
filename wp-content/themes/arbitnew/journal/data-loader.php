e23e105d7884128ff7e8bae1c771857b1d54f4fe
nick <nickgayo12@gmail.com>
2019-10-22 10:46:48 +0800

    Value color status on totalprofit

--- wp-content/themes/arbitnew/journal/data-loader.php
+++ wp-content/themes/arbitnew/journal/data-loader.php
@@ -89,7 +89,13 @@
                 });
 
                 let totalprofit = (parseFloat(data.totalprofit)).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-                $(".totalplscore").text("₱"+totalprofit);
+                if (totalprofit > 0){
+                    $(".totalplscore").text("₱"+totalprofit).css({"color": "#27ae60"});
+                }else if(totalprofit = 0){
+                    $(".totalplscore").text("₱"+totalprofit).css({"color": "#fff"});
+                }else{
+                    $(".totalplscore").text("₱"+totalprofit).css({"color": "#e44c3c"});
+                }
                 
             },
             error: function (xhr, ajaxOptions, thrownError) {

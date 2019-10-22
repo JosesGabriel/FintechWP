e4153b6b5090f2d1d5566e86290a964c961fa54a
nick <nickgayo12@gmail.com>
2019-10-22 14:45:43 +0800

    Watchlist Debugging 109

--- wp-content/themes/arbitnew/dashboard/footer-files.php
+++ wp-content/themes/arbitnew/dashboard/footer-files.php
@@ -51,7 +51,7 @@
                                     let addslog = (parseFloat(ischange)).toFixed(2);
                                     candles.push({"category": ckey,"column-1": addslog});
                                 });
-                                console.log(data.data)
+                                console.log(data)
                                 let dcolor = (changetext == "equal" ? '#ffd900' : ( changetext == "up" ? '#53b987' : '#eb4d5c' ) );
                                 AmCharts.makeChart( "chartdiv"+stock, {
                                     "type":"serial",

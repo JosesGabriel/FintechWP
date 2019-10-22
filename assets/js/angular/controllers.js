9235e51f94e98d79701b426656400ba748a983f7
Emmanuel Tierra <emman.tierra@gmail.com>
2019-10-22 14:24:23 +0800

    Add full delete execution for bids and asks

--- assets/js/angular/controllers.js
+++ assets/js/angular/controllers.js
@@ -595,6 +595,7 @@ app.controller('chart', ['$scope','$filter', '$http', '$rootScope', '$timeout', 
      *  au => update price
      *  d => delete
      *  u => update new order
+     *  fd => fully executed delete order (subtract count and volume)
      */
      socket.on('psebd', function (data) {
         if ($scope.selectedStock == data.sym && $scope.enableBidsAndAsks) {
@@ -638,6 +639,11 @@ app.controller('chart', ['$scope','$filter', '$http', '$rootScope', '$timeout', 
                 });
             }
             list.push($scope.addToBidAskList(data.idn, data));
+        } else if (data.ty = 'fd') {
+            // decrement data.id's count by 1, if count is zero, remove from list
+            list = $scope.updateBidAskCount(list, index, -1, 0);
+
+            list = $scope.updateBidAskVolume(list, index, (-1 * data.vol));
         }
         return list;
     }
@@ -651,6 +657,13 @@ app.controller('chart', ['$scope','$filter', '$http', '$rootScope', '$timeout', 
                     return key != id;
                 });
             }
+        }
+        return list;
+    }
+
+    $scope.updateBidAskVolume = function (list, id, increment) {
+        if (typeof list[id] !== 'undefined') {
+            list[id].volume += increment;
         }
         return list;
     }

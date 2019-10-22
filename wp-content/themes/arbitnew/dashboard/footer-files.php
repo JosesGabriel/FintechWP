70e875929c0ef267fc3006fcc99e5c10781e9906
nick <nickgayo12@gmail.com>
2019-10-22 15:26:47 +0800

    Watchlist Debugging 115

--- wp-content/themes/arbitnew/dashboard/footer-files.php
+++ wp-content/themes/arbitnew/dashboard/footer-files.php
@@ -50,7 +50,6 @@
                                     }
                                     let addslog = (parseFloat(ischange)).toFixed(2);
                                     candles.push({"category": ckey,"column-1": addslog});
-                                    console.log(stock, svalue.chartdata.c[ckey], [ckey])
                                 });
                                 let dcolor = (changetext == "equal" ? '#ffd900' : ( changetext == "up" ? '#53b987' : '#eb4d5c' ) );
                                 AmCharts.makeChart( "chartdiv"+stock, {

bdbfb6daf8bed06a074925e8f9dc3e100f62e966
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-23 14:52:24 +0800

    Update enter-trade.php

--- wp-content/themes/arbitnew/virtual/enter-trade.php
+++ wp-content/themes/arbitnew/virtual/enter-trade.php
@@ -73,7 +73,7 @@
                                 <div class="groupinput midd"><label class="labelprice">Buy Price</label><input type="text" id="entertopdataprice" name="inpt_data_price" class="inputbuyprice number" disabled></div>
                             </div>
                             <div class="quantity">
-                                <div class="groupinput midd"><label class="labelquantity">Quantity</label><input type="text" autocomplete="off" class="inputquantity number" required></div>
+                                <div class="groupinput midd"><label class="labelquantity">Quantity</label><input type="text" autocomplete="off" class="inputquantity number" style="padding-right: 5px;" required></div>
                             </div>
                         </div>
                         <div class="total_cost" style="text-align: right; margin-top: -5px;"><span style="font-size: 11px;">Total Cost: </span><span class="tlcost"></span></div>

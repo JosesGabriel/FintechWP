d7326c683b5067796a70c270ed79a2898822fb69
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-24 13:07:02 +0800

    Update virtualtrade.js

--- wp-content/themes/arbitnew/virtual/virtualtrade.js
+++ wp-content/themes/arbitnew/virtual/virtualtrade.js
@@ -20,8 +20,10 @@ $(document).ready(function(){
 	    	var opt = '';
 
 	    	response.data.sort(function(a,b){
-	    		return "<option value"+ a.symbol +">" + a.symbol + "</option>" > "<option value"+ b.symbol +">" + b.symbol + "</option>" ;
-	    	}).append('#inpt_data_select_stock');
+	    		return a.symbol > b.symbol;
+	    		console.log(a.symbol);
+	    	});//append('#inpt_data_select_stock');
+
 	    	/*$.each(response.data, function(i, val) {
 	    		opt = "<option value="+ val.symbol +">" + val.symbol + "</option>";
 	    		$('#inpt_data_select_stock').append(opt);

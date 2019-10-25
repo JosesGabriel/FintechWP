72158aa39ac297412e965c1313e3b40a68fe09a2
nick <nickgayo12@gmail.com>
2019-10-25 09:47:48 +0800

    easthetics works  watchlist on chart

--- wp-content/themes/arbitnew/interactivechart/components/watchlist.php
+++ wp-content/themes/arbitnew/interactivechart/components/watchlist.php
@@ -67,6 +67,7 @@
         <ul class="watchlist_main">
             <li>
                 <div class="watch-content watchlist_1 row" style="margin:0;">
+                        <div class="watchlist-dragdrop col-md-1 col-xs-1"><i class="fas fa-bars"></i></div>
                         <div class="watch_left col-md-6 col-xs-6">
                             <span class="watch_stockcode">BDO</span><br>
                             <span class="watch_stockname">Banco De Oro</span>
@@ -83,6 +84,7 @@
             <hr class="style14 style15">
             <li>
                 <div class="watch-content watchlist_1 row" style="margin:0;">
+                    <div class="watchlist-dragdrop col-md-1 col-xs-1"><i class="fas fa-bars"></i></div>
                         <div class="watch_left col-md-6 col-xs-6">
                             <span class="watch_stockcode">BDO</span><br>
                             <span class="watch_stockname">Banco De Oro</span>
@@ -99,6 +101,7 @@
             <hr class="style14 style15">
             <li>
                 <div class="watch-content watchlist_1 row" style="margin:0;">
+                    <div class="watchlist-dragdrop col-md-1 col-xs-1"><i class="fas fa-bars"></i></div>
                         <div class="watch_left col-md-6 col-xs-6">
                             <span class="watch_stockcode">BDO</span><br>
                             <span class="watch_stockname">Banco De Oro</span>
@@ -115,6 +118,7 @@
             <hr class="style14 style15">
             <li>
                 <div class="watch-content watchlist_1 row" style="margin:0;">
+                    <div class="watchlist-dragdrop col-md-1 col-xs-1"><i class="fas fa-bars"></i></div>
                         <div class="watch_left col-md-6 col-xs-6">
                             <span class="watch_stockcode">BDO</span><br>
                             <span class="watch_stockname">Banco De Oro</span>

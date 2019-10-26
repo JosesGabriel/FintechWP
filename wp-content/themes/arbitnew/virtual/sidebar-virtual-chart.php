179fb3a63da62ae0f1b6faee3ad5bfbb5baa4bd0
Rocky Gonzales <rocky@arbitrage.ph>
2019-10-26 17:23:05 +0800

    Update sidebar-virtual-chart.php

--- wp-content/themes/arbitnew/virtual/sidebar-virtual-chart.php
+++ wp-content/themes/arbitnew/virtual/sidebar-virtual-chart.php
@@ -3,12 +3,12 @@
 	<div style="font-size: 10px; text-align: center; display: block;">Wins/Losses for Tradelogs</div>
 </div>
 <div class="chart_win">
-	<span style="font-size: 10px;"><i class="fas fa-arrow-up" style="width: 100%;"></i></span>
+	<span class="dgrenpart" style="font-size: 10px;"><i class="fas fa-arrow-up" style="width: 100%;"></i></span>
 	<div style="width: 100%;"><span class="total_wins"></span></div>
 	<span style="font-size: 10px;" style="width: 100%;">Wins</span>
 </div>
 <div class="chart_loss">
-	<span style="font-size: 10px;"><i class="fas fa-arrow-up" style="width: 100%;"></i></span>
+	<span class="dredpart" style="font-size: 10px;"><i class="fas fa-arrow-down" style="width: 100%;"></i></span>
 	<div style="width: 100%;"><span class="total_loss" style="width: 100%;"></span></div>
 	<span style="font-size: 10px;" style="width: 100%;">Losses</span>
 </div>

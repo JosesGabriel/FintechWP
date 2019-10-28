199f3bee19539e3521fcd817ec28c16ad5f44476
Emmanuel Tierra <emman.tierra@gmail.com>
2019-10-28 15:23:26 +0800

    Add classes to functions

--- wp-content/themes/arbitnew/functions.php
+++ wp-content/themes/arbitnew/functions.php
@@ -2,6 +2,8 @@
 
 // wp-json end points
 #include 'functions-api.php';
+include 'functions-api-gateway.php';
+include 'functions-accounts-api.php';
 include 'functions-journalapi.php';
 include 'functions-charts-api.php';
 include 'functions-data-api.php';
